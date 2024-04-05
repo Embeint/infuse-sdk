5eba5ed3ed23bd4496cbe846b057c7b1e388ffde
Jordan Yates <jordan@embeint.com>
2024-04-05 22:02:44 +1000

    identifiers: added
    Add an API for querying the current EIS device ID. Currently hardcoded
    to 1234.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ include/eis/identifiers.h
@@ -0,0 +1,40 @@
+/**
+ * @file
+ * @brief EIS device identifiers
+ * @copyright 2024 Embeint Pty Ltd
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#ifndef EMBEINT_SDK_INCLUDE_EIS_IDENTIFIERS_H_
+#define EMBEINT_SDK_INCLUDE_EIS_IDENTIFIERS_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief EIS identifier API
+ * @defgroup eis_identifier_apis EIS identifier APIs
+ * @{
+ */
+
+/**
+ * @brief Get local device ID
+ *
+ * @return uint32_t local device ID
+ */
+uint32_t eis_device_id(void);
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* EMBEINT_SDK_INCLUDE_EIS_IDENTIFIERS_H_ */
---
+++ lib/identifiers.c
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Pty Ltd
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <eis/identifiers.h>
+
+uint32_t eis_device_id(void)
+{
+	return 1234;
+}
