0a135c48cb8421bb054eb27c80e3e0236d4a3035
Jordan Yates <jordan@embeint.com>
2024-04-24 12:10:42 +1000

    lib: common_boot: initialise KV store
    Initialise the KV store and increment the reboot counter in the common
    boot logic.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- lib/common_boot.c
+++ lib/common_boot.c
@@ -10,15 +10,38 @@
 #include <zephyr/logging/log.h>
 
 #include <infuse/identifiers.h>
+#include <infuse/fs/kv_store.h>
+#include <infuse/fs/kv_types.h>
 
 LOG_MODULE_REGISTER(infuse, CONFIG_INFUSE_COMMON_LOG_LEVEL);
 
 static int infuse_common_boot(void)
 {
+	KV_KEY_TYPE(KV_KEY_REBOOTS) reboot = {0};
 	uint32_t device_id = infuse_device_id();
 
-	LOG_INF("\tDevice: %08x", device_id);
-	LOG_INF("\t Board: %s", CONFIG_BOARD);
+#ifdef CONFIG_KV_STORE
+	KV_KEY_TYPE(KV_KEY_REBOOTS) reboot_fallback = {0};
+	int rc;
+
+	/* Initialise KV store */
+	rc = kv_store_init();
+
+	/* Get current reboot count */
+	if (rc == 0) {
+		rc = kv_store_read_fallback(KV_KEY_REBOOTS, &reboot, sizeof(reboot), &reboot_fallback,
+					    sizeof(reboot_fallback));
+		if (rc == sizeof(reboot)) {
+			/* Increment reboot counter */
+			reboot.count += 1;
+			(void)KV_STORE_WRITE(KV_KEY_REBOOTS, &reboot);
+		}
+	}
+#endif /* CONFIG_KV_STORE */
+
+	LOG_INF("\t Device: %08x", device_id);
+	LOG_INF("\t  Board: %s", CONFIG_BOARD);
+	LOG_INF("\tReboots: %d", reboot.count);
 	return 0;
 }
 
