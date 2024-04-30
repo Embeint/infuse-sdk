a739c1cdf7df8407c0342e9ee4abe3e9cfbe6968
Jordan Yates <jordan@embeint.com>
2024-04-30 20:40:30 +1000

    data_logger: initial implementation
    Add initial implementation of the core data_logger functions, together
    with backends for ePacket and flash map partitions.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ subsys/data_logger/backends/epacket.h
@@ -0,0 +1,39 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Pty Ltd
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#ifndef INFUSE_SDK_INCLUDE_INFUSE_DATA_LOGGER_BACKENDS_EPACKET_H_
+#define INFUSE_SDK_INCLUDE_INFUSE_DATA_LOGGER_BACKENDS_EPACKET_H_
+
+#include <stdint.h>
+
+#include <zephyr/devicetree.h>
+
+#include "backend_api.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DATA_LOGGER_BACKEND_CONFIG_EPACKET(node, data_ptr)                                                             \
+	.backend_api = &data_logger_epacket_api,                                                                       \
+	.backend_config = {                                                                                            \
+		.data = data_ptr,                                                                                      \
+		.backend = DEVICE_DT_GET(DT_PROP(node, epacket)),                                                      \
+		.logical_blocks = UINT32_MAX,                                                                          \
+		.physical_blocks = UINT32_MAX,                                                                         \
+		.erase_size = 0,                                                                                       \
+		.max_block_size = CONFIG_EPACKET_PAYLOAD_MAX,                                                          \
+	},
+
+extern const struct data_logger_backend_api data_logger_epacket_api;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INFUSE_SDK_INCLUDE_INFUSE_DATA_LOGGER_BACKENDS_EPACKET_H_ */
---
+++ subsys/data_logger/backends/flash_map.h
@@ -0,0 +1,38 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Pty Ltd
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#ifndef INFUSE_SDK_INCLUDE_INFUSE_DATA_LOGGER_BACKENDS_FLASH_MAP_H_
+#define INFUSE_SDK_INCLUDE_INFUSE_DATA_LOGGER_BACKENDS_FLASH_MAP_H_
+
+#include "backend_api.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define BLOCK_SIZE 512
+#define MAX_WRAPS  254
+
+#define DATA_LOGGER_BACKEND_CONFIG_FLASH_MAP(node, data_ptr)                                                           \
+	.backend_api = &data_logger_flash_map_api,                                                                     \
+	.backend_config = {                                                                                            \
+		.data = data_ptr,                                                                                      \
+		.flash_area_id = DT_FIXED_PARTITION_ID(DT_PROP(node, partition)),                                      \
+		.logical_blocks = (DT_REG_SIZE(DT_PROP(node, partition)) / BLOCK_SIZE) * MAX_WRAPS,                    \
+		.physical_blocks = (DT_REG_SIZE(DT_PROP(node, partition)) / BLOCK_SIZE),                               \
+		.erase_size = DT_PROP_OR(DT_GPARENT(DT_PROP(node, partition)), erase_block_size, 4096),                \
+		.max_block_size = BLOCK_SIZE,                                                                          \
+	},
+
+extern const struct data_logger_backend_api data_logger_flash_map_api;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INFUSE_SDK_INCLUDE_INFUSE_DATA_LOGGER_BACKENDS_FLASH_MAP_H_ */
