0c8fb1c706d3d2d19ffa1f655d2f49a963695453
Jordan Yates <jordan@embeint.com>
2024-05-01 11:21:05 +1000

    epacket: interfaces: dummy: added
    Add a dummy interface to ePacket for testing purposes.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ subsys/epacket/interfaces/epacket_dummy.c
@@ -0,0 +1,63 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Pty Ltd
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <zephyr/device.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/sys/byteorder.h>
+
+#include <infuse/epacket/interface.h>
+#include <infuse/epacket/packet.h>
+#include <infuse/epacket/interface/epacket_dummy.h>
+
+#include "epacket_internal.h"
+
+#define DT_DRV_COMPAT embeint_epacket_dummy
+
+LOG_MODULE_REGISTER(epacket_dummy, CONFIG_EPACKET_LOG_LEVEL);
+
+static K_FIFO_DEFINE(epacket_dummy_fifo);
+
+struct k_fifo *epacket_dummmy_transmit_fifo_get(void)
+{
+	return &epacket_dummy_fifo;
+}
+
+static void epacket_dummy_packet_overhead(const struct device *dev, size_t *header, size_t *footer)
+{
+	*header = sizeof(struct epacket_dummy_frame);
+	*footer = 0;
+}
+
+static int epacket_dummy_send(const struct device *dev, struct net_buf *buf)
+{
+	struct epacket_dummy_frame *header = net_buf_push(buf, sizeof(struct epacket_dummy_frame));
+	struct epacket_metadata *meta = net_buf_user_data(buf);
+
+	header->type = meta->type;
+	header->auth = meta->auth;
+	header->flags = meta->flags;
+
+	net_buf_put(&epacket_dummy_fifo, buf);
+	return 0;
+}
+
+static int epacket_dummy_init(const struct device *dev)
+{
+	k_fifo_init(&epacket_dummy_fifo);
+	return 0;
+}
+
+static const struct epacket_interface_api dummy_api = {
+	.packet_overhead = epacket_dummy_packet_overhead,
+	.send = epacket_dummy_send,
+};
+
+#define EPACKET_DUMMY_DEFINE(inst)                                                                                     \
+	DEVICE_DT_INST_DEFINE(inst, epacket_dummy_init, NULL, NULL, NULL, POST_KERNEL, 0, &dummy_api);
+
+DT_INST_FOREACH_STATUS_OKAY(EPACKET_DUMMY_DEFINE)
