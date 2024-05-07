ccaf9ad3ce663869b0a3377e8d2e04bfbf30ea28
Jordan Yates <jordan@embeint.com>
2024-05-07 10:11:22 +1000

    epacket: serial: rework packet reconstructor
    Rework the packet reconstructor to not block on RX buffers and not
    immediately start searching for a new header if a too large packet is
    observed.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- subsys/epacket/interfaces/epacket_serial_crypt.c
+++ subsys/epacket/interfaces/epacket_serial_crypt.c
@@ -12,6 +12,7 @@
 #include <zephyr/random/random.h>
 #include <zephyr/net/buf.h>
 #include <zephyr/sys/byteorder.h>
+#include <zephyr/logging/log.h>
 
 #include <psa/crypto.h>
 
@@ -23,6 +24,12 @@
 
 #include "epacket_internal.h"
 
+#ifdef CONFIG_ZTEST
+LOG_MODULE_REGISTER(epacket_serial, LOG_LEVEL_INF);
+#else
+LOG_MODULE_DECLARE(epacket_serial);
+#endif
+
 static const uint8_t sync_bytes[2] = {EPACKET_SERIAL_SYNC_A, EPACKET_SERIAL_SYNC_B};
 
 void epacket_serial_reconstruct(const struct device *dev, uint8_t *buffer, size_t len,
@@ -31,15 +38,15 @@ void epacket_serial_reconstruct(const struct device *dev, uint8_t *buffer, size_
 	static struct net_buf *rx_buffer;
 	static uint16_t payload_remaining;
 	static uint8_t len_lsb;
-	static uint16_t header_idx;
+	static uint16_t pkt_idx;
 	struct epacket_rx_metadata *meta;
 
 	for (int i = 0; i < len; i++) {
-		switch (header_idx) {
+		switch (pkt_idx) {
 		case 0:
 		case 1:
-			if (buffer[i] != sync_bytes[header_idx]) {
-				header_idx = 0;
+			if (buffer[i] != sync_bytes[pkt_idx]) {
+				pkt_idx = 0;
 				continue;
 			}
 			break;
@@ -48,46 +55,60 @@ void epacket_serial_reconstruct(const struct device *dev, uint8_t *buffer, size_
 			break;
 		case 3:
 			payload_remaining = ((uint16_t)buffer[i] << 8) | len_lsb;
-			if (payload_remaining > CONFIG_EPACKET_PACKET_SIZE_MAX) {
-				/* Can't receive this packet */
-				header_idx = 0;
-			}
 			break;
 		}
-		/* Still waiting on the payload length */
-		if (header_idx <= 3) {
-			header_idx++;
-			/* Next iteration unless a 0 byte packet */
-			if (header_idx < 4 || payload_remaining) {
-				continue;
-			}
+		pkt_idx++;
+		if (pkt_idx == 4 && payload_remaining == 0) {
+			/* Zero length packet */
+			rx_buffer = epacket_alloc_rx(K_NO_WAIT);
+			goto packet_complete;
 		}
-		/* Claim memory if none */
-		if (rx_buffer == NULL) {
-			rx_buffer = epacket_alloc_rx(K_FOREVER);
+		if (pkt_idx <= 4) {
+			continue;
+		}
+		/* Allocate RX buffer */
+		if (pkt_idx == 5) {
+			if (payload_remaining > CONFIG_EPACKET_PACKET_SIZE_MAX) {
+				LOG_WRN("Payload %d too large", payload_remaining);
+			} else {
+				/* Can't block in interrupts */
+				rx_buffer = epacket_alloc_rx(K_NO_WAIT);
+				if (rx_buffer == NULL) {
+					LOG_WRN("Dropping packet");
+				}
+			}
 		}
 		/* Payload bytes */
 		uint16_t to_add = MIN(payload_remaining, len - i);
 
 		/* Add payload to buffer */
 		if (to_add > 0) {
-			net_buf_add_mem(rx_buffer, buffer + i, to_add);
+			if (rx_buffer) {
+				net_buf_add_mem(rx_buffer, buffer + i, to_add);
+			}
+			pkt_idx += to_add;
 			payload_remaining -= to_add;
 			i += to_add - 1;
 		}
 
 		/* Is packet done? */
 		if (payload_remaining == 0) {
-			meta = net_buf_user_data(rx_buffer);
-			meta->interface = dev;
-			meta->interface_id = EPACKET_INTERFACE_SERIAL;
-			meta->rssi = 0;
 
-			/* Hand off to core ePacket functions */
-			handler(rx_buffer);
+			/* clang-format off */
+packet_complete:
+			/* clang-format on */
+			if (rx_buffer) {
+				meta = net_buf_user_data(rx_buffer);
+				meta->interface = dev;
+				meta->interface_id = EPACKET_INTERFACE_SERIAL;
+				meta->rssi = 0;
+
+				/* Hand off to core ePacket functions */
+				handler(rx_buffer);
+			}
 			/* Reset parsing state */
 			rx_buffer = NULL;
-			header_idx = 0;
+			pkt_idx = 0;
 		}
 	}
 }
