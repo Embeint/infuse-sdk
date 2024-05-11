994fde4ec8a2ea3df212168bcf23698e766f6aac
Jordan Yates <jordan@embeint.com>
2024-05-11 19:57:52 +1000

    rpc: commands: reboot: added
    Add a RPC implementation to reboot the device after a delay.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ subsys/rpc/commands/reboot.c
@@ -0,0 +1,34 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Pty Ltd
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <zephyr/net/buf.h>
+#include <zephyr/logging/log.h>
+
+#include <infuse/rpc/types.h>
+#include <infuse/reboot.h>
+
+#include "../server.h"
+
+LOG_MODULE_DECLARE(rpc_server);
+
+struct net_buf *rpc_command_reboot(struct net_buf *request)
+{
+	struct rpc_reboot_request *req = (void *)request->data;
+	struct rpc_reboot_response rsp = {
+		.delay_ms = req->delay_ms ? req->delay_ms : 2000,
+	};
+	struct net_buf *response;
+
+	/* Allocate the response packet */
+	response = rpc_response_simple_req(request, 0, &rsp, sizeof(rsp));
+	/* Schedule the reboot */
+	infuse_reboot_delayed(INFUSE_REBOOT_RPC, (uintptr_t)rpc_command_reboot, 0x00, K_MSEC(rsp.delay_ms));
+	LOG_INF("%s: Rebooting in %d ms", __func__, rsp.delay_ms);
+	/* Return the response */
+	return response;
+}
