8c4e77bb32637d62a448a416663e55639f292699
Jordan Yates <jordan@embeint.com>
2024-05-14 14:25:43 +1000

    tests: data_logger: tdf: test masked functions
    Add tests for the masked log and flush operations.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ tests/subsys/data_logger/high_level/tdf_multi/src/main.c
@@ -0,0 +1,71 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Pty Ltd
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <stdint.h>
+
+#include <zephyr/ztest.h>
+#include <zephyr/kernel.h>
+
+#include <infuse/epacket/packet.h>
+#include <infuse/data_logger/logger.h>
+#include <infuse/data_logger/high_level/tdf.h>
+
+enum {
+	TDF_RANDOM = 37,
+};
+
+static void validate_loggers(uint32_t expected_flash, uint32_t expected_epacket)
+{
+	const struct device *flash_logger = DEVICE_DT_GET(DT_NODELABEL(data_logger_flash));
+	const struct device *epacket_logger = DEVICE_DT_GET(DT_NODELABEL(data_logger_udp));
+	struct data_logger_state state;
+
+	data_logger_get_state(flash_logger, &state);
+	zassert_equal(expected_flash, state.current_block);
+	data_logger_get_state(epacket_logger, &state);
+	zassert_equal(expected_epacket, state.current_block);
+}
+
+ZTEST(tdf_data_logger_multi, test_standard)
+{
+	uint8_t tdf_data[32];
+	uint32_t expected_flash = 0;
+	uint32_t expected_epacket = 0;
+
+	/* Initial state */
+	validate_loggers(expected_flash, expected_epacket);
+
+	/* Flush both devices */
+	tdf_data_logger_flush(TDF_DATA_LOGGER_FLASH | TDF_DATA_LOGGER_UDP);
+	validate_loggers(expected_flash, expected_epacket);
+
+	/* Push data to one, flush the other */
+	tdf_data_logger_log(TDF_DATA_LOGGER_FLASH, TDF_RANDOM, 17, 0, tdf_data);
+	tdf_data_logger_flush(TDF_DATA_LOGGER_UDP);
+
+	validate_loggers(expected_flash, expected_epacket);
+
+	/* Flush the one we pushed to */
+	tdf_data_logger_flush(TDF_DATA_LOGGER_FLASH);
+	validate_loggers(++expected_flash, expected_epacket);
+
+	/* Push to non-existent */
+	tdf_data_logger_log(0x80 | TDF_DATA_LOGGER_SERIAL, TDF_RANDOM, 17, 0, tdf_data);
+	tdf_data_logger_flush(0x80 | TDF_DATA_LOGGER_SERIAL);
+	validate_loggers(expected_flash, expected_epacket);
+
+	/* Add to both */
+	tdf_data_logger_log(TDF_DATA_LOGGER_FLASH | TDF_DATA_LOGGER_UDP, TDF_RANDOM, 17, 0, tdf_data);
+	validate_loggers(expected_flash, expected_epacket);
+
+	/* Flush both */
+	tdf_data_logger_flush(TDF_DATA_LOGGER_FLASH | TDF_DATA_LOGGER_UDP);
+	validate_loggers(++expected_flash, ++expected_epacket);
+}
+
+ZTEST_SUITE(tdf_data_logger_multi, NULL, NULL, NULL, NULL, NULL);
