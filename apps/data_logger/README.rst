05c00b80a1fed84a96db77634ba709ad6a127c7a
Jordan Yates <jordan@embeint.com>
2024-05-15 11:08:46 +1000

    apps: data_logger: added
    Add a simple application that samples the IMU, environmental and battery
    sensors on a Thingy53 with hardcoded parameters.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ apps/data_logger/README.rst
@@ -0,0 +1,10 @@
+.. _data_logger:
+
+Data Logger
+###########
+
+Overview
+********
+
+A basic data gathering application that reads from the Battery, IMU and
+environmental sensors at fixed frequencies.
---
+++ apps/data_logger/src/main.c
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Pty Ltd
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/pm/device_runtime.h>
+
+#include <infuse/time/civil.h>
+#include <infuse/fs/kv_store.h>
+#include <infuse/fs/kv_types.h>
+#include <infuse/epacket/interface.h>
+#include <infuse/epacket/packet.h>
+#include <infuse/data_logger/high_level/tdf.h>
+#include <infuse/tdf/definitions.h>
+
+LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
+
+int main(void)
+{
+	/* No work to do in main thread */
+	k_sleep(K_FOREVER);
+}
