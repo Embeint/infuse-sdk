9191128a92e1d040c8c3e531e37820cffac9f31f
Jordan Yates <jordan@embeint.com>
2024-05-18 11:42:57 +1000

    treewide: update file copyrights
    Update file copyrights to Embeint Inc, the US entity.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- include/infuse/identifiers.h
+++ include/infuse/identifiers.h
@@ -1,7 +1,7 @@
 /**
  * @file
  * @brief Infuse IoT device identifiers
- * @copyright 2024 Embeint Pty Ltd
+ * @copyright 2024 Embeint Inc
  * @author Jordan Yates <jordan@embeint.com>
  *
  * SPDX-License-Identifier: LicenseRef-Embeint
--- subsys/data_logger/backends/backend_api.h
+++ subsys/data_logger/backends/backend_api.h
@@ -1,7 +1,7 @@
 /**
  * @file
  * @brief Data logger backend
- * @copyright 2024 Embeint Pty Ltd
+ * @copyright 2024 Embeint Inc
  * @author Jordan Yates <jordan@embeint.com>
  *
  * SPDX-License-Identifier: LicenseRef-Embeint
