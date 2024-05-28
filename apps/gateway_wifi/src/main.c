18052b5542238636d8a91ba37f8924d50d2894eb
Jordan Yates <jordan@embeint.com>
2024-05-28 15:07:56 +1000

    apps: gateway_wifi: remove wifi config options
    There is no need for to hardcode WiFi SSID and PSK as they are easily
    set at runtime via RPC.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- apps/gateway_wifi/src/main.c
+++ apps/gateway_wifi/src/main.c
@@ -9,14 +9,12 @@
 #include <stdio.h>
 
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/net/net_if.h>
-
 #include <zephyr/net/net_mgmt.h>
 #include <zephyr/net/wifi_mgmt.h>
 
-#include <infuse/fs/kv_store.h>
-#include <infuse/fs/kv_types.h>
 #include <infuse/epacket/interface.h>
 #include <infuse/epacket/packet.h>
 #include <infuse/data_logger/high_level/tdf.h>
@@ -26,14 +24,8 @@ LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
 
 int main(void)
 {
-	KV_STRING_CONST(wifi_ssid, CONFIG_WIFI_SSID);
-	KV_STRING_CONST(wifi_psk, CONFIG_WIFI_PSK);
 	const struct device *tdf_logger_udp = DEVICE_DT_GET(DT_NODELABEL(tdf_logger_udp));
 	struct tdf_announce announce;
-
-	/* Write WiFi configuration */
-	kv_store_write(KV_KEY_WIFI_SSID, &wifi_ssid, sizeof(wifi_ssid));
-	kv_store_write(KV_KEY_WIFI_PSK, &wifi_psk, sizeof(wifi_psk));
 
 	for (;;) {
 		announce.application = 0x1234;
