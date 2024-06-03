c9da35c19874fd65c3243e1f74f28e7319f4ab0d
Jordan Yates <jordan@embeint.com>
2024-06-03 16:58:52 +1000

    fs: secure_storage: added
    Add implementation of PSA internal-trusted-storage over Zephyr NVS.
    The implementation uses the same filesystem instance as the KV store to
    minimise resource usage.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ subsys/fs/secure_storage/include/psa/error.h
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * @brief PSA internal trusted storage errors
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ *
+ * @details
+ */
+
+#include <psa/crypto_values.h>
