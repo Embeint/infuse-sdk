b5a37f8ef4ebb44520292df4af997461b77a2f30
Jordan Yates <jordan@embeint.com>
2024-06-10 14:54:28 +1000

    lib: vendor: move identifier code
    Move the identifier logic to vendor specific folders, as the storage
    mechanism will differ depending on the vendor.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ lib/vendor/qemu/identifiers.c
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <stdint.h>
+
+uint64_t infuse_device_id(void)
+{
+	/* Hardcoded ID */
+	return 0x1000000ULL;
+}
