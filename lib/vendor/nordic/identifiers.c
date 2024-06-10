70a45d204009cdb4032e09880e6da0c8cb0348c5
Jordan Yates <jordan@embeint.com>
2024-06-10 21:13:25 +1000

    lib: vendor: nordic: read identifiers from UICR
    Retrieve the current device ID from UICR.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- lib/vendor/nordic/identifiers.c
+++ lib/vendor/nordic/identifiers.c
@@ -6,13 +6,30 @@
  * SPDX-License-Identifier: LicenseRef-Embeint
  */
 
+#include <string.h>
+
 #include <infuse/identifiers.h>
+
+#include <nrf.h>
+
+#ifdef CONFIG_ARM_TRUSTZONE_M
+#define UICR_PTR NRF_UICR_S
+#define UICR_REG OTP
+#else
+#define UICR_PTR NRF_UICR
+#define UICR_REG CUSTOMER
+#endif
+
+struct nrf_uicr_structure {
+	uint64_t infuse_device_id;
+	uint8_t infuse_identifier_secret[16];
+};
 
 uint64_t infuse_device_id(void)
 {
-#ifdef CONFIG_BOARD_THINGY53_NRF5340_CPUAPP
-	return 1235;
-#else
-	return 1234;
-#endif
+	struct nrf_uicr_structure readout;
+
+	memcpy(&readout, (const void *)UICR_PTR->UICR_REG, sizeof(readout));
+
+	return readout.infuse_device_id;
 }
