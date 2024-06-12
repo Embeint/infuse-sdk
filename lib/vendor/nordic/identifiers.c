0193e4cd12b81eb8624c1e7f0ba64f1bfe249eb0
Jordan Yates <jordan@embeint.com>
2024-06-12 18:03:31 +1000

    lib: nordic: identifiers: work with TF-M
    Update the indentity query to work through the secure image when built
    with TF-M. As this can be expensive compared to a memcpy, cache the
    result for future calls.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- lib/vendor/nordic/identifiers.c
+++ lib/vendor/nordic/identifiers.c
@@ -10,6 +10,7 @@
 
 #include <infuse/identifiers.h>
 
+#include <soc_secure.h>
 #include <nrf.h>
 
 #ifdef CONFIG_ARM_TRUSTZONE_M
@@ -27,9 +28,22 @@ struct nrf_uicr_structure {
 
 uint64_t infuse_device_id(void)
 {
+#ifdef CONFIG_BUILD_WITH_TFM
+	static struct nrf_uicr_structure readout;
+	static bool queried;
+
+	if (queried == false) {
+		if (soc_secure_mem_read(&readout, (void *)UICR_PTR->UICR_REG, sizeof(readout)) <
+		    0) {
+			readout.infuse_device_id = UINT64_MAX - 1;
+		}
+		queried = true;
+	}
+#else
 	struct nrf_uicr_structure readout;
 
 	memcpy(&readout, (const void *)UICR_PTR->UICR_REG, sizeof(readout));
+#endif
 
 	return readout.infuse_device_id;
 }
