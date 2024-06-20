466ec3e128c15c2ca292fc765c4967ae01200357
Jordan Yates <jordan@embeint.com>
2024-06-20 13:48:33 +1000

    samples: low_power: added
    Add a basic sample that simply goes into the default low power state of
    the hardware.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ samples/low_power/src/main.c
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <stdio.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
+
+int main(void)
+{
+	for (;;) {
+		LOG_INF("Uptime: %6d seconds", k_uptime_seconds());
+		k_sleep(K_SECONDS(1));
+	}
+	return 0;
+}
