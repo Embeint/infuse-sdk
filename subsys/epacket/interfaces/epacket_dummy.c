9e14034c86b3ba45c87fd0a30844c97618a51b16
Jordan Yates <jordan@embeint.com>
2024-06-28 14:31:16 +1000

    epacket: dummy: support `receive_ctrl`
    Add support for `receive_ctrl` to enable testing the common interface
    logic. Don't require explicit RX for `epacket_dummy_receive_extra` to
    keep all the other test logic simpler.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- subsys/epacket/interfaces/epacket_dummy.c
+++ subsys/epacket/interfaces/epacket_dummy.c
@@ -24,6 +24,8 @@ LOG_MODULE_REGISTER(epacket_dummy, CONFIG_EPACKET_LOG_LEVEL);
 static K_FIFO_DEFINE(epacket_dummy_fifo);
 static int send_error_code;
 static uint16_t max_packet_size = EPACKET_INTERFACE_MAX_PACKET(DT_DRV_INST(0));
+static bool receiving;
+static int receive_rc;
 
 struct k_fifo *epacket_dummmy_transmit_fifo_get(void)
 {
@@ -33,6 +35,11 @@ struct k_fifo *epacket_dummmy_transmit_fifo_get(void)
 void epacket_dummy_set_tx_failure(int error_code)
 {
 	send_error_code = error_code;
+}
+
+bool epacket_dummy_receive_scheduled(void)
+{
+	return receiving;
 }
 
 void epacket_dummy_set_max_packet(uint16_t packet_size)
@@ -81,6 +88,14 @@ static void epacket_dummy_send(const struct device *dev, struct net_buf *buf)
 	}
 }
 
+static int epacket_dummy_receive_ctrl(const struct device *dev, bool enable)
+{
+	if (receive_rc == 0) {
+		receiving = enable;
+	}
+	return receive_rc;
+}
+
 uint16_t epacket_dummy_max_packet_size(const struct device *dev)
 {
 	return max_packet_size;
@@ -109,10 +124,21 @@ static int epacket_dummy_init(const struct device *dev)
 	return 0;
 }
 
-static const struct epacket_interface_api dummy_api = {
+static struct epacket_interface_api dummy_api = {
 	.send = epacket_dummy_send,
+	.receive_ctrl = epacket_dummy_receive_ctrl,
 	.max_packet_size = epacket_dummy_max_packet_size,
 };
+
+void epacket_dummy_receive_api_override(bool func_exists, int rc)
+{
+	if (func_exists) {
+		dummy_api.receive_ctrl = epacket_dummy_receive_ctrl;
+	} else {
+		dummy_api.receive_ctrl = NULL;
+	}
+	receive_rc = rc;
+}
 
 #define EPACKET_DUMMY_DEFINE(inst)                                                                 \
 	BUILD_ASSERT(sizeof(struct epacket_dummy_frame) == DT_INST_PROP(inst, header_size));       \
