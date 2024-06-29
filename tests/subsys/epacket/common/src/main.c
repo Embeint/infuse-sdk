679b46fe644d7384b3633b2f2384b08050df7957
Jordan Yates <jordan@embeint.com>
2024-06-29 15:24:59 +1000

    tests: epacket: common: test forwarding function
    Add tests for the packet forwarding function.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- tests/subsys/epacket/common/src/main.c
+++ tests/subsys/epacket/common/src/main.c
@@ -8,13 +8,22 @@
 
 #include <zephyr/ztest.h>
 #include <zephyr/kernel.h>
+#include <zephyr/random/random.h>
+#include <zephyr/sys/byteorder.h>
 
 #include <infuse/types.h>
 #include <infuse/epacket/interface.h>
 #include <infuse/epacket/packet.h>
 #include <infuse/epacket/interface/epacket_dummy.h>
+#include <infuse/epacket/interface/epacket_bt_adv.h>
+#include <infuse/security.h>
 
-static K_FIFO_DEFINE(handler_fifo);
+#include "../subsys/epacket/interfaces/epacket_internal.h"
+
+uint64_t infuse_device_id(void)
+{
+	return 0x0123456789ABCDEF;
+}
 
 ZTEST(epacket_common, test_alloc_failure)
 {
@@ -124,4 +133,96 @@ ZTEST(epacket_common, test_receive_error)
 	epacket_dummy_receive_api_override(true, 0);
 }
 
-ZTEST_SUITE(epacket_common, NULL, NULL, NULL, NULL, NULL);
+static struct net_buf *create_received_tdf_packet(uint8_t payload_len, bool encrypt)
+{
+	const bt_addr_le_t bt_addr_none = {0, {{0, 0, 0, 0, 0, 0}}};
+	struct net_buf *buf_tx, *buf_rx;
+	struct epacket_tx_metadata *tx_meta;
+	struct epacket_rx_metadata *rx_meta;
+	uint8_t *p;
+
+	/* Construct the original TX packet */
+	buf_tx = epacket_alloc_tx(K_NO_WAIT);
+	zassert_not_null(buf_tx);
+	net_buf_reserve(buf_tx, sizeof(struct epacket_bt_adv_frame));
+	epacket_set_tx_metadata(buf_tx, EPACKET_AUTH_DEVICE, 0, INFUSE_TDF);
+	p = net_buf_add(buf_tx, 60);
+	sys_rand_get(p, 60);
+
+	if (encrypt) {
+		zassert_equal(0, epacket_bt_adv_encrypt(buf_tx));
+	}
+	tx_meta = net_buf_user_data(buf_tx);
+
+	/* Copy across to a received packet */
+	buf_rx = epacket_alloc_rx(K_NO_WAIT);
+	zassert_not_null(buf_rx);
+	net_buf_add_mem(buf_rx, buf_tx->data, buf_tx->len);
+
+	/* Add metadata */
+	rx_meta = net_buf_user_data(buf_rx);
+	rx_meta->interface = NULL;
+	rx_meta->interface_id = EPACKET_INTERFACE_BT_ADV;
+	rx_meta->interface_address.bluetooth = bt_addr_none;
+	rx_meta->rssi = -80;
+	rx_meta->flags = encrypt ? EPACKET_FLAGS_ENCRYPTION_DEVICE : 0x00;
+	rx_meta->auth = encrypt ? EPACKET_AUTH_FAILURE : EPACKET_AUTH_DEVICE;
+
+	/* Free the TX buffer */
+	net_buf_unref(buf_tx);
+	return buf_rx;
+}
+
+ZTEST(epacket_common, test_receive_append)
+{
+	const struct device *epacket_dummy = DEVICE_DT_GET(DT_NODELABEL(epacket_dummy));
+	struct net_buf *buf_rx, *buf_tx;
+	uint16_t offset, len;
+	int rc;
+
+	buf_tx = epacket_alloc_tx_for_interface(epacket_dummy, K_NO_WAIT);
+	zassert_not_null(buf_tx);
+
+	/* Append packet that decrypted */
+	buf_rx = create_received_tdf_packet(32, false);
+	rc = epacket_received_packet_append(buf_tx, buf_rx);
+	zassert_equal(0, rc);
+
+	/* Append packet that did not decrypt */
+	buf_rx = create_received_tdf_packet(32, true);
+	rc = epacket_received_packet_append(buf_tx, buf_rx);
+	zassert_equal(0, rc);
+
+	/* Should be out of space */
+	len = buf_tx->len;
+	buf_rx = create_received_tdf_packet(32, true);
+	rc = epacket_received_packet_append(buf_tx, buf_rx);
+	zassert_equal(-ENOMEM, rc);
+	zassert_equal(len, buf_tx->len);
+	net_buf_unref(buf_rx);
+
+	/* Basic validation of our format */
+	offset = 0;
+	/* First appended packet */
+	len = sys_get_le16(buf_tx->data + offset);
+	zassert_not_equal(0, len);
+	zassert_equal(0x0000, len & 0x8000);
+	offset += len & 0x7FFF;
+	/* Second appended packet */
+	len = sys_get_le16(buf_tx->data + offset);
+	zassert_not_equal(0, len);
+	zassert_equal(0x8000, len & 0x8000);
+	offset += len & 0x7FFF;
+	/* Should equal buffer size */
+	zassert_equal(buf_tx->len, offset);
+
+	net_buf_unref(buf_tx);
+}
+
+static bool security_init(const void *global_state)
+{
+	infuse_security_init();
+	return true;
+}
+
+ZTEST_SUITE(epacket_common, security_init, NULL, NULL, NULL, NULL);
