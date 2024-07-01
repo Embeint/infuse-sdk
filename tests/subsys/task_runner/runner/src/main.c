8bfe8f99ccf311ebc80d6792cc7f6747b9c9e711
Jordan Yates <jordan@embeint.com>
2024-07-01 12:54:51 +1000

    tests: task_runner: runner: added
    Add tests for the core runner logic.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ tests/subsys/task_runner/runner/src/main.c
@@ -0,0 +1,187 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <string.h>
+
+#include <zephyr/ztest.h>
+#include <zephyr/kernel.h>
+#include <zephyr/random/random.h>
+
+#include <infuse/task_runner/runner.h>
+
+enum task_ids {
+	TASK_ID_SLEEPY = 113,
+};
+
+static k_timeout_t example_task_block_timeout;
+static int example_task_expected_block_rc;
+static int example_task_run_cnt;
+
+void example_task_fn(const struct task_schedule *schedule, struct k_poll_signal *terminate)
+{
+	int rc;
+
+	zassert_not_null(schedule);
+	zassert_not_null(terminate);
+	example_task_run_cnt += 1;
+
+	/* Block for the expected duration */
+	rc = task_runner_task_block(terminate, example_task_block_timeout);
+	/* Ensure result matches the expected value */
+	zassert_equal(example_task_expected_block_rc, rc);
+}
+
+K_THREAD_STACK_DEFINE(example_stack_area, 1024);
+static const struct task_config task_configs[] = {
+	{
+		.name = "sleepy",
+		.task_id = TASK_ID_SLEEPY,
+		.task_fn = example_task_fn,
+		.thread_stack = example_stack_area,
+		.thread_stack_size = K_THREAD_STACK_SIZEOF(example_stack_area),
+	},
+};
+static struct task_data task_data[ARRAY_SIZE(task_configs)];
+
+ZTEST(task_runner_runner, test_init_invalid)
+{
+	struct task_schedule schedules[] = {
+		{
+			.task_id = TASK_ID_SLEEPY + 1,
+			.validity = TASK_VALID_ALWAYS,
+			.periodicity_type = TASK_PERIODICITY_FIXED,
+			.periodicity.fixed.period_s = 5,
+			.timeout_s = 4,
+		},
+	};
+	struct task_schedule_state states[ARRAY_SIZE(schedules)];
+	uint32_t gps_time = 7000;
+	uint32_t uptime = 0;
+	uint32_t iter = k_uptime_seconds() + 1;
+
+	/* Schedule refers to task that does not exist */
+	task_runner_init(schedules, states, ARRAY_SIZE(schedules), task_configs, task_data,
+			 ARRAY_SIZE(task_configs));
+	for (int i = 0; i < 10; i++) {
+		task_runner_iterate(uptime++, gps_time++, 100);
+		k_sleep(K_TIMEOUT_ABS_MS(iter * MSEC_PER_SEC));
+		iter++;
+	}
+	zassert_equal(0, example_task_run_cnt);
+
+	/* Schedule is invalid */
+	schedules[0].task_id = TASK_ID_SLEEPY;
+	schedules[0].battery_start_threshold = 110;
+	task_runner_init(schedules, states, ARRAY_SIZE(schedules), task_configs, task_data,
+			 ARRAY_SIZE(task_configs));
+	for (int i = 0; i < 10; i++) {
+		task_runner_iterate(uptime++, gps_time++, 100);
+		k_sleep(K_TIMEOUT_ABS_MS(iter * MSEC_PER_SEC));
+		iter++;
+	}
+	zassert_equal(0, example_task_run_cnt);
+}
+
+ZTEST(task_runner_runner, test_basic_behaviour)
+{
+	struct task_schedule schedules[] = {
+		{
+			.task_id = TASK_ID_SLEEPY,
+			.validity = TASK_VALID_ALWAYS,
+			.periodicity_type = TASK_PERIODICITY_FIXED,
+			.periodicity.fixed.period_s = 5,
+			.timeout_s = 4,
+		},
+	};
+	struct task_schedule_state states[ARRAY_SIZE(schedules)];
+	uint32_t gps_time = 7000;
+	uint32_t uptime = 0;
+	uint32_t iter = k_uptime_seconds() + 1;
+
+	task_runner_init(schedules, states, ARRAY_SIZE(schedules), task_configs, task_data,
+			 ARRAY_SIZE(task_configs));
+
+	/* Immediate termination (10 seconds with 5 second period == 2 runs) */
+	for (int i = 0; i < 10; i++) {
+		task_runner_iterate(uptime++, gps_time++, 100);
+		k_sleep(K_TIMEOUT_ABS_MS(iter * MSEC_PER_SEC));
+		iter++;
+	}
+	zassert_equal(2, example_task_run_cnt);
+
+	/* "run" for a few seconds before terminating */
+	example_task_block_timeout = K_SECONDS(3);
+	example_task_run_cnt = 0;
+	for (int i = 0; i < 10; i++) {
+		task_runner_iterate(uptime++, gps_time++, 100);
+		k_sleep(K_TIMEOUT_ABS_MS(iter * MSEC_PER_SEC));
+		iter++;
+	}
+	zassert_equal(2, example_task_run_cnt);
+
+	/* Block until runner requests termination */
+	example_task_block_timeout = K_FOREVER;
+	example_task_expected_block_rc = 1;
+	example_task_run_cnt = 0;
+	for (int i = 0; i < 10; i++) {
+		task_runner_iterate(uptime++, gps_time++, 100);
+		k_sleep(K_TIMEOUT_ABS_MS(iter * MSEC_PER_SEC));
+		iter++;
+	}
+	zassert_equal(2, example_task_run_cnt);
+}
+
+ZTEST(task_runner_runner, test_multi_schedule)
+{
+	struct task_schedule schedules[] = {
+		{
+			.task_id = TASK_ID_SLEEPY,
+			.validity = TASK_VALID_ALWAYS,
+			.periodicity_type = TASK_PERIODICITY_FIXED,
+			.periodicity.fixed.period_s = 5,
+			.timeout_s = 4,
+		},
+		{
+			.task_id = TASK_ID_SLEEPY,
+			.validity = TASK_VALID_ALWAYS,
+			.periodicity_type = TASK_PERIODICITY_FIXED,
+			.periodicity.fixed.period_s = 111111,
+			/* Short timeout, ensure this doesn't happen */
+			.timeout_s = 1,
+		},
+	};
+	struct task_schedule_state states[ARRAY_SIZE(schedules)];
+	uint32_t gps_time = 7000;
+	uint32_t uptime = 0;
+	uint32_t iter = k_uptime_seconds() + 1;
+
+	task_runner_init(schedules, states, ARRAY_SIZE(schedules), task_configs, task_data,
+			 ARRAY_SIZE(task_configs));
+
+	/* "run" for a few seconds before terminating.
+	 * If second schedule terminates first, we would see a different return code
+	 */
+	example_task_block_timeout = K_SECONDS(3);
+	example_task_expected_block_rc = 0;
+	example_task_run_cnt = 0;
+	for (int i = 0; i < 10; i++) {
+		task_runner_iterate(uptime++, gps_time++, 100);
+		k_sleep(K_TIMEOUT_ABS_MS(iter * MSEC_PER_SEC));
+		iter++;
+	}
+	zassert_equal(2, example_task_run_cnt);
+}
+
+static void runner_before(void *fixture)
+{
+	example_task_block_timeout = K_NO_WAIT;
+	example_task_expected_block_rc = 0;
+	example_task_run_cnt = 0;
+}
+
+ZTEST_SUITE(task_runner_runner, NULL, NULL, runner_before, NULL, NULL);
