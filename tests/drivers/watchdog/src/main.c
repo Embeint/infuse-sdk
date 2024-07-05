56366d73a6304c3a3ce114d2f4ae0848544b053e
Jordan Yates <jordan@embeint.com>
2024-07-05 14:47:32 +1000

    tests: drivers: watchdog: test infuse watchdog
    Basic tests for the infuse watchdog implementation.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ tests/drivers/watchdog/src/main.c
@@ -0,0 +1,66 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <string.h>
+
+#include <zephyr/ztest.h>
+#include <zephyr/kernel.h>
+#include <zephyr/random/random.h>
+#include <zephyr/logging/log.h>
+
+#include <infuse/drivers/watchdog.h>
+
+K_SEM_DEFINE(watchdog_expired, 0, 1);
+
+LOG_MODULE_DECLARE(test, LOG_LEVEL_INF);
+
+void infuse_watchdog_expired(const struct device *dev, int channel_id)
+{
+	k_sem_give(&watchdog_expired);
+}
+
+ZTEST(drivers_watchdog, test_watchdog)
+{
+	k_timeout_t feed_period;
+	int channel, rc;
+
+	/* QEMU watchdog only has one timeout channel */
+	channel = infuse_watchdog_install(&feed_period);
+	zassert_equal(0, channel);
+	channel = infuse_watchdog_install(&feed_period);
+	zassert_equal(-ENOMEM, channel);
+	channel = infuse_watchdog_install(&feed_period);
+	zassert_equal(-ENOMEM, channel);
+
+	/* Start watchdog */
+	zassert_equal(0, infuse_watchdog_start());
+
+	/* Try to allocate channel afterwards */
+	channel = infuse_watchdog_install(&feed_period);
+	zassert_equal(-EBUSY, channel);
+
+	for (int i = 0; i < 5; i++) {
+		infuse_watchdog_feed(0);
+		k_sleep(K_SECONDS(1));
+	}
+	/* Failing to call task_runner_iterate should result in a watchdog interrupt */
+	rc = k_sem_take(&watchdog_expired, K_NO_WAIT);
+	zassert_equal(-EBUSY, rc, "Watchdog expired early");
+
+	/* Failing to call infuse_watchdog_feed should result in a watchdog interrupt */
+	rc = k_sem_take(&watchdog_expired, K_MSEC(CONFIG_INFUSE_WATCHDOG_PERIOD_MS + 100));
+	zassert_equal(0, rc, "Watchdog did not expire");
+}
+
+void watchdog_teardown(void *fixture)
+{
+	/* Disable the watchdog to ensure we aren't rebooted */
+	wdt_disable(INFUSE_WATCHDOG_DEV);
+}
+
+ZTEST_SUITE(drivers_watchdog, NULL, NULL, NULL, NULL, watchdog_teardown);
