9d2f4c5886ca85c59799dbbb7b591f5d185ba299
Jordan Yates <jordan@embeint.com>
2024-07-08 15:53:25 +1000

    task_runner: auto_iterate: pull battery from zbus
    Use battery information from zbus instead of hardcoded 100%.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- subsys/task_runner/auto_iterate.c
+++ subsys/task_runner/auto_iterate.c
@@ -7,20 +7,36 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/zbus/zbus.h>
 
 #include <infuse/time/civil.h>
 #include <infuse/task_runner/runner.h>
+#include <infuse/tdf/definitions.h>
+#include <infuse/zbus/channels.h>
 
 static struct k_work_delayable iterate_work;
 extern struct k_work_q task_runner_workq;
+
+INFUSE_ZBUS_CHAN_DECLARE(INFUSE_ZBUS_CHAN_BATTERY);
+
+#define CHAN_BAT INFUSE_ZBUS_CHAN_GET(INFUSE_ZBUS_CHAN_BATTERY)
 
 static void iterate_worker(struct k_work *work)
 {
 	uint32_t gps_time = civil_time_seconds(civil_time_now());
 	uint32_t next_iter = k_uptime_seconds() + 1;
+	uint8_t charge = 0; /* Default charge is 0% until measured */
+
+	/* Get battery charge from zbus */
+	INFUSE_ZBUS_TYPE(INFUSE_ZBUS_CHAN_BATTERY) battery;
+
+	if (zbus_chan_publish_count(CHAN_BAT) > 0) {
+		zbus_chan_read(CHAN_BAT, &battery, K_FOREVER);
+		charge = battery.soc / 100;
+	}
 
 	/* Iterate the runner */
-	task_runner_iterate(k_uptime_seconds(), gps_time, 100);
+	task_runner_iterate(k_uptime_seconds(), gps_time, charge);
 
 	/* Schedule the next iteration */
 	k_work_schedule_for_queue(&task_runner_workq, &iterate_work,
