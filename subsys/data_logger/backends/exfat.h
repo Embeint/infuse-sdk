d44295f2ddd37e795168077f920cfd4c6ec73800
Jordan Yates <jordan@embeint.com>
2024-07-12 14:36:22 +1000

    data_logger: backends: exfat: SD block count
    Update block count macros to compile with `zephyr,sdmmc-disk` disks.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- subsys/data_logger/backends/exfat.h
+++ subsys/data_logger/backends/exfat.h
@@ -23,11 +23,21 @@ extern "C" {
 
 #define DATA_LOGGER_EXFAT_BLOCK_SIZE 512
 
-#define _DL_EXFAT_BLOCKS(disk_node)                                                                \
+#define _DL_EXFAT_BLOCKS_FLASHDISK(disk_node)                                                      \
 	COND_CODE_1(DT_NODE_HAS_COMPAT(disk_node, zephyr_flash_disk),                              \
 		    (DT_REG_SIZE(DT_PROP(disk_node, partition)) / DATA_LOGGER_EXFAT_BLOCK_SIZE),   \
-		    (COND_CODE_1(DT_NODE_HAS_COMPAT(disk_node, zephyr_ram_disk),                   \
-				 (DT_PROP(disk_node, sector_count)), ())))
+		    ())
+
+#define _DL_EXFAT_BLOCKS_RAMDISK(disk_node)                                                        \
+	COND_CODE_1(DT_NODE_HAS_COMPAT(disk_node, zephyr_ram_disk),                                \
+		    (DT_PROP(disk_node, sector_count)), ())
+
+#define _DL_EXFAT_BLOCKS_SDMMC(disk_node)                                                          \
+	COND_CODE_1(DT_NODE_HAS_COMPAT(disk_node, zephyr_sdmmc_disk), (UINT32_MAX), ())
+
+#define _DL_EXFAT_BLOCKS(disk_node)                                                                \
+	(_DL_EXFAT_BLOCKS_FLASHDISK(disk_node) _DL_EXFAT_BLOCKS_RAMDISK(disk_node)                 \
+		 _DL_EXFAT_BLOCKS_SDMMC(disk_node))
 
 #define DATA_LOGGER_BACKEND_CONFIG_EXFAT(node, data_ptr)                                           \
 	{                                                                                          \
