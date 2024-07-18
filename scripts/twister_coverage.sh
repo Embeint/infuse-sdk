6430dc2b64faa896d90ad2d4c4139e4418a4c670
Jordan Yates <jordan@embeint.com>
2024-07-18 11:52:56 +1000

    treewide: HWMv2 updates
    Update platform names to comply to HWMv2.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- scripts/twister_coverage.sh
+++ scripts/twister_coverage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-echo "Twister Coverage - mps2_an385"
+echo "Twister Coverage - mps2/an385"
 ./zephyr/scripts/twister -i --force-color -N -v --filter runnable \
-    -p mps2_an385 --coverage -T infuse-sdk/tests --coverage-tool gcovr \
+    -p mps2/an385 --coverage -T infuse-sdk/tests --coverage-tool gcovr \
     -xCONFIG_TEST_EXTRA_STACK_SIZE=4096 -e nano --coverage-basedir infuse-sdk
