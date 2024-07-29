19fda11ff59364d505144381e8c90aeada068267
Jordan Yates <jordan@embeint.com>
2024-07-29 16:25:05 +1000

    tests: gnss: driver build test
    Add a test to ensure the GNSS driver builds.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ tests/drivers/gnss/src/main.c
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+int main(void)
+{
+	return 0;
+}
