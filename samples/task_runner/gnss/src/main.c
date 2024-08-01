7584aea9172d78349292c9059b3ce030d6f479cd
Jordan Yates <jordan@embeint.com>
2024-08-01 10:54:05 +1000

    samples: task_runner: gnss: added
    Add a sample which runs the GNSS task runner.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ samples/task_runner/gnss/src/main.c
@@ -0,0 +1,75 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/pm/device_runtime.h>
+#include <zephyr/net/conn_mgr_connectivity.h>
+
+#include <infuse/drivers/watchdog.h>
+#include <infuse/time/epoch.h>
+#include <infuse/fs/kv_store.h>
+#include <infuse/fs/kv_types.h>
+#include <infuse/epacket/interface.h>
+#include <infuse/epacket/packet.h>
+#include <infuse/data_logger/high_level/tdf.h>
+#include <infuse/tdf/definitions.h>
+
+#include <infuse/task_runner/runner.h>
+#include <infuse/task_runner/tasks/infuse_tasks.h>
+
+LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
+
+static const struct task_schedule schedules[] = {
+	{
+		.task_id = TASK_ID_GNSS,
+		.validity = TASK_VALID_ALWAYS,
+		.periodicity_type = TASK_PERIODICITY_FIXED,
+		.timeout_s = 115,
+		.periodicity.fixed.period_s = 10,
+		.task_logging =
+			{
+				{
+					.loggers = TDF_DATA_LOGGER_SERIAL,
+					.tdf_mask = TASK_GNSS_LOG_UBX_NAV_PVT,
+				},
+			},
+		.task_args.infuse.gnss =
+			{
+				.flags = TASK_GNSS_FLAGS_PERFORMANCE_MODE |
+					 TASK_GNSS_FLAGS_RUN_FOREVER,
+				.accuracy_m = 5,
+				.position_dop = 40,
+			},
+	},
+};
+struct task_schedule_state states[ARRAY_SIZE(schedules)];
+
+TASK_RUNNER_TASKS_DEFINE(app_tasks, app_tasks_data, (GNSS_TASK, DEVICE_DT_GET(DT_ALIAS(gnss))));
+
+int main(void)
+{
+	/* Start the watchdog */
+	(void)infuse_watchdog_start();
+
+#ifdef CONFIG_NETWORKING
+	conn_mgr_all_if_up(false);
+	conn_mgr_all_if_connect(false);
+#endif /* CONFIG_NETWORKING */
+
+	/* Initialise task runner */
+	task_runner_init(schedules, states, ARRAY_SIZE(schedules), app_tasks, app_tasks_data,
+			 ARRAY_SIZE(app_tasks));
+
+	/* Start auto iteration */
+	task_runner_start_auto_iterate();
+
+	/* No more work to do in this context */
+	k_sleep(K_FOREVER);
+}
