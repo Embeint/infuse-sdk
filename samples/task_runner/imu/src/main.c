8a38334c447c8e0a178850d11014c3c88cb0a9dc
Jordan Yates <jordan@embeint.com>
2024-08-06 17:54:10 +1000

    samples: task_runner: imu: added
    Add a sample for running the IMU task.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ samples/task_runner/imu/src/main.c
@@ -0,0 +1,88 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/pm/device_runtime.h>
+#include <zephyr/net/conn_mgr_connectivity.h>
+
+#include <infuse/drivers/watchdog.h>
+#include <infuse/time/epoch.h>
+#include <infuse/fs/kv_store.h>
+#include <infuse/fs/kv_types.h>
+#include <infuse/epacket/interface.h>
+#include <infuse/epacket/packet.h>
+#include <infuse/data_logger/high_level/tdf.h>
+#include <infuse/tdf/definitions.h>
+
+#include <infuse/task_runner/runner.h>
+#include <infuse/task_runner/tasks/infuse_tasks.h>
+
+LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
+
+#if defined(CONFIG_INFUSE_IMU_BMI270)
+#define IMU_COMPAT bosch_bmi270
+#elif defined(CONFIG_INFUSE_IMU_LSM6DSV)
+#define IMU_COMPAT st_lsm6dsv16x
+#else
+#error No compatibile IMU
+#endif
+
+static const struct task_schedule schedules[] = {
+	{
+		.task_id = TASK_ID_IMU,
+		.validity = TASK_VALID_ALWAYS,
+		.timeout_s = 50,
+		.task_logging =
+			{
+				{
+					.loggers = TDF_DATA_LOGGER_SERIAL,
+					.tdf_mask = TASK_IMU_LOG_ACC | TASK_IMU_LOG_GYR,
+				},
+			},
+		.task_args.infuse.imu =
+			{
+				.accelerometer =
+					{
+						.range_g = 2,
+						.rate_hz = 30,
+					},
+				.gyroscope =
+					{
+						.range_dps = 500,
+						.rate_hz = 15,
+					},
+				.fifo_sample_buffer = 100,
+			},
+	},
+};
+struct task_schedule_state states[ARRAY_SIZE(schedules)];
+
+TASK_RUNNER_TASKS_DEFINE(app_tasks, app_tasks_data, (IMU_TASK, DEVICE_DT_GET_ONE(IMU_COMPAT)));
+
+int main(void)
+{
+	/* Start the watchdog */
+	(void)infuse_watchdog_start();
+
+#ifdef CONFIG_NETWORKING
+	conn_mgr_all_if_up(false);
+	conn_mgr_all_if_connect(false);
+#endif /* CONFIG_NETWORKING */
+
+	/* Initialise task runner */
+	task_runner_init(schedules, states, ARRAY_SIZE(schedules), app_tasks, app_tasks_data,
+			 ARRAY_SIZE(app_tasks));
+
+	/* Start auto iteration */
+	task_runner_start_auto_iterate();
+
+	/* No more work to do in this context */
+	k_sleep(K_FOREVER);
+}
