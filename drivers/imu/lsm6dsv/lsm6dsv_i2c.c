9e4422247d559d72976fa18a14cbec05ee25a0bc
Jordan Yates <jordan@embeint.com>
2024-08-06 17:34:52 +1000

    imu: lsm6dsv driver
    Add a driver for the ST LSM6DSV IMU, reusing the upstream dts
    definitions.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ drivers/imu/lsm6dsv/lsm6dsv_i2c.c
@@ -0,0 +1,42 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <errno.h>
+
+#include <zephyr/device.h>
+
+#include "lsm6dsv.h"
+
+static int lsm6dsv_bus_check_i2c(const union lsm6dsv_bus *bus)
+{
+	return device_is_ready(bus->i2c.bus) ? 0 : -ENODEV;
+}
+
+static int lsm6dsv_reg_read_i2c(const union lsm6dsv_bus *bus, uint8_t start, uint8_t *data,
+				uint16_t len)
+{
+	return i2c_burst_read_dt(&bus->i2c, start, data, len);
+}
+
+static int lsm6dsv_reg_write_i2c(const union lsm6dsv_bus *bus, uint8_t start, const uint8_t *data,
+				 uint16_t len)
+{
+	return i2c_burst_write_dt(&bus->i2c, start, data, len);
+}
+
+static int lsm6dsv_bus_init_i2c(const union lsm6dsv_bus *bus)
+{
+	return 0;
+}
+
+const struct lsm6dsv_bus_io lsm6dsv_bus_io_i2c = {
+	.check = lsm6dsv_bus_check_i2c,
+	.read = lsm6dsv_reg_read_i2c,
+	.write = lsm6dsv_reg_write_i2c,
+	.init = lsm6dsv_bus_init_i2c,
+};
