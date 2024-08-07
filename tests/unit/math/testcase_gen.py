b578916652e56f5d03885392b821629f99bf9ce0
Jordan Yates <jordan@embeint.com>
2024-08-07 11:19:34 +1000

    tests: unit: math: test math functions
    Add tests for the new math functions.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ tests/unit/math/testcase_gen.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import random
+import math
+
+
+def random_int16():
+    return random.randint(-(2**15), 2**15)
+
+
+for _ in range(10):
+    x = random_int16()
+    y = random_int16()
+    mag = math.sqrt((x * x) + (y * y))
+    print(f"zassert_equal({int(mag)}, math_vector_xy_magnitude({x}, {y}));")
+
+for _ in range(10):
+    x = random_int16()
+    y = random_int16()
+    z = random_int16()
+    mag = math.sqrt((x * x) + (y * y) + (z * z))
+    print(f"zassert_equal({int(mag)}, math_vector_xyz_magnitude({x}, {y}, {z}));")
