4f8ebf09889289957a74d87aaf1b084763c59d87
Jordan Yates <jordan@embeint.com>
2024-08-08 14:46:32 +1000

    tests: data_logger: watchdog: added
    Add a test to ensure the watchdog on the offloaded write thread works
    properly.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ tests/subsys/data_logger/watchdog/src/main.c
@@ -0,0 +1,54 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <string.h>
+
+#include <zephyr/ztest.h>
+#include <zephyr/kernel.h>
+#include <zephyr/random/random.h>
+#include <zephyr/logging/log.h>
+
+#include <infuse/drivers/watchdog.h>
+#include <infuse/task_runner/runner.h>
+
+K_SEM_DEFINE(watchdog_expired, 0, 1);
+
+LOG_MODULE_DECLARE(test, LOG_LEVEL_INF);
+
+void infuse_watchdog_expired(const struct device *dev, int channel_id)
+{
+	k_sem_give(&watchdog_expired);
+}
+
+ZTEST(data_logger_watchdog, test_watchdog)
+{
+	extern const k_tid_t logger_commit_thread;
+	int rc;
+
+	/* Start the watchdog */
+	zassert_equal(0, infuse_watchdog_start());
+
+	/* Watchdog shouldn't expire under normal operation */
+	rc = k_sem_take(&watchdog_expired, K_SECONDS(5));
+	zassert_equal(-EAGAIN, rc, "Watchdog expired prematurely");
+
+	/* Block the RPC server thread */
+	k_thread_suspend(logger_commit_thread);
+
+	/* Failing to call task_runner_iterate should result in a watchdog interrupt */
+	rc = k_sem_take(&watchdog_expired, K_MSEC(CONFIG_INFUSE_WATCHDOG_PERIOD_MS + 100));
+	zassert_equal(0, rc, "Watchdog did not expire");
+}
+
+void watchdog_teardown(void *fixture)
+{
+	/* Disable the watchdog to ensure we aren't rebooted */
+	wdt_disable(INFUSE_WATCHDOG_DEV);
+}
+
+ZTEST_SUITE(data_logger_watchdog, NULL, NULL, NULL, NULL, watchdog_teardown);
