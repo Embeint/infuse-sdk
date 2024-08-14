52937253ccb37ac04b806de0dd0869bc8d301d5b
Jordan Yates <jordan@embeint.com>
2024-08-14 17:14:11 +1000

    validation: more tests added
    Added basic validation for a range of hardware peripherals.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ subsys/validation/validation_flash.c
@@ -0,0 +1,128 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <zephyr/pm/device_runtime.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/random/random.h>
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/flash.h>
+#include <infuse/validation/core.h>
+#include <infuse/validation/flash.h>
+
+#define TEST "FLASH"
+
+static uint8_t write_buffer[256];
+static uint8_t read_buffer[256];
+
+static int write_read_erase_page(const struct device *dev, size_t page, size_t page_size)
+{
+	size_t page_offset = sys_rand32_get() % (page_size - sizeof(write_buffer));
+	off_t erase_offset = page * page_size;
+	off_t write_offset = erase_offset + page_offset;
+	int rc;
+
+	VALIDATION_REPORT_INFO(TEST, "Testing address %d.%d (0x%08lX)", page, page_offset,
+			       write_offset);
+
+	/* Fill buffer with random bytes */
+	sys_rand_get(write_buffer, sizeof(write_buffer));
+
+	/* Write the buffer */
+	rc = flash_write(dev, write_offset, write_buffer, sizeof(write_buffer));
+	if (rc < 0) {
+		VALIDATION_REPORT_ERROR(TEST, "flash_write (%d)", rc);
+		return rc;
+	}
+
+	/* Read it back */
+	rc = flash_read(dev, write_offset, read_buffer, sizeof(read_buffer));
+	if (rc < 0) {
+		VALIDATION_REPORT_ERROR(TEST, "flash_read (%d)", rc);
+		return rc;
+	}
+
+	/* Erase the page */
+	rc = flash_erase(dev, erase_offset, page_size);
+	if (rc < 0) {
+		VALIDATION_REPORT_ERROR(TEST, "flash_erase (%d)", rc);
+		return rc;
+	}
+
+	/* Validate written == read */
+	if (memcmp(write_buffer, read_buffer, sizeof(write_buffer)) != 0) {
+		VALIDATION_REPORT_ERROR(TEST, "Data read != data written");
+		return rc;
+	}
+
+	VALIDATION_REPORT_INFO(TEST, "Write-Read-Erase test passed");
+	return 0;
+}
+
+int infuse_validation_flash(const struct device *dev, uint8_t flags)
+{
+	struct flash_pages_info info;
+	size_t page_count;
+	int rc;
+
+	VALIDATION_REPORT_INFO(TEST, "DEV=%s", dev->name);
+
+	/* Check init succeeded */
+	if (!device_is_ready(dev)) {
+		VALIDATION_REPORT_ERROR(TEST, "Device not ready");
+		rc = -ENODEV;
+		goto test_end;
+	}
+
+	/* Power up device */
+	rc = pm_device_runtime_get(dev);
+	if (rc < 0) {
+		VALIDATION_REPORT_ERROR(TEST, "pm_device_runtime_get (%d)", rc);
+		goto test_end;
+	}
+
+	if (flash_get_page_info_by_idx(dev, 0, &info) < 0) {
+		VALIDATION_REPORT_ERROR(TEST, "flash_get_page_info_by_idx (%d)", rc);
+		goto test_end;
+	}
+	page_count = flash_get_page_count(dev);
+	VALIDATION_REPORT_INFO(TEST, "Page Size: %d", info.size);
+	VALIDATION_REPORT_INFO(TEST, " Page Cnt: %d", page_count);
+
+	if (flags & VALIDATION_FLASH_DRIVER) {
+		/* Pick a random page in the back half of the flash */
+		size_t page = (sys_rand32_get() % (page_count / 2)) + (page_count / 2);
+
+		rc = write_read_erase_page(dev, page, info.size);
+	}
+
+	if ((rc == 0) && (flags & VALIDATION_FLASH_CHIP_ERASE)) {
+		VALIDATION_REPORT_INFO(TEST, "Erasing entire chip");
+
+		rc = flash_erase(dev, 0, info.size * page_count);
+		if (rc == 0) {
+			VALIDATION_REPORT_INFO(TEST, "Chip erase complete");
+		} else {
+			VALIDATION_REPORT_ERROR(TEST, "Chip erase failed (%d)", rc);
+		}
+	}
+
+	/* Power down device */
+	if (pm_device_runtime_put(dev) < 0) {
+		if (rc == 0) {
+			VALIDATION_REPORT_ERROR(TEST, "pm_device_runtime_put");
+			rc = -EIO;
+		}
+	}
+test_end:
+	if (rc == 0) {
+		VALIDATION_REPORT_PASS(TEST, "DEV=%s", dev->name);
+	}
+
+	return rc;
+}
---
+++ subsys/validation/validation_nrf_modem.c
@@ -0,0 +1,115 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <infuse/validation/core.h>
+#include <infuse/validation/nrf_modem.h>
+
+#include <modem/nrf_modem_lib.h>
+#include <modem/lte_lc.h>
+#include <modem/modem_info.h>
+#include <nrf_modem_at.h>
+
+#define TEST "MODEM"
+
+static int infuse_modem_info(void)
+{
+	char storage[64];
+	uint64_t imei;
+
+	/* Model identifier */
+	if (nrf_modem_at_scanf("AT+CGMM", "%64s\n", storage) != 1) {
+		VALIDATION_REPORT_ERROR(TEST, "Failed to read model identifier");
+		return -EIO;
+	}
+	VALIDATION_REPORT_INFO(TEST, "%16s: %s", "Modem Model", storage);
+
+	/* Modem ESN */
+	if (nrf_modem_at_scanf("AT+CGSN=0", "%64s\n", storage) != 1) {
+		VALIDATION_REPORT_ERROR(TEST, "Failed to read ESN");
+		return -EIO;
+	}
+	VALIDATION_REPORT_INFO(TEST, "%16s: %s", "Modem ESN", storage);
+
+	/* Modem IMEI */
+	if (nrf_modem_at_scanf("AT+CGSN=1", "+CGSN: \"%" SCNd64 "\"\n", &imei) != 1) {
+		VALIDATION_REPORT_ERROR(TEST, "Failed to read IMEI");
+		return -EIO;
+	}
+	VALIDATION_REPORT_INFO(TEST, "%16s: %s", "Modem IMEI", storage);
+	/* Modem firmware revision */
+	if (nrf_modem_at_scanf("AT+CGMR", "%64s\n", storage) != 1) {
+		VALIDATION_REPORT_ERROR(TEST, "Failed to read firmware version");
+		return -EIO;
+	}
+	VALIDATION_REPORT_INFO(TEST, "%16s: %s", "Firmware Version", storage);
+
+	return 0;
+}
+
+static int infuse_sim_card(void)
+{
+	char response[64];
+
+	/* Power up SIM card */
+	if (nrf_modem_at_cmd(response, sizeof(response), "AT+CFUN=41") != 0) {
+		VALIDATION_REPORT_ERROR(TEST, "Failed to activate UICC");
+		return -EIO;
+	}
+	k_sleep(K_SECONDS(1));
+
+	if (nrf_modem_at_scanf("AT+CIMI", "%64s\n", response) != 1) {
+		VALIDATION_REPORT_ERROR(TEST, "Failed to read IMSI");
+	}
+	VALIDATION_REPORT_INFO(TEST, "%16s: %s", "IMSI", response);
+	if (nrf_modem_at_scanf("AT%XICCID", "%%XICCID: %64s\n", response) != 1) {
+		VALIDATION_REPORT_ERROR(TEST, "Failed to read ICCID");
+	}
+	VALIDATION_REPORT_INFO(TEST, "%16s: %s", "ICCID", response);
+
+	/* Power down SIM card */
+	if (nrf_modem_at_cmd(response, sizeof(response), "AT+CFUN=40") != 0) {
+		VALIDATION_REPORT_ERROR(TEST, "Failed to deactivate UICC");
+		return -EIO;
+	}
+	return 0;
+}
+
+int infuse_validation_nrf_modem(uint8_t flags)
+{
+	int rc;
+
+	VALIDATION_REPORT_INFO(TEST, "Starting");
+
+	rc = nrf_modem_lib_init();
+	if (rc != 0) {
+		VALIDATION_REPORT_ERROR(TEST, "Failed to initialise modem (%d)", rc);
+		goto test_end;
+	}
+
+	rc = infuse_modem_info();
+	if (rc < 0) {
+		goto test_end;
+	}
+
+	if (flags & VALIDATION_NRF_MODEM_SIM_CARD) {
+		rc = infuse_sim_card();
+	}
+
+	if (nrf_modem_lib_shutdown() != 0) {
+		VALIDATION_REPORT_ERROR(TEST, "Failed to shutdown modem");
+		if (rc == 0) {
+			rc = -EIO;
+		}
+	};
+
+test_end:
+	if (rc == 0) {
+		VALIDATION_REPORT_PASS(TEST, "PASSED");
+	}
+	return rc;
+}
