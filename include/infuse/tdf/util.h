4e959e5c71b2a117f152969f4669d10807870571
Jordan Yates <jordan@embeint.com>
2024-08-28 20:09:17 +1000

    tdf: util: `REBOOT_INFO` utility functions
    Add utility functions to populate and log reboot events.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- include/infuse/tdf/util.h
+++ include/infuse/tdf/util.h
@@ -13,8 +13,15 @@
 #define INFUSE_SDK_INCLUDE_INFUSE_TDF_UTIL_H_
 
 #include <stdint.h>
+#include <string.h>
 
+#include <infuse/data_logger/high_level/tdf.h>
 #include <infuse/tdf/definitions.h>
+#include <infuse/reboot.h>
+#include <infuse/common_boot.h>
+#include <infuse/time/epoch.h>
+#include <infuse/fs/kv_store.h>
+#include <infuse/fs/kv_types.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -67,6 +74,53 @@ static inline uint16_t tdf_id_from_gyroscope_range(uint16_t range)
 	default:
 		return TDF_GYR_2000DPS;
 	}
+}
+
+/**
+ * @brief Populate a REBOOT_INFO TDF from Infuse-IoT reboot state
+ *
+ * @param state Infuse-IoT reboot state pointer
+ * @param info REBOOT_INFO TDF pointer
+ */
+static inline void tdf_reboot_info_from_state(struct infuse_reboot_state *state,
+					      struct tdf_reboot_info *info)
+{
+	KV_KEY_TYPE(KV_KEY_REBOOTS) reboot = {0};
+
+#ifdef CONFIG_KV_STORE
+	KV_STORE_READ(KV_KEY_REBOOTS, &reboot);
+	info->count = reboot.count;
+#endif
+
+	info->reason = state->reason;
+	info->hardware_flags = state->hardware_reason;
+	info->uptime = state->uptime;
+	info->count = reboot.count;
+	info->param_1 = state->param_1.program_counter;
+	info->param_2 = state->param_2.link_register;
+	strncpy(info->thread, state->thread_name, sizeof(info->thread));
+}
+
+/**
+ * @brief Log REBOOT_INFO TDF to specified TDF data loggers
+ *
+ * TDF is populated from @ref infuse_common_boot_last_reboot.
+ *
+ * @param logger_mask TDF data loggers to write to
+ */
+static inline void tdf_reboot_info_log(uint8_t logger_mask)
+{
+#ifdef CONFIG_TDF_UTIL_REBOOT_INFO_LOG
+	struct infuse_reboot_state reboot_state;
+	struct tdf_reboot_info reboot_info;
+	uint64_t t = epoch_time_from_ticks(0);
+
+	/* Construct reboot info TDF */
+	infuse_common_boot_last_reboot(&reboot_state);
+	tdf_reboot_info_from_state(&reboot_state, &reboot_info);
+	/* Push TDF at logger */
+	tdf_data_logger_log(logger_mask, TDF_REBOOT_INFO, sizeof(reboot_info), t, &reboot_info);
+#endif /* CONFIG_TDF_UTIL_REBOOT_INFO_LOG */
 }
 
 /**
