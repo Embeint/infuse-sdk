9368b75e24a67f379038b23f4d9fc2d44112be23
Jordan Yates <jordan@embeint.com>
2024-08-29 11:02:30 +1000

    tests: lib: auto: time_sync_log: test reboot logging
    Add tests for the behaviour of the `AUTO_TIME_SYNC_LOG_REBOOT_ON_SYNC`
    flag.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- tests/lib/auto/time_sync_log/src/main.c
+++ tests/lib/auto/time_sync_log/src/main.c
@@ -7,6 +7,7 @@
  */
 
 #include <stdint.h>
+#include <string.h>
 
 #include <zephyr/ztest.h>
 #include <zephyr/kernel.h>
@@ -19,6 +20,14 @@
 #include <infuse/tdf/tdf.h>
 #include <infuse/time/epoch.h>
 #include <infuse/types.h>
+#include <infuse/reboot.h>
+
+int infuse_common_boot_last_reboot(struct infuse_reboot_state *state)
+{
+	memset(state, 0x00, sizeof(*state));
+	state->reason = INFUSE_REBOOT_UNKNOWN;
+	return 0;
+}
 
 static void expect_sync(struct k_fifo *sent_queue, uint8_t source, int32_t shift)
 {
@@ -33,7 +42,7 @@ static void expect_sync(struct k_fifo *sent_queue, uint8_t source, int32_t shift
 	frame = (void *)buf->data;
 	zassert_equal(INFUSE_TDF, frame->type);
 
-	/* Single TDF of type TDF_TIME_SYNC, jump forward 1 second */
+	/* Single TDF of type TDF_TIME_SYNC */
 	tdf_parse_start(&state, frame->payload, buf->len - sizeof(*frame));
 	zassert_equal(0, tdf_parse(&state, &parsed));
 	zassert_equal(TDF_TIME_SYNC, parsed.tdf_id);
@@ -44,6 +53,32 @@ static void expect_sync(struct k_fifo *sent_queue, uint8_t source, int32_t shift
 	zassert_equal(-ENOMEM, tdf_parse(&state, &parsed));
 
 	net_buf_unref(buf);
+}
+
+static void expect_reboot(struct k_fifo *sent_queue, bool expect)
+{
+	struct epacket_dummy_frame *frame;
+	struct tdf_buffer_state state;
+	struct tdf_parsed parsed;
+	struct net_buf *buf;
+
+	buf = net_buf_get(sent_queue, K_MSEC(1));
+	if (expect) {
+		zassert_not_null(buf);
+		frame = (void *)buf->data;
+		zassert_equal(INFUSE_TDF, frame->type);
+
+		/* Single TDF of type TDF_REBOOT_INFO*/
+		tdf_parse_start(&state, frame->payload, buf->len - sizeof(*frame));
+		zassert_equal(0, tdf_parse(&state, &parsed));
+		zassert_equal(TDF_REBOOT_INFO, parsed.tdf_id);
+		zassert_equal(1, parsed.tdf_num);
+		zassert_equal(-ENOMEM, tdf_parse(&state, &parsed));
+		net_buf_unref(buf);
+
+	} else {
+		zassert_is_null(buf);
+	}
 }
 
 ZTEST(time_sync_log, test_auto_log)
@@ -77,4 +112,58 @@ ZTEST(time_sync_log, test_auto_log)
 	expect_sync(sent_queue, TIME_SOURCE_NTP, (-3 * (int32_t)USEC_PER_SEC) / 2);
 }
 
-ZTEST_SUITE(time_sync_log, NULL, NULL, NULL, NULL, NULL);
+ZTEST(time_sync_log, test_reboot_log)
+{
+	struct k_fifo *sent_queue = epacket_dummmy_transmit_fifo_get();
+	struct timeutil_sync_instant reference = {
+		.local = 10 * CONFIG_SYS_CLOCK_TICKS_PER_SEC,
+		.ref = 100 * INFUSE_EPOCH_TIME_TICKS_PER_SEC,
+	};
+
+	/* Configure automatic logging */
+	auto_time_sync_log_configure(TDF_DATA_LOGGER_SERIAL, AUTO_TIME_SYNC_LOG_REBOOT_ON_SYNC);
+	tdf_data_logger_flush(TDF_DATA_LOGGER_SERIAL);
+	expect_reboot(sent_queue, false);
+
+	/* Update reference time, expect the REBOOT_INFO TDF */
+	zassert_equal(0, epoch_time_set_reference(TIME_SOURCE_GNSS, &reference));
+	tdf_data_logger_flush(TDF_DATA_LOGGER_SERIAL);
+	expect_reboot(sent_queue, true);
+
+	/* Second update should not result in a log */
+	reference.ref += INFUSE_EPOCH_TIME_TICKS_PER_SEC;
+	zassert_equal(0, epoch_time_set_reference(TIME_SOURCE_GNSS, &reference));
+	tdf_data_logger_flush(TDF_DATA_LOGGER_SERIAL);
+	expect_reboot(sent_queue, false);
+}
+
+ZTEST(time_sync_log, test_reboot_log_time_known)
+{
+	struct k_fifo *sent_queue = epacket_dummmy_transmit_fifo_get();
+	struct timeutil_sync_instant reference = {
+		.local = 10 * CONFIG_SYS_CLOCK_TICKS_PER_SEC,
+		.ref = 100 * INFUSE_EPOCH_TIME_TICKS_PER_SEC,
+	};
+
+	/* Time recovered on boot */
+	zassert_equal(
+		0, epoch_time_set_reference(TIME_SOURCE_GNSS | TIME_SOURCE_RECOVERED, &reference));
+
+	/* Configure automatic logging */
+	auto_time_sync_log_configure(TDF_DATA_LOGGER_SERIAL, AUTO_TIME_SYNC_LOG_REBOOT_ON_SYNC);
+	tdf_data_logger_flush(TDF_DATA_LOGGER_SERIAL);
+	expect_reboot(sent_queue, false);
+
+	/* Update should not result in a log */
+	reference.ref += INFUSE_EPOCH_TIME_TICKS_PER_SEC;
+	zassert_equal(0, epoch_time_set_reference(TIME_SOURCE_GNSS, &reference));
+	tdf_data_logger_flush(TDF_DATA_LOGGER_SERIAL);
+	expect_reboot(sent_queue, false);
+}
+
+void test_init(void *fixture)
+{
+	epoch_time_reset();
+}
+
+ZTEST_SUITE(time_sync_log, NULL, NULL, test_init, NULL, NULL);
