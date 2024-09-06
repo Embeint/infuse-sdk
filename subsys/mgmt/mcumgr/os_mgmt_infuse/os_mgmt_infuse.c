c72c36d064ae1669de7a0ee6404facf85cc99858
Jordan Yates <jordan@embeint.com>
2024-09-06 16:51:40 +1000

    mgmt: mcumgr: add `OS_MGMT_ID_BOOTLOADER_INFO`
    Add an implementation for `OS_MGMT_ID_BOOTLOADER_INFO`.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- subsys/mgmt/mcumgr/os_mgmt_infuse/os_mgmt_infuse.c
+++ subsys/mgmt/mcumgr/os_mgmt_infuse/os_mgmt_infuse.c
@@ -86,11 +86,82 @@ static int os_mgmt_mcumgr_params(struct smp_streamer *ctxt)
 	return ok ? MGMT_ERR_EOK : MGMT_ERR_EMSGSIZE;
 }
 
+#if defined(CONFIG_TFM_BL2)
+/* MCUBOOT_UPGRADE_STRATEGY from TF-M, defaults to "OVERWRITE_ONLY" */
+#define BOOTLOADER_MODE 2
+#elif defined(CONFIG_BOOTLOADER_MCUBOOT)
+#include <bootutil/boot_status.h>
+/* From zephyr/subsys/mgmt/mcumgr/grp/os_mgmt/src/os_mgmt.c */
+#if defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_SINGLE_APP)
+#define BOOTLOADER_MODE MCUBOOT_MODE_SINGLE_SLOT
+#elif defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_SWAP_SCRATCH)
+#define BOOTLOADER_MODE MCUBOOT_MODE_SWAP_USING_SCRATCH
+#elif defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_OVERWRITE_ONLY)
+#define BOOTLOADER_MODE MCUBOOT_MODE_UPGRADE_ONLY
+#elif defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_SWAP_WITHOUT_SCRATCH)
+#define BOOTLOADER_MODE MCUBOOT_MODE_SWAP_USING_MOVE
+#elif defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_DIRECT_XIP)
+#define BOOTLOADER_MODE MCUBOOT_MODE_DIRECT_XIP
+#elif defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_DIRECT_XIP_WITH_REVERT)
+#define BOOTLOADER_MODE MCUBOOT_MODE_DIRECT_XIP_WITH_REVERT
+#elif defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_FIRMWARE_UPDATER)
+#define BOOTLOADER_MODE MCUBOOT_MODE_FIRMWARE_LOADER
+#else
+#define BOOTLOADER_MODE -1
+#endif
+#else
+#define BOOTLOADER_MODE -1
+#endif
+
+#if defined(CONFIG_TFM_BL2)
+/* MCUBOOT_HW_ROLLBACK_PROT from TF-M, defaults ON, not exposed to Zephyr build */
+#define NO_DOWNGRADE
+#elif defined(CONFIG_MCUBOOT_BOOTLOADER_NO_DOWNGRADE)
+#define NO_DOWNGRADE
+#endif
+
+static int os_mgmt_bootloader_info(struct smp_streamer *ctxt)
+{
+	zcbor_state_t *zse = ctxt->writer->zs;
+	zcbor_state_t *zsd = ctxt->reader->zs;
+	struct zcbor_string query = {0};
+	size_t decoded;
+	bool ok;
+
+	struct zcbor_map_decode_key_val bootloader_info[] = {
+		ZCBOR_MAP_DECODE_KEY_DECODER("query", zcbor_tstr_decode, &query),
+	};
+
+	if (zcbor_map_decode_bulk(zsd, bootloader_info, ARRAY_SIZE(bootloader_info), &decoded)) {
+		return MGMT_ERR_EINVAL;
+	}
+
+	/* If no parameter is recognized then just introduce the bootloader. */
+	if (decoded == 0) {
+		ok = zcbor_tstr_put_lit(zse, "bootloader") && zcbor_tstr_put_lit(zse, "MCUboot");
+	} else if (zcbor_map_decode_bulk_key_found(bootloader_info, ARRAY_SIZE(bootloader_info),
+						   "query") &&
+		   (sizeof("mode") - 1) == query.len &&
+		   memcmp("mode", query.value, query.len) == 0) {
+
+		ok = zcbor_tstr_put_lit(zse, "mode") && zcbor_int32_put(zse, BOOTLOADER_MODE);
+#ifdef NO_DOWNGRADE
+		ok = ok && zcbor_tstr_put_lit(zse, "no-downgrade") &&
+		     zcbor_bool_encode(zse, &(bool){true});
+#endif
+	} else {
+		return OS_MGMT_ERR_QUERY_YIELDS_NO_ANSWER;
+	}
+
+	return ok ? MGMT_ERR_EOK : MGMT_ERR_EMSGSIZE;
+}
+
 static const struct mgmt_handler os_mgmt_group_handlers[] = {
 	[OS_MGMT_ID_ECHO] = {os_mgmt_echo, os_mgmt_echo},
 	[OS_MGMT_ID_DATETIME_STR] = {os_mgmt_datetime_read, NULL},
 	[OS_MGMT_ID_RESET] = {NULL, os_mgmt_reset},
 	[OS_MGMT_ID_MCUMGR_PARAMS] = {os_mgmt_mcumgr_params, NULL},
+	[OS_MGMT_ID_BOOTLOADER_INFO] = {os_mgmt_bootloader_info, NULL},
 };
 
 static struct mgmt_group os_mgmt_group = {
