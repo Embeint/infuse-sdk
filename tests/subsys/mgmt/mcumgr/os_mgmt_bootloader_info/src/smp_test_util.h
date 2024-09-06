378b7b346705e027cd0c93b23acb2459ee4bb8cc
Jordan Yates <jordan@embeint.com>
2024-09-06 17:00:45 +1000

    tests: mgmt: mcumgr: test `OS_MGMT_ID_BOOTLOADER_INFO`
    Add tests for the `OS_MGMT_ID_BOOTLOADER_INFO` implementation.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ tests/subsys/mgmt/mcumgr/os_mgmt_bootloader_info/src/smp_test_util.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ * Copyright (c) 2023 Jamie M.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef H_SMP_TEST_UTIL_
+#define H_SMP_TEST_UTIL_
+
+#include <zephyr/ztest.h>
+#include <zephyr/mgmt/mcumgr/mgmt/mgmt.h>
+#include <zcbor_common.h>
+#include <smp_internal.h>
+
+/* SMP header function for generating os_mgmt bootloader_info command header with sequence number
+ * set to 1
+ */
+void smp_make_hdr(struct smp_hdr *rsp_hdr, size_t len, bool version2, bool write);
+
+/* Function for creating an os_mgmt bootloader_info get command */
+bool create_mcumgr_bootloader_info_get_packet(zcbor_state_t *zse, bool version2, uint8_t *buffer,
+					      uint8_t *output_buffer, uint16_t *buffer_size,
+					      char *query);
+
+#endif
