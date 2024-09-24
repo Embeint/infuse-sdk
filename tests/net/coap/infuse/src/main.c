127f0d77bf68b5e7addcb8d116562f883f00515a
Jordan Yates <jordan@embeint.com>
2024-09-24 12:37:29 +1000

    tests: net: coap: infuse tests added
    Add tests for downloads from the Infuse-IoT COAP server with DTLS.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ tests/net/coap/infuse/src/main.c
@@ -0,0 +1,176 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <time.h>
+
+#include <zephyr/ztest.h>
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/conn_mgr_monitor.h>
+#include <zephyr/net/conn_mgr_connectivity.h>
+#include <zephyr/net/tls_credentials.h>
+#include <zephyr/net/socket.h>
+
+#include <infuse/fs/kv_store.h>
+#include <infuse/fs/kv_types.h>
+#include <infuse/net/coap.h>
+#include <infuse/net/dns.h>
+
+#ifdef CONFIG_WIFI
+#define IF_DELAY K_SECONDS(20)
+#else
+#define IF_DELAY K_SECONDS(5)
+#endif
+
+K_SEM_DEFINE(l4_up, 0, 1);
+
+const char *coap_test_server = "coap.dev.infuse-iot.com";
+
+/* Hardcoded authentication that only has access to test files */
+const uint8_t test_identity[8] = {0xfd, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
+const uint8_t test_psk[32] = {0xfd, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+			      0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+			      0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
+uint8_t work_area[2048];
+
+#define TLS_TAG_INFUSE_COAP 1
+
+static const sec_tag_t sec_tls_tags[] = {
+	TLS_TAG_INFUSE_COAP,
+};
+
+struct cb_ctx {
+	const char *expected_data;
+	uint32_t expected_offset;
+	uint32_t cb_count;
+};
+
+static void l4_event_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event,
+			     struct net_if *iface)
+{
+	ARG_UNUSED(iface);
+	ARG_UNUSED(cb);
+
+	if (mgmt_event == NET_EVENT_L4_CONNECTED) {
+		k_sem_give(&l4_up);
+	}
+}
+
+void data_cb(uint32_t offset, const uint8_t *data, uint16_t data_len, void *context)
+{
+	struct cb_ctx *ctx = context;
+
+	zassert_equal(ctx->expected_offset, offset);
+	if (ctx->expected_data) {
+		zassert_mem_equal(ctx->expected_data + offset, data, data_len);
+	}
+	ctx->expected_offset += data_len;
+	ctx->cb_count += 1;
+}
+
+static int socket_setup(void)
+{
+	struct sockaddr address;
+	socklen_t address_len;
+	int sock;
+	int rc;
+
+	/* Add credentials to system */
+	zassert_equal(0, tls_credential_add(TLS_TAG_INFUSE_COAP, TLS_CREDENTIAL_PSK_ID,
+					    test_identity, sizeof(test_identity)));
+	zassert_equal(0, tls_credential_add(TLS_TAG_INFUSE_COAP, TLS_CREDENTIAL_PSK, test_psk,
+					    sizeof(test_psk)));
+
+	/* IPv4 lookup */
+	zassert_equal(0, infuse_sync_dns(coap_test_server, 5684, AF_INET, SOCK_STREAM, &address,
+					 &address_len));
+
+	/* Create socket */
+	sock = zsock_socket(address.sa_family, SOCK_DGRAM, IPPROTO_DTLS_1_2);
+	zassert_true(sock >= 0);
+
+	/* Assign DTLS security tags */
+	zassert_equal(0, zsock_setsockopt(sock, SOL_TLS, TLS_SEC_TAG_LIST, sec_tls_tags,
+					  sizeof(sec_tls_tags)));
+
+	/* Complete DTLS handshake */
+	rc = zsock_connect(sock, &address, address_len);
+	if (rc != 0) {
+		zsock_close(sock);
+	}
+	zassert_equal(0, rc);
+	return sock;
+}
+
+ZTEST(infuse_coap, test_download)
+{
+	struct cb_ctx context;
+	int sock;
+	int rc;
+
+	/* Wait for the interface to come up */
+	zassert_equal(0, k_sem_take(&l4_up, IF_DELAY));
+
+	/* Open socket */
+	sock = socket_setup();
+
+	/* Short retrieval: file/small_file -> hello_world\n */
+	context.expected_data = "hello_world\n";
+	context.expected_offset = 0;
+	context.cb_count = 0;
+	rc = infuse_coap_download(sock, "file/small_file", data_cb, &context, work_area, 128, 1000);
+	zassert_equal(strlen(context.expected_data), rc);
+	zassert_equal(1, context.cb_count);
+
+	/* Medium retrieval: file/med_file -> 10030 bytes == 10 packets at 1024 block size */
+	context.expected_data = NULL;
+	context.expected_offset = 0;
+	context.cb_count = 0;
+	rc = infuse_coap_download(sock, "file/med_file", data_cb, &context, work_area,
+				  sizeof(work_area), 1000);
+	zassert_equal(10030, rc);
+	zassert_equal(10, context.cb_count);
+
+	/* Close socket */
+	zassert_equal(0, zsock_close(sock));
+	k_sem_give(&l4_up);
+}
+
+void *test_init(void)
+{
+	static struct net_mgmt_event_callback mgmt_cb;
+
+#ifdef CONFIG_WIFI
+	KV_STRING_CONST(ssid, CONFIG_WIFI_SSID);
+	KV_STRING_CONST(psk, CONFIG_WIFI_PSK);
+
+	kv_store_write(KV_KEY_WIFI_SSID, &ssid, sizeof(ssid));
+	kv_store_write(KV_KEY_WIFI_PSK, &psk, sizeof(psk));
+#endif /* CONFIG_WIFI */
+
+	if (IS_ENABLED(CONFIG_NET_CONNECTION_MANAGER)) {
+		net_mgmt_init_event_callback(&mgmt_cb, l4_event_handler, NET_EVENT_L4_CONNECTED);
+		net_mgmt_add_event_callback(&mgmt_cb);
+	}
+
+#ifdef CONFIG_NET_NATIVE_OFFLOADED_SOCKETS
+	struct net_if *iface = net_if_get_default();
+	struct in_addr addr;
+
+	/* Add the IP address to trigger NET_EVENT_L4_CONNECTED */
+	net_addr_pton(AF_INET, "192.0.2.1", &addr);
+	net_if_ipv4_addr_add(iface, &addr, NET_ADDR_MANUAL, 0);
+#else
+	/* Turn on all interfaces */
+	conn_mgr_all_if_up(false);
+	conn_mgr_all_if_connect(false);
+#endif /* CONFIG_NET_NATIVE_OFFLOADED_SOCKETS */
+
+	return NULL;
+}
+
+ZTEST_SUITE(infuse_coap, NULL, test_init, NULL, NULL, NULL);
