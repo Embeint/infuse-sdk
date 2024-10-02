5baf01cc8af8f23b3091a8ecffb48112959cf924
Jordan Yates <jordan@embeint.com>
2024-10-02 21:34:29 +1000

    gnss: ubx: extract common code
    Extract code that does not depend on M10 or I2C to a common file.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- drivers/gnss/gnss_u_blox_m10_i2c.c
+++ drivers/gnss/gnss_u_blox_m10_i2c.c
@@ -8,12 +8,13 @@
  * Uses the NAV-PVT message to fulfill the requirements of the GNSS API.
  */
 
+#define DT_DRV_COMPAT u_blox_m10_i2c
+
 #include <zephyr/device.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/drivers/i2c.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/gnss.h>
-#include <zephyr/drivers/gnss/gnss_publish.h>
 #include <zephyr/pm/device.h>
 #include <zephyr/pm/device_runtime.h>
 #include <zephyr/sys/byteorder.h>
@@ -23,41 +24,20 @@
 #include <infuse/gnss/ubx/cfg.h>
 #include <infuse/gnss/ubx/modem.h>
 #include <infuse/gnss/ubx/protocol.h>
-#include <infuse/gnss/ubx/zephyr.h>
 
-#define DT_DRV_COMPAT u_blox_m10_i2c
+#include "u_blox_common.h"
 
 #define SYNC_MESSAGE_TIMEOUT K_MSEC(250)
-#define MIN_TIMEPULSE_EDGES  3
 
 struct ubx_m10_i2c_config {
+	struct ubx_common_config common;
 	struct i2c_dt_spec i2c;
-	struct gpio_dt_spec reset_gpio;
-	struct gpio_dt_spec extint_gpio;
-	struct gpio_dt_spec timepulse_gpio;
-	struct gpio_dt_spec data_ready_gpio;
-	uint8_t data_ready_pio;
 };
 
 struct ubx_m10_i2c_data {
-	/* UBX modem data */
-	struct ubx_modem_data modem;
+	struct ubx_common_data common;
 	/* I2C modem backend */
 	struct modem_backend_ublox_i2c i2c_backend;
-	/* Wake time */
-	k_timeout_t min_wake_time;
-	/* Callback for data ready GPIO */
-	struct gpio_callback timepulse_cb;
-	/* Timestamp of the latest timepulse */
-	k_ticks_t latest_timepulse;
-#ifndef CONFIG_GNSS_U_BLOX_NO_API_COMPAT
-	/* NAV-PVT message handler */
-	struct ubx_message_handler_ctx pvt_handler;
-#ifdef CONFIG_GNSS_SATELLITES
-	/* NAV-SAT message handler */
-	struct ubx_message_handler_ctx sat_handler;
-#endif /* CONFIG_GNSS_SATELLITES */
-#endif /* CONFIG_GNSS_U_BLOX_NO_API_COMPAT */
 };
 
 BUILD_ASSERT(__alignof(struct ubx_frame) == 1);
@@ -65,86 +45,6 @@ BUILD_ASSERT(__alignof(struct ubx_frame) == 1);
 LOG_MODULE_DECLARE(ubx_modem);
 
 #ifndef CONFIG_GNSS_U_BLOX_NO_API_COMPAT
-
-static int nav_pvt_cb(uint8_t message_class, uint8_t message_id, const void *payload,
-		      size_t payload_len, void *user_data)
-{
-	const struct device *dev = user_data;
-	const struct ubx_msg_nav_pvt *pvt = payload;
-	/* Translate to GNSS API structure */
-	struct gnss_data data = {
-		.nav_data =
-			{
-				.latitude = ((int64_t)pvt->lat) * 100,
-				.longitude = ((int64_t)pvt->lon) * 100,
-				.bearing = pvt->head_mot / 10,
-				.speed = pvt->g_speed,
-				.altitude = pvt->height,
-			},
-		.info =
-			{
-				.satellites_cnt = pvt->num_sv,
-				.hdop = pvt->p_dop * 10,
-				.fix_status = ubx_nav_pvt_to_fix_status(pvt),
-				.fix_quality = ubx_nav_pvt_to_fix_quality(pvt),
-			},
-		.utc =
-			{
-				.century_year = pvt->year % 100,
-				.month = pvt->month,
-				.month_day = pvt->day,
-				.hour = pvt->hour,
-				.minute = pvt->min,
-				.millisecond = (1000 * (uint16_t)pvt->sec) + (pvt->nano / 1000000),
-			},
-	};
-	/* Push data to compile-time consumers */
-	gnss_publish_data(dev, &data);
-	return 0;
-}
-
-#ifdef CONFIG_GNSS_SATELLITES
-
-static int nav_sat_cb(uint8_t message_class, uint8_t message_id, const void *payload,
-		      size_t payload_len, void *user_data)
-{
-	const struct device *dev = user_data;
-	const struct ubx_msg_nav_sat *sat = payload;
-	const struct ubx_msg_nav_sat_sv *sv;
-	struct gnss_satellite satellites[CONFIG_GNSS_U_BLOX_SATELLITES_COUNT];
-	uint8_t num_report = 0;
-	uint32_t sv_quality;
-	bool tracked;
-
-	for (int i = 0; i < sat->num_svs; i++) {
-		sv = &sat->svs[i];
-		sv_quality = sv->flags & UBX_MSG_NAV_SAT_FLAGS_QUALITY_IND_MASK;
-		tracked = (sv_quality == UBX_MSG_NAV_SAT_FLAGS_QUALITY_IND_ACQUIRED) ||
-			  (sv_quality >= UBX_MSG_NAV_SAT_FLAGS_QUALITY_IND_CODE_LOCKED);
-
-		LOG_DBG("\t%7s ID:%3d Qual: %d CNo: %2ddBHz Elev: %3ddeg Azim: %3ddeg %08X",
-			ubx_gnss_id_name(sv->gnss_id), sv->sv_id,
-			sv->flags & UBX_MSG_NAV_SAT_FLAGS_QUALITY_IND_MASK, sv->cno, sv->elev,
-			sv->azim, sv->flags);
-
-		if (num_report >= ARRAY_SIZE(satellites)) {
-			continue;
-		}
-		/* Untracked satellites already skipped */
-		satellites[num_report].system = ubx_gnss_id_to_gnss_system(sv->gnss_id);
-		satellites[num_report].prn = sv->sv_id;
-		satellites[num_report].snr = sv->cno;
-		satellites[num_report].azimuth = sv->azim;
-		satellites[num_report].elevation = sv->elev;
-		satellites[num_report].is_tracked = tracked;
-		num_report += 1;
-	}
-	/* Push data to compile-time consumers */
-	gnss_publish_satellites(dev, satellites, num_report);
-	return 0;
-}
-
-#endif /* CONFIG_GNSS_SATELLITES */
 
 static int get_fix_rate_handler(uint8_t message_class, uint8_t message_id, const void *payload,
 				size_t payload_len, void *user_data)
@@ -181,7 +81,7 @@ static int ubx_m10_i2c_get_fix_rate(const struct device *dev, uint32_t *fix_inte
 	net_buf_simple_add_le32(&cfg_buf, UBX_CFG_KEY_RATE_NAV);
 	ubx_msg_finalise(&cfg_buf);
 
-	return ubx_modem_send_sync(&data->modem, &cfg_buf, UBX_HANDLING_RSP_ACK,
+	return ubx_modem_send_sync(&data->common.modem, &cfg_buf, UBX_HANDLING_RSP_ACK,
 				   get_fix_rate_handler, fix_interval_ms, SYNC_MESSAGE_TIMEOUT);
 }
 
@@ -199,7 +99,7 @@ static int ubx_m10_i2c_set_fix_rate(const struct device *dev, uint32_t fix_inter
 	UBX_CFG_VALUE_APPEND(&cfg_buf, UBX_CFG_KEY_RATE_MEAS, fix_interval_ms);
 	UBX_CFG_VALUE_APPEND(&cfg_buf, UBX_CFG_KEY_RATE_NAV, 1);
 	ubx_msg_finalise(&cfg_buf);
-	return ubx_modem_send_sync_acked(&data->modem, &cfg_buf, SYNC_MESSAGE_TIMEOUT);
+	return ubx_modem_send_sync_acked(&data->common.modem, &cfg_buf, SYNC_MESSAGE_TIMEOUT);
 }
 
 static int get_navigation_mode_handler(uint8_t message_class, uint8_t message_id,
@@ -250,7 +150,7 @@ static int ubx_m10_i2c_get_navigation_mode(const struct device *dev,
 	net_buf_simple_add_le32(&cfg_buf, UBX_CFG_KEY_NAVSPG_DYNMODEL);
 	ubx_msg_finalise(&cfg_buf);
 
-	return ubx_modem_send_sync(&data->modem, &cfg_buf, UBX_HANDLING_RSP_ACK,
+	return ubx_modem_send_sync(&data->common.modem, &cfg_buf, UBX_HANDLING_RSP_ACK,
 				   get_navigation_mode_handler, mode, SYNC_MESSAGE_TIMEOUT);
 }
 
@@ -279,7 +179,7 @@ static int ubx_m10_i2c_set_navigation_mode(const struct device *dev, enum gnss_n
 			       UBX_MSG_CFG_VALSET_LAYERS_RAM | UBX_MSG_CFG_VALSET_LAYERS_BBR);
 	UBX_CFG_VALUE_APPEND(&cfg_buf, UBX_CFG_KEY_NAVSPG_DYNMODEL, ubx_dynmodel);
 	ubx_msg_finalise(&cfg_buf);
-	return ubx_modem_send_sync_acked(&data->modem, &cfg_buf, SYNC_MESSAGE_TIMEOUT);
+	return ubx_modem_send_sync_acked(&data->common.modem, &cfg_buf, SYNC_MESSAGE_TIMEOUT);
 }
 
 static int get_enabled_systems_handler(uint8_t message_class, uint8_t message_id,
@@ -340,7 +240,7 @@ static int ubx_m10_i2c_get_enabled_systems(const struct device *dev, gnss_system
 	net_buf_simple_add_le32(&cfg_buf, UBX_CFG_KEY_SIGNAL_QZSS_ENA);
 	ubx_msg_finalise(&cfg_buf);
 
-	return ubx_modem_send_sync(&data->modem, &cfg_buf, UBX_HANDLING_RSP_ACK,
+	return ubx_modem_send_sync(&data->common.modem, &cfg_buf, UBX_HANDLING_RSP_ACK,
 				   get_enabled_systems_handler, systems, SYNC_MESSAGE_TIMEOUT);
 }
 
@@ -371,7 +271,7 @@ static int ubx_m10_i2c_set_enabled_systems(const struct device *dev, gnss_system
 	UBX_CFG_VALUE_APPEND(&cfg_buf, UBX_CFG_KEY_SIGNAL_SBAS_ENA, s & GNSS_SYSTEM_SBAS);
 	UBX_CFG_VALUE_APPEND(&cfg_buf, UBX_CFG_KEY_SIGNAL_QZSS_ENA, s & GNSS_SYSTEM_QZSS);
 	ubx_msg_finalise(&cfg_buf);
-	rc = ubx_modem_send_sync_acked(&data->modem, &cfg_buf, SYNC_MESSAGE_TIMEOUT);
+	rc = ubx_modem_send_sync_acked(&data->common.modem, &cfg_buf, SYNC_MESSAGE_TIMEOUT);
 	if (rc == 0) {
 		/* Integration guide specifies a 0.5 second delay after changing GNSS config
 		 */
@@ -390,56 +290,6 @@ static int ubx_m10_i2c_get_supported_systems(const struct device *dev, gnss_syst
 }
 
 #endif /* CONFIG_GNSS_U_BLOX_NO_API_COMPAT */
-
-static void ubx_m10_i2c_extint_wake(const struct device *dev)
-{
-	const struct ubx_m10_i2c_config *cfg = dev->config;
-
-	gpio_pin_set_dt(&cfg->extint_gpio, 1);
-	k_sleep(K_MSEC(1));
-	gpio_pin_set_dt(&cfg->extint_gpio, 0);
-	/* Modem needs some time before it is ready to respond to commands */
-	k_sleep(K_MSEC(250));
-}
-
-static int ubx_m10_i2c_get_latest_timepulse(const struct device *dev, k_ticks_t *timestamp)
-{
-	const struct ubx_m10_i2c_config *cfg = dev->config;
-	struct ubx_m10_i2c_data *data = dev->data;
-	k_ticks_t now = k_uptime_ticks();
-	k_ticks_t max_age = (3 * CONFIG_SYS_CLOCK_TICKS_PER_SEC) / 2;
-	k_ticks_t tp_age = now - data->latest_timepulse;
-
-	if (cfg->timepulse_gpio.port == NULL) {
-		/* No timepulse pin connected */
-		return -ENOTSUP;
-	}
-	if (data->latest_timepulse <= MIN_TIMEPULSE_EDGES) {
-		/* Timepulse has not occurred or is not considered stable */
-		return -EAGAIN;
-	}
-	if (tp_age > max_age) {
-		/* Timepulse has not occurred in last 1.5 seconds, no longer valid */
-		data->latest_timepulse = 0;
-		return -EAGAIN;
-	}
-	*timestamp = data->latest_timepulse;
-	return 0;
-}
-
-static void timepulse_gpio_callback(const struct device *dev, struct gpio_callback *cb,
-				    uint32_t pins)
-{
-	struct ubx_m10_i2c_data *data = CONTAINER_OF(cb, struct ubx_m10_i2c_data, timepulse_cb);
-	k_ticks_t now = k_uptime_ticks();
-
-	if (data->latest_timepulse < MIN_TIMEPULSE_EDGES) {
-		data->latest_timepulse += 1;
-	} else {
-		data->latest_timepulse = now;
-	}
-	LOG_DBG("%lld", data->latest_timepulse);
-}
 
 static int mon_ver_handler(uint8_t message_class, uint8_t message_id, const void *payload,
 			   size_t payload_len, void *user_data)
@@ -474,7 +324,7 @@ static int ubx_m10_i2c_port_setup(const struct device *dev, bool hardware_reset)
 
 		ubx_msg_simple(&cfg_buf, UBX_MSG_CLASS_CFG, UBX_MSG_ID_CFG_CFG, &cfg_cfg,
 			       sizeof(cfg_cfg));
-		rc = ubx_modem_send_sync_acked(&data->modem, &cfg_buf, SYNC_MESSAGE_TIMEOUT);
+		rc = ubx_modem_send_sync_acked(&data->common.modem, &cfg_buf, SYNC_MESSAGE_TIMEOUT);
 		if (rc < 0) {
 			LOG_WRN("Failed to reset previous configuration");
 		}
@@ -490,7 +340,7 @@ static int ubx_m10_i2c_port_setup(const struct device *dev, bool hardware_reset)
 	UBX_CFG_VALUE_APPEND(&cfg_buf, UBX_CFG_KEY_I2COUTPROT_UBX, true);
 	UBX_CFG_VALUE_APPEND(&cfg_buf, UBX_CFG_KEY_I2COUTPROT_NMEA, false);
 	ubx_msg_finalise(&cfg_buf);
-	rc = ubx_modem_send_sync_acked(&data->modem, &cfg_buf, SYNC_MESSAGE_TIMEOUT);
+	rc = ubx_modem_send_sync_acked(&data->common.modem, &cfg_buf, SYNC_MESSAGE_TIMEOUT);
 	if (rc < 0) {
 		return rc;
 	}
@@ -499,7 +349,7 @@ static int ubx_m10_i2c_port_setup(const struct device *dev, bool hardware_reset)
 	ubx_msg_prepare_valset(&cfg_buf,
 			       UBX_MSG_CFG_VALSET_LAYERS_RAM | UBX_MSG_CFG_VALSET_LAYERS_BBR);
 	UBX_CFG_VALUE_APPEND(&cfg_buf, UBX_CFG_KEY_TXREADY_ENABLED, true);
-	UBX_CFG_VALUE_APPEND(&cfg_buf, UBX_CFG_KEY_TXREADY_PIN, cfg->data_ready_pio);
+	UBX_CFG_VALUE_APPEND(&cfg_buf, UBX_CFG_KEY_TXREADY_PIN, cfg->common.data_ready_pio);
 	UBX_CFG_VALUE_APPEND(&cfg_buf, UBX_CFG_KEY_TXREADY_POLARITY,
 			     UBX_CFG_TXREADY_POLARITY_ACTIVE_HIGH);
 	UBX_CFG_VALUE_APPEND(&cfg_buf, UBX_CFG_KEY_TXREADY_INTERFACE,
@@ -514,7 +364,7 @@ static int ubx_m10_i2c_port_setup(const struct device *dev, bool hardware_reset)
 	UBX_CFG_VALUE_APPEND(&cfg_buf, UBX_CFG_KEY_TP_POL_TP1, UBX_CFG_TP_POL_TP1_FALLING_EDGE);
 
 	ubx_msg_finalise(&cfg_buf);
-	rc = ubx_modem_send_sync_acked(&data->modem, &cfg_buf, SYNC_MESSAGE_TIMEOUT);
+	rc = ubx_modem_send_sync_acked(&data->common.modem, &cfg_buf, SYNC_MESSAGE_TIMEOUT);
 	if (rc < 0) {
 		return rc;
 	}
@@ -522,7 +372,7 @@ static int ubx_m10_i2c_port_setup(const struct device *dev, bool hardware_reset)
 	modem_backend_ublox_i2c_use_data_ready_gpio(&data->i2c_backend);
 
 	/* Display version information */
-	return ubx_modem_send_sync_poll(&data->modem, UBX_MSG_CLASS_MON, UBX_MSG_ID_MON_VER,
+	return ubx_modem_send_sync_poll(&data->common.modem, UBX_MSG_CLASS_MON, UBX_MSG_ID_MON_VER,
 					mon_ver_handler, NULL, SYNC_MESSAGE_TIMEOUT);
 }
 
@@ -541,10 +391,10 @@ static int ubx_m10_i2c_software_standby(const struct device *dev)
 	ubx_msg_simple(&pmreq, UBX_MSG_CLASS_RXM, UBX_MSG_ID_RXM_PMREQ, &payload, sizeof(payload));
 
 	/* Modem takes some time to go to sleep and respond to wakeup requests */
-	data->min_wake_time = K_TIMEOUT_ABS_MS(k_uptime_get() + 10);
+	data->common.min_wake_time = K_TIMEOUT_ABS_MS(k_uptime_get() + 10);
 
 	/* We don't expect a response, need to wait for TX to finish */
-	return ubx_modem_send_async(&data->modem, &pmreq, NULL, true);
+	return ubx_modem_send_async(&data->common.modem, &pmreq, NULL, true);
 }
 
 static int ubx_m10_i2c_software_resume(const struct device *dev)
@@ -553,9 +403,9 @@ static int ubx_m10_i2c_software_resume(const struct device *dev)
 	struct ubx_m10_i2c_data *data = dev->data;
 
 	/* Wait until modem is ready to wake */
-	k_sleep(data->min_wake_time);
+	k_sleep(data->common.min_wake_time);
 	/* Wake by generating an edge on the EXTINT pin */
-	ubx_m10_i2c_extint_wake(dev);
+	ubx_common_extint_wake(dev);
 	/* Modem uses NAV-PVT to fulfill requirements of GNSS API */
 	ubx_msg_prepare_valset(&cfg_buf,
 			       UBX_MSG_CFG_VALSET_LAYERS_RAM | UBX_MSG_CFG_VALSET_LAYERS_BBR);
@@ -564,91 +414,18 @@ static int ubx_m10_i2c_software_resume(const struct device *dev)
 	UBX_CFG_VALUE_APPEND(&cfg_buf, UBX_CFG_KEY_MSGOUT_UBX_NAV_SAT_I2C, 1);
 #endif /* CONFIG_GNSS_SATELLITES */
 	ubx_msg_finalise(&cfg_buf);
-	return ubx_modem_send_sync_acked(&data->modem, &cfg_buf, SYNC_MESSAGE_TIMEOUT);
+	return ubx_modem_send_sync_acked(&data->common.modem, &cfg_buf, SYNC_MESSAGE_TIMEOUT);
 }
 
 static int ubx_m10_i2c_pm_control(const struct device *dev, enum pm_device_action action)
 {
-	const struct ubx_m10_i2c_config *cfg = dev->config;
-	struct ubx_m10_i2c_data *data = dev->data;
-	bool hardware_reset = false;
-	int rc = 0;
+	const struct ubx_common_pm_fn pm_fn = {
+		.port_setup = ubx_m10_i2c_port_setup,
+		.software_resume = ubx_m10_i2c_software_resume,
+		.software_standby = ubx_m10_i2c_software_standby,
+	};
 
-	switch (action) {
-	case PM_DEVICE_ACTION_SUSPEND:
-		/* Disable timepulse interrupt */
-		if (cfg->timepulse_gpio.port != NULL) {
-			data->latest_timepulse = 0;
-			(void)gpio_pin_interrupt_configure_dt(&cfg->timepulse_gpio,
-							      GPIO_INT_DISABLE);
-		}
-		/* Put into low power mode */
-		rc = ubx_m10_i2c_software_standby(dev);
-		if (rc < 0) {
-			LOG_INF("Failed to go to standby mode");
-			return rc;
-		}
-		/* Notify modem layer */
-		ubx_modem_software_standby(&data->modem);
-		break;
-	case PM_DEVICE_ACTION_RESUME:
-		rc = ubx_m10_i2c_software_resume(dev);
-		if (rc < 0) {
-			LOG_INF("Failed to resume");
-			return rc;
-		}
-		/* Enable timepulse interrupt */
-		if (cfg->timepulse_gpio.port != NULL) {
-			data->latest_timepulse = 0;
-			(void)gpio_pin_interrupt_configure_dt(&cfg->timepulse_gpio,
-							      GPIO_INT_EDGE_TO_ACTIVE);
-		}
-		break;
-	case PM_DEVICE_ACTION_TURN_OFF:
-		gpio_pin_configure_dt(&cfg->reset_gpio, GPIO_DISCONNECTED);
-		gpio_pin_configure_dt(&cfg->extint_gpio, GPIO_DISCONNECTED);
-		break;
-	case PM_DEVICE_ACTION_TURN_ON:
-		gpio_pin_configure_dt(&cfg->extint_gpio, GPIO_OUTPUT_INACTIVE);
-		gpio_pin_configure_dt(&cfg->reset_gpio, GPIO_OUTPUT_INACTIVE);
-
-		/* Attempt to wake modem in case it is in sleep state */
-		ubx_m10_i2c_extint_wake(dev);
-		/* Attempt to communicate without hardware reset to preserve GNSS state */
-		rc = modem_pipe_open(data->modem.pipe);
-		if (rc < 0) {
-			/* Failed to open, hardware reset and try again */
-			LOG_INF("Resetting %s...", dev->name);
-			hardware_reset = true;
-			gpio_pin_configure_dt(&cfg->reset_gpio, GPIO_OUTPUT_ACTIVE);
-			k_sleep(K_MSEC(2));
-			gpio_pin_configure_dt(&cfg->reset_gpio, GPIO_OUTPUT_INACTIVE);
-			rc = modem_pipe_open(data->modem.pipe);
-		}
-		if (rc < 0) {
-			LOG_WRN("Failed to establish comms");
-			return rc;
-		}
-		/* Configure modem for I2C comms */
-		rc = ubx_m10_i2c_port_setup(dev, hardware_reset);
-		if (rc < 0) {
-			LOG_INF("Failed to setup comms port");
-			modem_pipe_close(data->modem.pipe);
-			return rc;
-		}
-		/* Put into low power mode */
-		rc = ubx_m10_i2c_software_standby(dev);
-		if (rc < 0) {
-			LOG_INF("Failed to go to standby mode");
-			modem_pipe_close(data->modem.pipe);
-			return rc;
-		}
-		break;
-	default:
-		return -ENOTSUP;
-	}
-
-	return 0;
+	return ubx_common_pm_control(dev, action, &pm_fn);
 }
 
 static int ubx_m10_i2c_init(const struct device *dev)
@@ -658,51 +435,15 @@ static int ubx_m10_i2c_init(const struct device *dev)
 	struct modem_pipe *pipe;
 	struct modem_backend_ublox_i2c_config i2c_backend_config = {
 		.i2c = &cfg->i2c,
-		.data_ready = &cfg->data_ready_gpio,
+		.data_ready = &cfg->common.data_ready_gpio,
 		.poll_period = K_MSEC(50),
 	};
 
 	/* Initialise modem backend */
 	pipe = modem_backend_ublox_i2c_init(&data->i2c_backend, &i2c_backend_config);
-	ubx_modem_init(&data->modem, pipe);
 
-	/* Setup timepulse pin interrupt */
-	if (cfg->timepulse_gpio.port != NULL) {
-		(void)gpio_pin_configure_dt(&cfg->timepulse_gpio, GPIO_INPUT);
-		gpio_init_callback(&data->timepulse_cb, timepulse_gpio_callback,
-				   BIT(cfg->timepulse_gpio.pin));
-		if (gpio_add_callback(cfg->timepulse_gpio.port, &data->timepulse_cb) < 0) {
-			LOG_ERR("Unable to add timepulse callback");
-		}
-	}
-
-#ifndef CONFIG_GNSS_U_BLOX_NO_API_COMPAT
-	/* Subscribe to all NAV-PVT messages */
-	data->pvt_handler.message_class = UBX_MSG_CLASS_NAV,
-	data->pvt_handler.message_id = UBX_MSG_ID_NAV_PVT,
-	data->pvt_handler.message_cb = nav_pvt_cb;
-	data->pvt_handler.user_data = (void *)dev;
-	ubx_modem_msg_subscribe(&data->modem, &data->pvt_handler);
-
-#ifdef CONFIG_GNSS_SATELLITES
-	/* Subscribe to all NAV-SAT messages */
-	data->sat_handler.message_class = UBX_MSG_CLASS_NAV,
-	data->sat_handler.message_id = UBX_MSG_ID_NAV_SAT,
-	data->sat_handler.message_cb = nav_sat_cb;
-	data->sat_handler.user_data = (void *)dev;
-	ubx_modem_msg_subscribe(&data->modem, &data->sat_handler);
-#endif /* CONFIG_GNSS_SATELLITES */
-#endif /* CONFIG_GNSS_U_BLOX_NO_API_COMPAT */
-
-	/* Run boot sequence */
-	return pm_device_driver_init(dev, ubx_m10_i2c_pm_control);
-}
-
-struct ubx_modem_data *ubx_modem_data_get(const struct device *dev)
-{
-	struct ubx_m10_i2c_data *data = dev->data;
-
-	return &data->modem;
+	/* Run common initialisation logic*/
+	return ubx_common_init(dev, pipe, ubx_m10_i2c_pm_control);
 }
 
 static const struct gnss_driver_api gnss_api = {
@@ -715,17 +456,13 @@ static const struct gnss_driver_api gnss_api = {
 	.get_enabled_systems = ubx_m10_i2c_get_enabled_systems,
 	.get_supported_systems = ubx_m10_i2c_get_supported_systems,
 #endif /* CONFIG_GNSS_U_BLOX_NO_API_COMPAT */
-	.get_latest_timepulse = ubx_m10_i2c_get_latest_timepulse,
+	.get_latest_timepulse = ubx_common_get_latest_timepulse,
 };
 
 #define UBX_M10_I2C(inst)                                                                          \
 	static const struct ubx_m10_i2c_config ubx_m10_cfg_##inst = {                              \
+		.common = UBX_COMMON_CONFIG_INST(inst),                                            \
 		.i2c = I2C_DT_SPEC_INST_GET(inst),                                                 \
-		.reset_gpio = GPIO_DT_SPEC_INST_GET(inst, reset_gpios),                            \
-		.extint_gpio = GPIO_DT_SPEC_INST_GET(inst, extint_gpios),                          \
-		.timepulse_gpio = GPIO_DT_SPEC_INST_GET_OR(inst, timepulse_gpios, {0}),            \
-		.data_ready_gpio = GPIO_DT_SPEC_INST_GET(inst, data_ready_gpios),                  \
-		.data_ready_pio = DT_INST_PROP(inst, data_ready_pio),                              \
 	};                                                                                         \
 	static struct ubx_m10_i2c_data ubx_m10_data_##inst;                                        \
 	PM_DEVICE_DT_INST_DEFINE(inst, ubx_m10_i2c_pm_control);                                    \
