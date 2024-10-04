6012945e1d3de114cd7526583feda76ed11c8d0a
Jordan Yates <jordan@embeint.com>
2024-10-04 11:05:19 +1000

    tests: drivers: watchdog_warning: added
    Add a test for the behaviour of the watchdog pre-expiry software
    warning.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ tests/drivers/watchdog_warning/src/main.c
@@ -0,0 +1,84 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <string.h>
+
+#include <zephyr/ztest.h>
+#include <zephyr/kernel.h>
+#include <zephyr/random/random.h>
+
+#include <infuse/drivers/watchdog.h>
+
+static uint8_t expired_channel = UINT8_MAX;
+static int64_t warning;
+K_SEM_DEFINE(watchdog_expired, 0, 1);
+
+void infuse_watchdog_warning(const struct device *dev, int channel_id)
+{
+	zassert_equal(INFUSE_WATCHDOG_DEV, dev);
+	expired_channel = channel_id;
+	warning = k_uptime_get();
+}
+
+void infuse_watchdog_expired(const struct device *dev, int channel_id)
+{
+	zassert_equal(INFUSE_WATCHDOG_DEV, dev);
+	warning = k_uptime_get() - warning;
+	k_sem_give(&watchdog_expired);
+}
+
+ZTEST(drivers_watchdog, test_watchdog)
+{
+	k_timeout_t feed_period;
+	int channel, rc;
+
+	/* QEMU watchdog only has one timeout channel */
+	channel = infuse_watchdog_install(&feed_period);
+	zassert_equal(0, channel);
+
+	/* Register watchdog against this thread */
+	infuse_watchdog_thread_register(0, _current);
+
+	/* Start watchdog */
+	zassert_equal(0, infuse_watchdog_start());
+
+	/* Feed for a while */
+	for (int i = 0; i < 3; i++) {
+		infuse_watchdog_feed(0);
+		k_sleep(K_SECONDS(1));
+	}
+
+	/* Failing to call task_runner_iterate should result in a watchdog interrupt */
+	rc = k_sem_take(&watchdog_expired, K_NO_WAIT);
+	zassert_equal(-EBUSY, rc, "Watchdog expired early");
+
+	/* Failing to call infuse_watchdog_feed should result in a watchdog interrupt */
+	rc = k_sem_take(&watchdog_expired, K_MSEC(CONFIG_INFUSE_WATCHDOG_PERIOD_MS + 100));
+	zassert_equal(0, rc, "Watchdog did not expire");
+
+	/* Warning should have been received CONFIG_INFUSE_WATCHDOG_SOFTWARE_WARNING_MS early */
+	zassert_within(CONFIG_INFUSE_WATCHDOG_SOFTWARE_WARNING_MS, warning, 1,
+		       "Watchdog warning not at expected time");
+	zassert_equal(0, expired_channel, "Unexpected channel ID");
+}
+
+void watchdog_before(void *fixture)
+{
+	/* Clear all registrations */
+	for (int i = 0; i < 8; i++) {
+		infuse_watchdog_thread_register(i, NULL);
+	}
+}
+
+void watchdog_teardown(void *fixture)
+{
+	/* Disable the watchdog to ensure we aren't rebooted */
+	wdt_disable(INFUSE_WATCHDOG_DEV);
+}
+
+ZTEST_SUITE(drivers_watchdog, NULL, NULL, watchdog_before, NULL, watchdog_teardown);
