3f51975602e6887fb4116f3244e3c9a4f8729fc1
Jordan Yates <jordan@embeint.com>
2024-10-14 15:22:54 +1000

    math: geodesy: added
    Add a library for performing math operations related to the earth.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ include/infuse/math/geodesy.h
@@ -0,0 +1,51 @@
+/**
+ * @file
+ * @brief Earth geodesic functions
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#ifndef INFUSE_SDK_INCLUDE_INFUSE_MATH_GEODESY_H_
+#define INFUSE_SDK_INCLUDE_INFUSE_MATH_GEODESY_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief geodesy API
+ * @defgroup geodesy_apis geodesy APIs
+ * @{
+ */
+
+/* Coordinate point */
+struct geodesy_coordinate {
+	/* Latitude, scaled by 1e7 */
+	int32_t latitude;
+	/* Longitude, scaled by 1e7 */
+	int32_t longitude;
+};
+
+/**
+ * @brief Calculate the great-circle (shortest) distance between two locations
+ *
+ * @param a First location
+ * @param b Second location
+ *
+ * @return uint32_t Distance between location in meters
+ */
+uint32_t geodesy_great_circle_distance(struct geodesy_coordinate a, struct geodesy_coordinate b);
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INFUSE_SDK_INCLUDE_INFUSE_MATH_GEODESY_H_ */
---
+++ lib/math/geodesy.c
@@ -0,0 +1,56 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <infuse/math/geodesy.h>
+
+#include "arm_math.h"
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846f
+#endif
+
+/* Earth's radius in meters */
+#define EARTH_RADIUS_METERS 6371000
+#define DEG_TO_RAD          (M_PI / 180.0f)
+#define SCALE_FACTOR        10000000
+
+static float32_t scaled_deg_to_rad(int32_t scaled_deg)
+{
+	return ((float32_t)scaled_deg / SCALE_FACTOR) * DEG_TO_RAD;
+}
+
+uint32_t geodesy_great_circle_distance(struct geodesy_coordinate p1, struct geodesy_coordinate p2)
+{
+	/* Convert latitude and longitude to radians */
+	float32_t lat1_rad = scaled_deg_to_rad(p1.latitude);
+	float32_t lon1_rad = scaled_deg_to_rad(p1.longitude);
+	float32_t lat2_rad = scaled_deg_to_rad(p2.latitude);
+	float32_t lon2_rad = scaled_deg_to_rad(p2.longitude);
+
+	/* Haversine formula */
+	float32_t sin_dlat_2, sin_dlon_2, cos_lat1, cos_lat2;
+	float32_t dlat = lat2_rad - lat1_rad;
+	float32_t dlon = lon2_rad - lon1_rad;
+
+	sin_dlat_2 = arm_sin_f32(dlat * 0.5f);
+	sin_dlon_2 = arm_sin_f32(dlon * 0.5f);
+	cos_lat1 = arm_cos_f32(lat1_rad);
+	cos_lat2 = arm_cos_f32(lat2_rad);
+
+	float32_t a = sin_dlat_2 * sin_dlat_2 + cos_lat1 * cos_lat2 * sin_dlon_2 * sin_dlon_2;
+	float32_t sqrt_a, sqrt_1_minus_a, atan2_result;
+
+	arm_sqrt_f32(a, &sqrt_a);
+	arm_sqrt_f32(1.0f - a, &sqrt_1_minus_a);
+	arm_atan2_f32(sqrt_a, sqrt_1_minus_a, &atan2_result);
+
+	float32_t distance = 2.0f * EARTH_RADIUS_METERS * atan2_result;
+
+	/* Round to nearest meter */
+	return (uint32_t)(distance + 0.5f);
+}
