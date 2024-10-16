4f7762be1f143d322d5601f1930f1464ee029e0b
Jordan Yates <jordan@embeint.com>
2024-10-16 16:26:27 +1000

    boards: nrf7002dk: fix TF-M flashing
    Fix flashing `tfm_merged.hex`.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- boards/nordic/nrf7002dk/board.cmake
+++ boards/nordic/nrf7002dk/board.cmake
@@ -11,7 +11,7 @@ if(CONFIG_BOARD_NRF7002DK_NRF5340_CPUAPP OR CONFIG_BOARD_NRF7002DK_NRF5340_CPUAP
 endif()
 
 if(CONFIG_TFM_FLASH_MERGED_BINARY)
-  set_property(TARGET runners_yaml_props_target PROPERTY hex_file "${CMAKE_BINARY_DIR}/tfm_merged.hex")
+  set_property(TARGET runners_yaml_props_target PROPERTY hex_file tfm_merged.hex)
 endif()
 
 if(CONFIG_BOARD_NRF7002DK_NRF5340_CPUNET OR CONFIG_BOARD_NRF7002DK_NRF5340_CPUNET_NRF7001)
