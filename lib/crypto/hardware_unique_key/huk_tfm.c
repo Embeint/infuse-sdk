eb7fc1b97164cfeaad05a00d7fdaafadf13d959f
Jordan Yates <jordan@embeint.com>
2024-10-19 14:03:10 +1000

    crypto: hardware_unique_key: support TF-M backend
    When building with TF-M, use the HUK created and protected by TF-M.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ lib/crypto/hardware_unique_key/huk_tfm.c
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <psa/crypto.h>
+
+#include "crypto_keys/tfm_builtin_key_ids.h"
+
+int hardware_unique_key_init(void)
+{
+	/* All initialisation done by TF-M */
+	return 0;
+}
+
+psa_key_id_t hardware_unique_key_id(void)
+{
+	return (psa_key_id_t)TFM_BUILTIN_KEY_ID_HUK;
+}
