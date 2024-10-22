915df5593dab953ae3491912dd215be55814cf37
Jordan Yates <jordan@embeint.com>
2024-10-23 09:07:38 +1000

    tests: epacket: callbacks: test `packet_received`
    Test the `packet_received` callback.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- tests/subsys/epacket/callbacks/src/main.c
+++ tests/subsys/epacket/callbacks/src/main.c
@@ -9,6 +9,7 @@
 #include <zephyr/ztest.h>
 #include <zephyr/kernel.h>
 
+#include <infuse/types.h>
 #include <infuse/epacket/interface.h>
 #include <infuse/epacket/packet.h>
 #include <infuse/epacket/interface/epacket_dummy.h>
@@ -16,6 +17,7 @@
 static struct epacket_interface_cb interface_cb;
 static struct k_poll_signal tx_fail_signal;
 static struct k_poll_signal tx_done_signal;
+static struct k_poll_signal rx_recv_signal;
 
 static void tx_failed_cb(const struct net_buf *buf, int reason, void *user_ctx)
 {
@@ -23,6 +25,14 @@ static void tx_failed_cb(const struct net_buf *buf, int reason, void *user_ctx)
 	zassert_equal(user_ctx, &interface_cb);
 
 	k_poll_signal_raise(&tx_fail_signal, reason);
+}
+
+static void packet_received_cb(const struct net_buf *buf, bool decrypted, void *user_ctx)
+{
+	zassert_not_null(buf);
+	zassert_equal(user_ctx, &interface_cb);
+
+	k_poll_signal_raise(&rx_recv_signal, (int)decrypted);
 }
 
 static void tx_done(const struct device *dev, struct net_buf *buf, int result)
@@ -47,6 +57,7 @@ ZTEST(epacket_callbacks, test_interface_tx_failure)
 	zassert_is_null(net_buf_get(sent_queue, K_NO_WAIT));
 
 	interface_cb.tx_failure = tx_failed_cb;
+	interface_cb.packet_received = packet_received_cb;
 	interface_cb.user_ctx = &interface_cb;
 
 	/* Allocate buffer */
@@ -100,6 +111,22 @@ ZTEST(epacket_callbacks, test_interface_tx_failure)
 
 	/* Reset interface failures */
 	epacket_dummy_set_tx_failure(0);
+
+	/* Receive a packet on the interface */
+	k_poll_signal_check(&rx_recv_signal, &signaled, &result);
+	zassert_equal(0, signaled);
+
+	struct epacket_dummy_frame frame = {
+		.type = INFUSE_RPC_CMD,
+		.auth = EPACKET_AUTH_DEVICE,
+		.flags = 0x00,
+	};
+
+	epacket_dummy_receive(epacket_dummy, &frame, payload, sizeof(payload));
+	k_sleep(K_MSEC(1));
+	k_poll_signal_check(&rx_recv_signal, &signaled, &result);
+	zassert_equal(1, signaled);
+	zassert_equal(1, result);
 }
 
 void *callback_setup(void)
@@ -108,6 +135,7 @@ void *callback_setup(void)
 
 	k_poll_signal_init(&tx_done_signal);
 	k_poll_signal_init(&tx_fail_signal);
+	k_poll_signal_init(&rx_recv_signal);
 	epacket_register_callback(epacket_dummy, &interface_cb);
 
 	return NULL;
