45167aef6e5b0a976d65bcba8568278146f30bfa
Jordan Yates <jordan@embeint.com>
2024-10-25 14:04:15 +1000

    bluetooth: controller_manager: API for DFU
    Add an API for upgrading a remote Bluetooth controller via the RPC
    client library.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- subsys/bluetooth/controller_manager.c
+++ subsys/bluetooth/controller_manager.c
@@ -18,6 +18,8 @@
 #include <infuse/fs/kv_types.h>
 
 static struct rpc_client_ctx ctx;
+static struct rpc_file_write_basic_response write_rsp;
+static K_SEM_DEFINE(write_done, 0, 1);
 
 LOG_MODULE_REGISTER(bt_ctlr_manager, LOG_LEVEL_INF);
 
@@ -52,4 +54,76 @@ int bt_controller_manager_init(void)
 	/* Write the version to storage so cloud can sync it */
 	(void)KV_STORE_WRITE(KV_KEY_BLUETOOTH_CTLR_VERSION, &bt_ctlr_ver);
 	return 0;
+}
+
+static void write_image_done(const struct net_buf *buf, void *user_data)
+{
+	if (buf == NULL) {
+		LOG_WRN("DFU process timed out");
+		write_rsp.header.return_code = -ETIMEDOUT;
+	} else {
+		const struct rpc_file_write_basic_response *rsp = (const void *)buf->data;
+
+		write_rsp = *rsp;
+	}
+
+	k_sem_give(&write_done);
+}
+
+int bt_controller_manager_dfu_write_start(uint32_t *context, size_t image_len)
+{
+	struct rpc_file_write_basic_request write_req = {
+		.data_header =
+			{
+				.size = image_len,
+				.rx_ack_period = 4,
+			},
+		.action = RPC_ENUM_FILE_ACTION_APP_IMG,
+	};
+	int rc;
+
+	rpc_client_init(&ctx, DEVICE_DT_GET(DT_INST(0, embeint_epacket_hci)));
+
+	LOG_INF("Starting DFU process");
+	rc = rpc_client_command_queue(&ctx, RPC_ID_FILE_WRITE_BASIC, &write_req, sizeof(write_req),
+				      write_image_done, NULL, K_NO_WAIT, K_SECONDS(10));
+	*context = rpc_client_last_request_id(&ctx);
+	if (rc < 0) {
+		goto end;
+	}
+
+	/* Wait for initial ACK */
+	rc = rpc_client_ack_wait(&ctx, *context, K_SECONDS(10));
+	LOG_DBG("DFU erase complete");
+	/* Writes should have a much tighter timeout */
+	(void)rpc_client_update_response_timeout(&ctx, *context, K_SECONDS(1));
+end:
+	if (rc < 0) {
+		*context = 0;
+		/* Unregister from callbacks */
+		rpc_client_cleanup(&ctx);
+	}
+	return rc;
+}
+
+int bt_controller_manager_dfu_write_next(uint32_t context, uint32_t image_offset,
+					 const void *image_chunk, size_t chunk_len)
+{
+	/* Push data to controller */
+	LOG_DBG("Writing offset %08X", image_offset);
+	return rpc_client_data_queue(&ctx, context, image_offset, image_chunk, chunk_len);
+}
+
+int bt_controller_manager_dfu_write_finish(uint32_t context, uint32_t *len, uint32_t *crc)
+{
+	if (context != 0) {
+		*len = write_rsp.recv_len;
+		*crc = write_rsp.recv_crc;
+
+		k_sem_take(&write_done, K_FOREVER);
+		/* Unregister from callbacks */
+		rpc_client_cleanup(&ctx);
+	}
+	LOG_INF("DFU process finished");
+	return write_rsp.header.return_code;
 }
