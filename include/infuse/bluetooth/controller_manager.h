6160bfdaff80b54fdf61716a39a82d5b5652859e
Jordan Yates <jordan@embeint.com>
2024-10-25 14:04:15 +1000

    bluetooth: controller_manager: API for DFU
    Add an API for upgrading a remote Bluetooth controller via the RPC
    client library.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- include/infuse/bluetooth/controller_manager.h
+++ include/infuse/bluetooth/controller_manager.h
@@ -10,6 +10,8 @@
 #ifndef INFUSE_SDK_INCLUDE_INFUSE_BLUETOOTH_CONTROLLER_MANAGE_H_
 #define INFUSE_SDK_INCLUDE_INFUSE_BLUETOOTH_CONTROLLER_MANAGE_H_
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -27,6 +29,43 @@ extern "C" {
  * @retval -errno on error
  */
 int bt_controller_manager_init(void);
+
+/**
+ * @brief Start Bluetooth controller device firmware update
+ *
+ * @param ctx Context value that must be provided to future calls
+ * @param image_len Length of the new image file
+ *
+ * @retval 0 On success
+ * @retval -errno On error
+ */
+int bt_controller_manager_dfu_write_start(uint32_t *ctx, size_t image_len);
+
+/**
+ * @brief Write the next chunk of the DFU image to the Bluetooth controller
+ *
+ * @param ctx Context value from @ref bt_controller_manager_dfu_write_start
+ * @param image_offset Byte offset of this chunk
+ * @param image_chunk Pointer to the chunk
+ * @param chunk_len Length of the chunk
+ *
+ * @retval 0 On success
+ * @retval -errno On error
+ */
+int bt_controller_manager_dfu_write_next(uint32_t ctx, uint32_t image_offset,
+					 const void *image_chunk, size_t chunk_len);
+
+/**
+ * @brief Finish the Bluetooth controller device firmware update
+ *
+ * @param ctx Context value from @ref bt_controller_manager_dfu_write_start
+ * @param len Output length of data received by the controller
+ * @param crc Output CRC of data received by the controller
+ *
+ * @retval 0 On success
+ * @retval -errno On error
+ */
+int bt_controller_manager_dfu_write_finish(uint32_t ctx, uint32_t *len, uint32_t *crc);
 
 /**
  * @}
