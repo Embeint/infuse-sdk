f2629b235e8d045d08e732e4fbf8b4bc7c63cbc5
Jordan Yates <jordan@embeint.com>
2024-10-26 12:56:29 +1000

    rpc: commands: common_file_actions: added
    Move the file handling logic into a common file to reduce duplication
    and simplify the process of adding and testing new options.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- subsys/rpc/commands/coap_download.c
+++ subsys/rpc/commands/coap_download.c
@@ -27,60 +27,24 @@
 #include "nrf_modem_delta_dfu.h"
 #endif /* CONFIG_NRF_MODEM_LIB */
 
+#include "common_file_actions.h"
 #include "../server.h"
 
 LOG_MODULE_DECLARE(rpc_server);
 
-struct cb_ctx {
-	const struct flash_area *fa;
-	uint32_t expected_len;
-	uint32_t crc;
-	uint8_t action;
-};
-
 static int data_cb(uint32_t offset, const uint8_t *data, uint16_t data_len, void *context)
 {
-	struct cb_ctx *ctx = context;
+	struct rpc_common_file_actions_ctx *ctx = context;
 	int rc;
-
-	ARG_UNUSED(rc);
 
 	/* Prevent RPC server watchdog channel timing out */
 	rpc_server_watchdog_feed();
 
-	/* Update CRC (Initialised to 0 in main function) */
-	ctx->crc = crc32_ieee_update(ctx->crc, data, data_len);
-
-	switch (ctx->action) {
-#ifdef CONFIG_INFUSE_DFU_HELPERS
-	case RPC_ENUM_FILE_ACTION_APP_IMG:
-		/* Erase receiving area on first packet */
-		if (offset == 0) {
-			rc = infuse_dfu_image_erase(ctx->fa, ctx->expected_len);
-			if (rc < 0) {
-				LOG_ERR("DFU: Failed to erase (%d)", rc);
-				return rc;
-			}
-		}
-		/* Write to opened flash area */
-		rc = flash_area_write(ctx->fa, offset, data, data_len);
-		if (rc < 0) {
-			LOG_ERR("DFU: Failed to write at 0x%08x (%d)", offset, rc);
-			return rc;
-		}
-		break;
-#endif /* CONFIG_INFUSE_DFU_HELPERS */
-#ifdef CONFIG_NRF_MODEM_LIB
-	case RPC_ENUM_FILE_ACTION_NRF91_MODEM_DIFF:
-		rc = nrf_modem_delta_dfu_write(data, data_len);
-		if (rc != 0) {
-			LOG_ERR("Modem: Failed to write at 0x%08x (%d)", offset, rc);
-			return rc;
-		}
-		break;
-#endif /* CONFIG_NRF_MODEM_LIB */
+	rc = rpc_common_file_actions_write(ctx, offset, data, data_len);
+	if (rc < 0) {
+		LOG_ERR("Failed to handle offset %08X (%d)", offset, rc);
 	}
-	return 0;
+	return rc;
 }
 
 struct net_buf *rpc_command_coap_download(struct net_buf *request)
@@ -89,62 +53,25 @@ struct net_buf *rpc_command_coap_download(struct net_buf *request)
 	const sec_tag_t sec_tls_tags[] = {
 		infuse_security_coap_dtls_tag(),
 	};
-	struct cb_ctx context = {
-		.action = req->action,
-		.expected_len = req->resource_len,
-		.crc = 0,
-	};
+	struct rpc_common_file_actions_ctx ctx;
 	int block_timeout = req->block_timeout_ms == 0 ? 1000 : req->block_timeout_ms;
 	struct sockaddr address;
 	socklen_t address_len;
 	uint8_t *work_mem;
 	size_t work_mem_size;
-	uint32_t crc;
 	int sock = -1;
 	int downloaded = 0;
 	int rc;
 
 	work_mem = rpc_server_command_working_mem(&work_mem_size);
 
-	/* Validate requested action */
-	switch (req->action) {
-	case RPC_ENUM_FILE_ACTION_DISCARD:
-		break;
-#if FIXED_PARTITION_EXISTS(slot1_partition)
-	case RPC_ENUM_FILE_ACTION_APP_IMG:
-		rc = flash_area_open(FIXED_PARTITION_ID(slot1_partition), &context.fa);
-		if (rc < 0) {
-			goto error;
-		}
-		if (req->resource_crc != UINT32_MAX) {
-			rc = flash_area_crc32(context.fa, 0, req->resource_len, &crc, work_mem,
-					      work_mem_size);
-			if (rc < 0) {
-				goto error;
-			}
-			if (req->resource_crc == crc) {
-				LOG_INF("Resource CRC already matches");
-				context.crc = crc;
-				rc = 0;
-				goto done;
-			}
-		}
-		break;
-#endif /* FIXED_PARTITION_EXISTS(slot1_partition) */
-#ifdef CONFIG_NRF_MODEM_LIB
-	case RPC_ENUM_FILE_ACTION_NRF91_MODEM_DIFF:
-		rc = infuse_dfu_nrf91_modem_delta_prepare();
-		if (rc > 0) {
-			rc = -EIO;
-		}
-		if (rc < 0) {
-			LOG_ERR("Failed to prepare modem (%d)", rc);
-			goto error;
-		}
-		break;
-#endif /* CONFIG_NRF_MODEM_LIB */
-	default:
-		rc = -EINVAL;
+	rc = rpc_common_file_actions_start(&ctx, req->action, req->resource_len, req->resource_crc);
+	if (rc == FILE_ALREADY_PRESENT) {
+		LOG_INF("File already present");
+		goto download_done;
+	}
+	if (rc < 0) {
+		LOG_ERR("Failed to prepare for %d (%d)", req->action, rc);
 		goto error;
 	}
 
@@ -196,7 +123,7 @@ struct net_buf *rpc_command_coap_download(struct net_buf *request)
 	}
 
 	/* Download the resource */
-	downloaded = infuse_coap_download(sock, req->resource, data_cb, &context, work_mem,
+	downloaded = infuse_coap_download(sock, req->resource, data_cb, &ctx, work_mem,
 					  work_mem_size, block_timeout);
 	if (downloaded < 0) {
 		LOG_DBG("infuse_coap_download failed (%d)", downloaded);
@@ -207,53 +134,16 @@ struct net_buf *rpc_command_coap_download(struct net_buf *request)
 	/* Close the socket */
 	zsock_close(sock);
 
-done:
-	/* Close flash area if open */
-	if (context.fa != NULL) {
-		flash_area_close(context.fa);
-	}
-
-	/* Post download actions */
-	switch (req->action) {
-#ifdef CONFIG_MCUBOOT_IMG_MANAGER
-	case RPC_ENUM_FILE_ACTION_APP_IMG:
-		if (boot_request_upgrade_multi(0, BOOT_UPGRADE_TEST) == 0) {
-#ifdef CONFIG_INFUSE_REBOOT
-			LOG_INF("DFU download complete, rebooting");
-			infuse_reboot_delayed(INFUSE_REBOOT_DFU,
-					      (uintptr_t)rpc_command_coap_download,
-					      RPC_ENUM_FILE_ACTION_APP_IMG, K_SECONDS(2));
-#else
-			LOG_WRN("INFUSE_REBOOT not enabled, cannot reboot");
-#endif /* CONFIG_INFUSE_REBOOT */
-		}
-		break;
-#endif /* CONFIG_MCUBOOT_IMG_MANAGER */
-#ifdef CONFIG_NRF_MODEM_LIB
-	case RPC_ENUM_FILE_ACTION_NRF91_MODEM_DIFF:
-		rc = infuse_dfu_nrf91_modem_delta_finish();
-		if (rc > 0) {
-			rc = -EIO;
-		}
-		if (rc == 0) {
-#ifdef CONFIG_INFUSE_REBOOT
-			/* Schedule the reboot in a few seconds time */
-			infuse_reboot_delayed(INFUSE_REBOOT_DFU,
-					      (uintptr_t)rpc_command_coap_download,
-					      RPC_ENUM_FILE_ACTION_NRF91_MODEM_DIFF, K_SECONDS(2));
-#else
-			LOG_WRN("INFUSE_REBOOT not enabled, cannot reboot");
-#endif /* CONFIG_INFUSE_REBOOT */
-		} else {
-			LOG_ERR("Modem DFU done failed (%d)", rc);
-		}
-		break;
-#endif /* CONFIG_NRF_MODEM_LIB */
+download_done:
+	/* Finish file write process */
+	rc = rpc_common_file_actions_finish(&ctx, RPC_ID_COAP_DOWNLOAD);
+	if (rc < 0) {
+		LOG_ERR("Failed to finish %d (%d)", req->action, rc);
 	}
 
 	struct rpc_coap_download_response rsp = {
-		.resource_crc = context.crc,
-		.resource_len = downloaded,
+		.resource_len = ctx.received,
+		.resource_crc = ctx.crc,
 	};
 
 	return rpc_response_simple_req(request, rc, &rsp, sizeof(rsp));
@@ -264,10 +154,8 @@ error:
 		.resource_len = 0,
 	};
 
-	/* Close flash area if open */
-	if (context.fa != NULL) {
-		flash_area_close(context.fa);
-	}
+	/* Cleanup resources */
+	(void)rpc_common_file_actions_error_cleanup(&ctx);
 
 	/* Close socket if open */
 	if (sock >= 0) {
---
+++ subsys/rpc/commands/common_file_actions.h
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <zephyr/storage/stream_flash.h>
+
+#include <infuse/rpc/types.h>
+
+struct rpc_common_file_actions_ctx {
+	union {
+		struct stream_flash_ctx stream;
+		uint32_t client_ctx;
+	};
+	enum rpc_enum_file_action action;
+	uint32_t received;
+	uint32_t crc;
+};
+
+#define FILE_ALREADY_PRESENT 1
+
+int rpc_common_file_actions_start(struct rpc_common_file_actions_ctx *ctx,
+				  enum rpc_enum_file_action action, uint32_t length, uint32_t crc);
+
+int rpc_common_file_actions_write(struct rpc_common_file_actions_ctx *ctx, uint32_t offset,
+				  const void *data, size_t data_len);
+
+int rpc_common_file_actions_finish(struct rpc_common_file_actions_ctx *ctx, uint16_t rpc_id);
+
+int rpc_common_file_actions_error_cleanup(struct rpc_common_file_actions_ctx *ctx);
