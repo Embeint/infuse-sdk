9a940aa23008da650306e89ec8d3da990b160522
Aeyohan Furtado <aeyohan@embeint.com>
2024-10-30 09:37:02 +1000

    drivers: fuel_gauge_custom_prop: added
    Added `fuel_gauge_custom_prop.h` to expose enabling a fuel gauge's
    hibernation mode.
    
    Signed-off-by: Aeyohan Furtado <aeyohan@embeint.com>

---
+++ include/infuse/drivers/fuel_gauge_custom_prop.h
@@ -0,0 +1,38 @@
+/**
+ * @file
+ * @brief Custom fuel gauge properties
+ * @copyright 2024 Embeint Inc
+ * @author Aeyohan Furtado <aeyohan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ *
+ */
+
+#ifndef INFUSE_SDK_INCLUDE_INFUSE_DRIVERS_FUEL_GAUGE_CUSTOM_PROP_H_
+#define INFUSE_SDK_INCLUDE_INFUSE_DRIVERS_FUEL_GAUGE_CUSTOM_PROP_H_
+
+#include <zephyr/drivers/fuel_gauge.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief fuel_gauge_custom_prop API
+ * @defgroup fuel_gauge_custom_prop_apis fuel_gauge_custom_prop APIs
+ * @{
+ */
+
+enum fuel_gauge_custom_prop_type {
+	FUEL_GAUGE_HIBERNATION_EN = FUEL_GAUGE_CUSTOM_BEGIN,
+};
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INFUSE_SDK_INCLUDE_INFUSE_DRIVERS_FUEL_GAUGE_CUSTOM_PROP_H_ */
