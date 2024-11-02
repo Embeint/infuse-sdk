38ac88cc6662c448377b93de4008e70fd3eaeae5
Jordan Yates <jordan@embeint.com>
2024-11-02 17:20:17 +1000

    scripts: bsim_coverage: added
    Add a helper script to run babblesim and generate the coverage report
    in HTML form.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ scripts/bsim_coverage.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+echo "BabbleSim Coverage"
+
+export ZEPHYR_BASE=`pwd`/zephyr
+export INFUSE_BASE=`pwd`/infuse-sdk
+export WORK_DIR=`pwd`/bsim_out
+# Run BabbleSim tests
+$INFUSE_BASE/tests/bsim/ci.bt.sh
+
+# Disable branch coverage on LOG_ and __ASSERT macros
+BRANCH_LOG="(^\s*LOG_(?:HEXDUMP_)?(?:DBG|INF|WRN|ERR)\(.*)"
+BRANCH_ASSERT="(^\s*__ASSERT(?:_EVAL|_NO_MSG|_POST_ACTION)?\(.*)"
+
+# Generate coverage.json
+gcovr -r $INFUSE_BASE \
+    --gcov-ignore-parse-errors=negative_hits.warn_once_per_file \
+    --gcov-executable gcov -e tests/* -e .*generated.* -e .*/tests/.* -e .*/samples/.* \
+    --exclude-branches-by-pattern "$BRANCH_LOG|$BRANCH_ASSERT" \
+    --merge-mode-functions=separate --json \
+    -o $WORK_DIR/coverage.json \
+    $WORK_DIR
+
+# Generate html
+mkdir -p $WORK_DIR/html
+gcovr -r $INFUSE_BASE --html $WORK_DIR/html/index.html --html-details \
+    --add-tracefile $WORK_DIR/coverage.json
