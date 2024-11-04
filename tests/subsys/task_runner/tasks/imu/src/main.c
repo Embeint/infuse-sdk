a7a0dcb17e7ad3d0c3afbe9f21ff193d621a71db
Jordan Yates <jordan@embeint.com>
2024-11-04 21:38:30 +1000

    tests: task_runner: tasks: imu: added
    Add basic validation of the IMU task using the emulated IMU device.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ tests/subsys/task_runner/tasks/imu/src/main.c
@@ -0,0 +1,187 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <stdint.h>
+
+#include <zephyr/ztest.h>
+#include <zephyr/kernel.h>
+
+#include <infuse/epacket/packet.h>
+#include <infuse/epacket/interface/epacket_dummy.h>
+#include <infuse/data_logger/logger.h>
+#include <infuse/data_logger/high_level/tdf.h>
+#include <infuse/drivers/imu.h>
+#include <infuse/tdf/tdf.h>
+#include <infuse/zbus/channels.h>
+
+#include <infuse/task_runner/task.h>
+#include <infuse/task_runner/runner.h>
+#include <infuse/task_runner/tasks/imu.h>
+
+IMU_TASK(1, 0, DEVICE_DT_GET_ONE(embeint_imu_emul));
+struct task_config config = IMU_TASK(0, 1, DEVICE_DT_GET_ONE(embeint_imu_emul));
+struct task_data data;
+struct task_schedule schedule = {.task_id = TASK_ID_IMU};
+struct task_schedule_state state;
+
+static K_SEM_DEFINE(imu_published, 0, 1);
+
+void imu_new_data_cb(const struct zbus_channel *chan);
+
+INFUSE_ZBUS_CHAN_DECLARE(INFUSE_ZBUS_CHAN_IMU);
+ZBUS_LISTENER_DEFINE(imu_listener, imu_new_data_cb);
+ZBUS_CHAN_ADD_OBS(INFUSE_ZBUS_NAME(INFUSE_ZBUS_CHAN_IMU), imu_listener, 5);
+#define ZBUS_CHAN INFUSE_ZBUS_CHAN_GET(INFUSE_ZBUS_CHAN_IMU)
+
+void imu_new_data_cb(const struct zbus_channel *chan)
+{
+	k_sem_give(&imu_published);
+}
+
+static k_tid_t task_schedule(struct task_data *data)
+{
+	data->schedule_idx = 0;
+	data->executor.workqueue.reschedule_counter = 0;
+	k_poll_signal_init(&data->terminate_signal);
+
+	return k_thread_create(&data->executor.thread, config.executor.thread.stack,
+			       config.executor.thread.stack_size,
+			       (k_thread_entry_t)config.executor.thread.task_fn, (void *)&schedule,
+			       &data->terminate_signal, config.task_arg.arg, 5, 0, K_NO_WAIT);
+}
+
+static void task_terminate(struct task_data *data)
+{
+	k_poll_signal_raise(&data->terminate_signal, 0);
+}
+
+ZTEST(task_imu, test_invalid_config)
+{
+	k_tid_t thread;
+
+	schedule.task_args.infuse.imu = (struct task_imu_args){
+		.accelerometer =
+			{
+				.range_g = 3,
+				.rate_hz = 50,
+			},
+		.fifo_sample_buffer = 10,
+	};
+
+	/* Schedule with invalid range */
+	thread = task_schedule(&data);
+
+	/* No data should be published*/
+	zassert_equal(-EAGAIN, k_sem_take(&imu_published, K_SECONDS(2)));
+	/* Thread should have terminated */
+	zassert_equal(0, k_thread_join(thread, K_NO_WAIT));
+}
+
+static void expect_logging(uint8_t range)
+{
+	struct k_fifo *tx_queue = epacket_dummmy_transmit_fifo_get();
+	struct net_buf *pkt = net_buf_get(tx_queue, K_MSEC(10));
+	struct tdf_parsed tdf;
+	uint16_t expected_tdf;
+
+	switch (range) {
+	case 2:
+		expected_tdf = TDF_ACC_2G;
+		break;
+	case 4:
+		expected_tdf = TDF_ACC_4G;
+		break;
+	case 8:
+		expected_tdf = TDF_ACC_8G;
+		break;
+	default:
+		expected_tdf = TDF_ACC_16G;
+		break;
+	}
+
+	zassert_not_null(pkt);
+	net_buf_pull(pkt, sizeof(struct epacket_dummy_frame));
+	zassert_equal(0, tdf_parse_find_in_buf(pkt->data, pkt->len, expected_tdf, &tdf));
+	net_buf_unref(pkt);
+}
+
+static void test_imu(uint8_t range, uint16_t rate, uint16_t num_samples, uint8_t num_buffers,
+		     bool log)
+{
+	struct k_fifo *tx_queue = epacket_dummmy_transmit_fifo_get();
+	const struct imu_sample_array *samples;
+	k_tid_t thread;
+
+	schedule.task_args.infuse.imu = (struct task_imu_args){
+		.accelerometer =
+			{
+				.range_g = range,
+				.rate_hz = rate,
+			},
+		.fifo_sample_buffer = num_samples,
+	};
+	schedule.task_logging[0].tdf_mask = 0;
+	schedule.task_logging[0].loggers = 0;
+	if (log) {
+		schedule.task_logging[0].tdf_mask = TASK_IMU_LOG_ACC;
+		schedule.task_logging[0].loggers = TDF_DATA_LOGGER_SERIAL;
+	}
+
+	(void)k_sem_take(&imu_published, K_NO_WAIT);
+	zassert_not_null(tx_queue);
+	thread = task_schedule(&data);
+
+	for (int i = 0; i < num_buffers; i++) {
+		/* Wait for emulated data */
+		zassert_equal(0, k_sem_take(&imu_published, K_SECONDS(1)));
+
+		/* Check published data */
+		zassert_equal(0, zbus_chan_claim(ZBUS_CHAN, K_MSEC(1)));
+		samples = ZBUS_CHAN->message;
+		zassert_equal(range, samples->accelerometer.full_scale_range);
+		zassert_equal(num_samples, samples->accelerometer.num);
+		zassert_equal(0, samples->gyroscope.num);
+		zassert_equal(0, samples->magnetometer.num);
+		zassert_equal(0, samples->accelerometer.offset);
+		zbus_chan_finish(ZBUS_CHAN);
+
+		/* No data being pushed */
+		tdf_data_logger_flush(TDF_DATA_LOGGER_SERIAL);
+		if (log == 0) {
+			zassert_is_null(net_buf_get(tx_queue, K_MSEC(1)));
+		} else {
+			expect_logging(range);
+		}
+	}
+
+	task_terminate(&data);
+	zassert_equal(0, k_thread_join(thread, K_SECONDS(2)));
+}
+
+ZTEST(task_imu, test_no_log)
+{
+	test_imu(4, 50, 25, 5, false);
+	test_imu(2, 25, 20, 5, false);
+	test_imu(8, 30, 15, 5, false);
+}
+
+ZTEST(task_imu, test_log)
+{
+	test_imu(4, 50, 25, 5, true);
+	test_imu(2, 25, 20, 5, true);
+	test_imu(8, 30, 15, 5, true);
+}
+
+static void logger_before(void *fixture)
+{
+	k_sem_reset(&imu_published);
+	/* Setup links between task config and data */
+	task_runner_init(&schedule, &state, 1, &config, &data, 1);
+}
+
+ZTEST_SUITE(task_imu, NULL, NULL, logger_before, NULL, NULL);
