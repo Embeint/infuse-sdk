5eeccb2cf665d6e8b4343b76f3e5c9bfe2e7fec2
Jordan Yates <jordan@embeint.com>
2024-11-04 16:16:00 +1000

    lib: math: statistics library
    Integer statistics library translated to C from:
    https://sfat.massey.ac.nz/research/centres/crisp/pdfs/2013_IVCNZ_214.pdf
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ include/infuse/math/statistics.h
@@ -0,0 +1,142 @@
+/**
+ * @file
+ * @brief Infuse-IoT statistics helpers
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#ifndef INFUSE_SDK_INCLUDE_INFUSE_MATH_STATISTICS_H_
+#define INFUSE_SDK_INCLUDE_INFUSE_MATH_STATISTICS_H_
+
+#include <stdint.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Running statistics API
+ * @defgroup statistics_apis Infuse-IoT running statistics APIs
+ *
+ * This API is a C implementation of the following paper, which is itself
+ * inspired by the well known Welford's algorithm:
+ *     https://sfat.massey.ac.nz/research/centres/crisp/pdfs/2013_IVCNZ_214.pdf
+ *
+ * @{
+ */
+
+struct statistics_state {
+	/* Variance approximation */
+	int64_t v;
+	/* Variance correction factor */
+	int64_t w;
+	/* Mean approximation */
+	int32_t m;
+	/* Mean fractional accumulation */
+	int32_t p;
+	/* Sequence count */
+	uint32_t n;
+};
+
+static inline void statistics_reset(struct statistics_state *state)
+{
+	memset(state, 0x00, sizeof(*state));
+}
+
+/**
+ * @brief Update the statistics object with a new sample
+ *
+ * @param state Statistics state object
+ * @param value New value to feed into state
+ */
+void statistics_update(struct statistics_state *state, int32_t value);
+
+/**
+ * @brief Compute the mean of the statistics object
+ *
+ * @param state Statistics state object
+ *
+ * @return float Mean of samples
+ */
+static inline float statistics_mean(struct statistics_state *state)
+{
+	if (state->n == 0) {
+		return 0.0f;
+	}
+
+	/* Equation 8 */
+	return (float)state->m + ((float)state->p / state->n);
+}
+
+/**
+ * @brief Compute the variance of the statistics object
+ *
+ * @param state Statistics state object
+ *
+ * @return float Variance of samples
+ */
+static inline float statistics_variance(struct statistics_state *state)
+{
+	if (state->n < 2) {
+		return 0.0f;
+	}
+	float mean_error = statistics_mean(state) - state->m;
+
+	/* Equation 9 */
+	return (float)state->v + ((float)state->w / (float)(state->n - 1)) -
+	       (state->n * mean_error * mean_error / (state->n - 1));
+}
+
+/**
+ * @brief Compute the rough mean of the statistics object
+ *
+ * The computed value is "rough" in the sense that it does not attempt to round
+ * to the nearest whole number, and merely takes the integer portion.
+ *
+ * Tests validate that this value is within 1.0f of the value returned by
+ * @ref statistics_mean.
+ *
+ * @param state Statistics state object
+ *
+ * @return uint32_t Approximate mean of samples
+ */
+static inline int32_t statistics_mean_rough(struct statistics_state *state)
+{
+	return state->m;
+}
+
+/**
+ * @brief Compute the rough variance of the statistics object
+ *
+ * The computed value is "rough" in the sense that it does not attempt to round
+ * to the nearest whole number, and merely takes the integer portion. It also
+ * does not take into account the difference between `state->m` and the true
+ * mean, as described in the paper.
+ *
+ * Tests validate that this value is within 1.0f or 2% of the value returned by
+ * @ref statistics_variance, whichever is less accurate.
+ *
+ * @param state Statistics state object
+ *
+ * @return uint64_t Approximate variance of samples
+ */
+static inline uint64_t statistics_variance_rough(struct statistics_state *state)
+{
+	if (state->n < 2) {
+		return 0;
+	}
+	return state->v + (state->w / (state->n - 1));
+}
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INFUSE_SDK_INCLUDE_INFUSE_MATH_STATISTICS_H_ */
---
+++ lib/math/statistics.c
@@ -0,0 +1,48 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <infuse/math/statistics.h>
+
+void statistics_update(struct statistics_state *state, int32_t x)
+{
+	int32_t m1 = state->m;
+	int32_t p1 = state->p;
+	int64_t w1 = state->w;
+	int64_t v1 = state->v;
+	uint32_t n1 = state->n;
+	uint32_t n = n1 + 1;
+
+	state->n = n;
+	if (n == 1) {
+		state->m = x;
+		return;
+	}
+
+	int64_t normalised = (int64_t)x - m1;
+
+	/* Equation 4 */
+	int64_t p_ = p1 + normalised;
+	/* Equation 5 */
+	int64_t delta_m = p_ / n;
+	/* Equation 6 */
+	state->m = m1 + delta_m;
+	/* Equation 7 */
+	state->p = p_ - (n * delta_m);
+
+	/* Equation 11 */
+	int64_t w_ = w1 + (normalised * normalised) - v1;
+	/* Equation 12 */
+	int64_t w__ = w_ - (2 * delta_m * p_) + (n * delta_m * delta_m);
+	/* Equation 13 */
+	int64_t delta_v = w__ / n1;
+
+	/* Equation 14 */
+	state->v = v1 + delta_v;
+	/* Equation 15 */
+	state->w = w__ - (n1 * delta_v);
+}
