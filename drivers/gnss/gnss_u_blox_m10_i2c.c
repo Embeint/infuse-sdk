66003dbc834b404186b8b22e2eec0eb2319a8558
Jordan Yates <jordan@embeint.com>
2024-11-04 19:45:27 +1000

    gnns: u_blox: fix assert
    The u_blox drivers require `wait_tx` to be true if no callback context
    is given.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- drivers/gnss/gnss_u_blox_m10_i2c.c
+++ drivers/gnss/gnss_u_blox_m10_i2c.c
@@ -405,7 +405,7 @@ static int ubx_m10_i2c_software_standby(const struct device *dev)
 	ubx_msg_simple(&pmreq, UBX_MSG_CLASS_RXM, UBX_MSG_ID_RXM_PMREQ, &payload, sizeof(payload));
 
 	/* We don't expect a response to this command */
-	rc = ubx_modem_send_async(&data->common.modem, &pmreq, NULL, false);
+	rc = ubx_modem_send_async(&data->common.modem, &pmreq, NULL, true);
 	if (rc < 0) {
 		LOG_WRN("SEND FAILED?");
 		return rc;
