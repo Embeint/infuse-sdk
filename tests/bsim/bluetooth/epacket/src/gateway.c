efda2bb63f88a987c9b8e59faecaa1631963783a
Jordan Yates <jordan@embeint.com>
2024-11-05 21:33:01 +1000

    tests: bsim: epacket: test BT connection RPCs
    Test the Bluetooth connection RPCs in CI.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- tests/bsim/bluetooth/epacket/src/gateway.c
+++ tests/bsim/bluetooth/epacket/src/gateway.c
@@ -17,8 +17,10 @@
 
 #include <infuse/epacket/interface.h>
 #include <infuse/epacket/interface/epacket_bt_central.h>
+#include <infuse/epacket/interface/epacket_dummy.h>
 #include <infuse/epacket/packet.h>
 #include <infuse/bluetooth/gatt.h>
+#include <infuse/rpc/types.h>
 
 extern enum bst_result_t bst_result;
 static K_SEM_DEFINE(epacket_adv_received, 0, 1);
@@ -201,22 +203,176 @@ static void main_gateway_connect_then_scan(void)
 	}
 }
 
+static void send_rpc(uint32_t request_id, uint16_t command_id, void *params, size_t params_len)
+{
+	const struct device *epacket_dummy = DEVICE_DT_GET(DT_NODELABEL(epacket_dummy));
+	struct epacket_dummy_frame header = {
+		.type = INFUSE_RPC_CMD,
+		.auth = EPACKET_AUTH_DEVICE,
+		.flags = 0x0000,
+	};
+	struct infuse_rpc_req_header *h = params;
+
+	h->command_id = command_id;
+	h->request_id = request_id;
+
+	/* Push command at RPC server */
+	epacket_dummy_receive(epacket_dummy, &header, params, params_len);
+}
+
+static struct net_buf *expect_response(uint32_t request_id, uint16_t command_id, int rc)
+{
+	struct k_fifo *response_queue = epacket_dummmy_transmit_fifo_get();
+	struct infuse_rpc_rsp_header *response;
+	struct net_buf *rsp;
+
+	/* Response was sent */
+	rsp = net_buf_get(response_queue, K_SECONDS(10));
+	if (rsp == NULL) {
+		LOG_ERR("No response");
+		return NULL;
+	}
+	net_buf_pull_mem(rsp, sizeof(struct epacket_dummy_frame));
+	response = (void *)rsp->data;
+
+	/* Parameters match what we expect */
+	if (request_id != response->request_id) {
+		LOG_ERR("Unexpected request ID (%08X != %08X)", response->request_id, request_id);
+		return NULL;
+	}
+	if (rc != response->return_code) {
+		LOG_ERR("Unexpected return code (%d != %d)", response->return_code, rc);
+		return NULL;
+	}
+
+	/* Return the response */
+	return rsp;
+}
+
+static void main_gateway_rpcs(void)
+{
+	struct rpc_bt_connect_infuse_response *connect_rsp;
+	struct net_buf *buf;
+	bt_addr_le_t addr;
+
+	common_init();
+	if (observe_peer(&addr) < 0) {
+		FAIL("Failed to observe peer");
+		return;
+	}
+
+	struct rpc_bt_connect_infuse_request connect = {
+		.peer =
+			{
+				.type = addr.type,
+				.val =
+					{
+						addr.a.val[0],
+						addr.a.val[1],
+						addr.a.val[2],
+						addr.a.val[3],
+						addr.a.val[4],
+						addr.a.val[5],
+					},
+			},
+		.conn_timeout_ms = 3000,
+		.subscribe = 0,
+		.inactivity_timeout_ms = 0,
+	};
+	struct rpc_bt_disconnect_request disconnect = {
+		.peer = connect.peer,
+	};
+
+	/* Basic connect + disconnect cycle */
+	send_rpc(1, RPC_ID_BT_CONNECT_INFUSE, &connect, sizeof(connect));
+	buf = expect_response(1, RPC_ID_BT_CONNECT_INFUSE, 0);
+	if (buf == NULL) {
+		FAIL("Failed to connect via RPC");
+		return;
+	}
+	connect_rsp = (void *)buf->data;
+	net_buf_unref(buf);
+
+	send_rpc(2, RPC_ID_BT_DISCONNECT, &disconnect, sizeof(disconnect));
+	buf = expect_response(2, RPC_ID_BT_DISCONNECT, 0);
+	if (buf == NULL) {
+		FAIL("Unexpected disconnection result");
+		return;
+	}
+	net_buf_unref(buf);
+
+	/* Connect timeout, disconnect should error */
+	connect.conn_timeout_ms = 10;
+	send_rpc(3, RPC_ID_BT_CONNECT_INFUSE, &connect, sizeof(connect));
+	buf = expect_response(3, RPC_ID_BT_CONNECT_INFUSE, -BT_HCI_ERR_UNKNOWN_CONN_ID);
+	if (buf == NULL) {
+		FAIL("Unexpected connection result");
+		return;
+	}
+	connect_rsp = (void *)buf->data;
+	net_buf_unref(buf);
+
+	send_rpc(4, RPC_ID_BT_DISCONNECT, &disconnect, sizeof(disconnect));
+	buf = expect_response(4, RPC_ID_BT_DISCONNECT, -EINVAL);
+	if (buf == NULL) {
+		FAIL("Unexpected disconnection result");
+		return;
+	}
+	net_buf_unref(buf);
+
+	/* Connect with subscribe */
+	connect.conn_timeout_ms = 3000;
+	connect.subscribe =
+		RPC_ENUM_INFUSE_BT_CHARACTERISTIC_COMMAND | RPC_ENUM_INFUSE_BT_CHARACTERISTIC_DATA;
+	send_rpc(5, RPC_ID_BT_CONNECT_INFUSE, &connect, sizeof(connect));
+	buf = expect_response(5, RPC_ID_BT_CONNECT_INFUSE, 0);
+	if (buf == NULL) {
+		FAIL("Failed to connect via RPC");
+		return;
+	}
+	connect_rsp = (void *)buf->data;
+	net_buf_unref(buf);
+
+	send_rpc(6, RPC_ID_BT_DISCONNECT, &disconnect, sizeof(disconnect));
+	buf = expect_response(6, RPC_ID_BT_DISCONNECT, 0);
+	if (buf == NULL) {
+		FAIL("Unexpected disconnection result");
+		return;
+	}
+	net_buf_unref(buf);
+
+	PASS("RPC connecter passed\n");
+}
+
 static const struct bst_test_instance epacket_gateway[] = {
-	{.test_id = "epacket_bt_gateway_scan",
-	 .test_descr = "Scans for advertising ePackets on advertising PHY",
-	 .test_pre_init_f = test_init,
-	 .test_tick_f = test_tick,
-	 .test_main_f = main_gateway_scan},
-	{.test_id = "epacket_bt_gateway_connect",
-	 .test_descr = "Connect to peer device",
-	 .test_pre_init_f = test_init,
-	 .test_tick_f = test_tick,
-	 .test_main_f = main_gateway_connect},
-	{.test_id = "epacket_bt_gateway_connect_then_scan",
-	 .test_descr = "Connect to peer device, then continue scanning",
-	 .test_pre_init_f = test_init,
-	 .test_tick_f = test_tick,
-	 .test_main_f = main_gateway_connect_then_scan},
+	{
+		.test_id = "epacket_bt_gateway_scan",
+		.test_descr = "Scans for advertising ePackets on advertising PHY",
+		.test_pre_init_f = test_init,
+		.test_tick_f = test_tick,
+		.test_main_f = main_gateway_scan,
+	},
+	{
+		.test_id = "epacket_bt_gateway_connect",
+		.test_descr = "Connect to peer device",
+		.test_pre_init_f = test_init,
+		.test_tick_f = test_tick,
+		.test_main_f = main_gateway_connect,
+	},
+	{
+		.test_id = "epacket_bt_gateway_connect_then_scan",
+		.test_descr = "Connect to peer device, then continue scanning",
+		.test_pre_init_f = test_init,
+		.test_tick_f = test_tick,
+		.test_main_f = main_gateway_connect_then_scan,
+	},
+	{
+		.test_id = "epacket_bt_gateway_connect_rpc",
+		.test_descr = "Bluetooth gateway RPCs",
+		.test_pre_init_f = test_init,
+		.test_tick_f = test_tick,
+		.test_main_f = main_gateway_rpcs,
+	},
 	BSTEST_END_MARKER};
 
 struct bst_test_list *test_epacket_bt_gateway(struct bst_test_list *tests)
