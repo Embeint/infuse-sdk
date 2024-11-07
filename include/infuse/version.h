be7c2370d61a63b4b133fd4e5debf466cc53bdaa
Jordan Yates <jordan@embeint.com>
2024-11-07 11:05:29 +1000

    version: move implementation to source file
    Move the `application_version_get` implementation to a source file. This
    enables us to declare the version structure in ROM, which is important
    for minimising the differences between binaries when the version number
    changes.
    
    Previously the number of instructions generated would depend on the
    version number, with consequences for the flash layout in the remainder
    of the application.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- include/infuse/version.h
+++ include/infuse/version.h
@@ -52,31 +52,7 @@ extern "C" {
  *
  * @returns Current application version
  */
-static inline struct infuse_version application_version_get(void)
-{
-#ifdef CONFIG_MCUBOOT_IMG_MANAGER
-	struct mcuboot_img_header header;
-	int rc;
-
-	/* Prefer version as reported by mcuboot */
-	rc = boot_read_bank_header(FIXED_PARTITION_ID(slot0_partition), &header, sizeof(header));
-	if ((rc == 0) && (header.mcuboot_version == 1)) {
-		return header.h.v1.sem_ver;
-	}
-#endif /* CONFIG_MCUBOOT_IMG_MANAGER */
-
-	/* Defines from the build system (should match mcuboot) */
-	return (struct infuse_version){
-		.major = APP_VERSION_MAJOR,
-		.minor = APP_VERSION_MINOR,
-		.revision = APP_PATCHLEVEL,
-#ifdef APP_GIT_COMMIT_HASH_SHORT
-		.build_num = APP_GIT_COMMIT_HASH_SHORT,
-#else
-		.build_num = APP_TWEAK,
-#endif
-	};
-}
+struct infuse_version application_version_get(void);
 
 /**
  * @brief Compare two version structures
---
+++ lib/version.c
@@ -0,0 +1,37 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <infuse/version.h>
+
+/* Defines from the build system (should match mcuboot) */
+static const struct infuse_version build_version = {
+	.major = APP_VERSION_MAJOR,
+	.minor = APP_VERSION_MINOR,
+	.revision = APP_PATCHLEVEL,
+#ifdef APP_GIT_COMMIT_HASH_SHORT
+	.build_num = APP_GIT_COMMIT_HASH_SHORT,
+#else
+	.build_num = APP_TWEAK,
+#endif
+};
+
+struct infuse_version application_version_get(void)
+{
+#ifdef CONFIG_MCUBOOT_IMG_MANAGER
+	struct mcuboot_img_header header;
+	int rc;
+
+	/* Prefer version as reported by mcuboot */
+	rc = boot_read_bank_header(FIXED_PARTITION_ID(slot0_partition), &header, sizeof(header));
+	if ((rc == 0) && (header.mcuboot_version == 1)) {
+		return header.h.v1.sem_ver;
+	}
+#endif /* CONFIG_MCUBOOT_IMG_MANAGER */
+
+	return build_version;
+}
