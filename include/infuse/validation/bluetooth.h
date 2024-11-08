d36dc2790de39791adcfe9937dd54056f384b9a1
Jordan Yates <jordan@embeint.com>
2024-11-08 15:53:41 +1000

    validation: bluetooth: added
    Add basic Bluetooth validation.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ include/infuse/validation/bluetooth.h
@@ -0,0 +1,40 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#ifndef INFUSE_SDK_INCLUDE_INFUSE_VALIDATION_BLUETOOTH_H_
+#define INFUSE_SDK_INCLUDE_INFUSE_VALIDATION_BLUETOOTH_H_
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/util.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @defgroup validation_bluetooth Bluetooth validation
+ * @{
+ */
+
+enum {
+	VALIDATION_BLUETOOTH_INIT = 0,
+	VALIDATION_BLUETOOTH_ADV_TX = BIT(0),
+};
+
+int infuse_validation_bluetooth(uint8_t flags);
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INFUSE_SDK_INCLUDE_INFUSE_VALIDATION_BLUETOOTH_H_ */
