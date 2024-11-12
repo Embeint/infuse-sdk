f71def92e24ee0fd708ea5e06e99b36b788ca67b
Jordan Yates <jordan@embeint.com>
2024-11-12 12:52:41 +1000

    auto: charger control: added
    Add a library that automatically controls the enable state of a charger
    based on environmental temperatures measured by the application.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ include/infuse/auto/charger_control.h
@@ -0,0 +1,43 @@
+/**
+ * @file
+ * @brief Automatically control battery charging based on temperatue
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ *
+ * @details
+ */
+
+#ifndef INFUSE_SDK_INCLUDE_INFUSE_AUTO_CHARGER_CONTROL_H_
+#define INFUSE_SDK_INCLUDE_INFUSE_AUTO_CHARGER_CONTROL_H_
+
+#include <stdint.h>
+
+#include <zephyr/sys/util.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @defgroup charger_control_apis Auto: Charger control
+ * @{
+ */
+
+/**
+ * @brief Control logging of charge control events
+ *
+ * @param tdf_logger_mask TDF data logger mask to log events to
+ */
+void auto_charger_control_log_configure(uint8_t tdf_logger_mask);
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INFUSE_SDK_INCLUDE_INFUSE_AUTO_CHARGER_CONTROL_H_ */
