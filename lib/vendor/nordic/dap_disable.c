55f66d8c131de4eb064dc33208856d0bb61a1959
Jordan Yates <jordan@embeint.com>
2024-11-17 16:52:15 +1000

    lib: common_boot: debug port disable
    Add an option to automatically disable the debug port at application
    boot. Currently only available for nRF52 devices. TF-M applications
    have an alternate option already.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ lib/vendor/nordic/dap_disable.c
@@ -0,0 +1,38 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ *
+ * From Nordic blog post:
+ * https://devzone.nordicsemi.com/nordic/nordic-blog/b/blog/posts/working-with-the-nrf52-series-improved-approtect
+ */
+
+#include <zephyr/sys/reboot.h>
+
+#include <nrf.h>
+
+void infuse_security_disable_dap(void)
+{
+	if ((NRF_UICR->APPROTECT & UICR_APPROTECT_PALL_Msk) ==
+	    (UICR_APPROTECT_PALL_Enabled << UICR_APPROTECT_PALL_Pos)) {
+		/* APPROTECT already written to enabled state */
+		return;
+	}
+	/* Enable writing to flash */
+	NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen;
+	while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
+	}
+	/* Write the APPROTECT register to enabled state */
+	NRF_UICR->APPROTECT = ((NRF_UICR->APPROTECT & ~((uint32_t)UICR_APPROTECT_PALL_Msk)) |
+			       (UICR_APPROTECT_PALL_Enabled << UICR_APPROTECT_PALL_Pos));
+
+	/* Wait for the write to complete */
+	NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren;
+	while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
+	}
+
+	/* Reboot so that configuration is applied */
+	sys_reboot(SYS_REBOOT_WARM);
+}
