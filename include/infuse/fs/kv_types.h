6462d241fe8ac13d569b12fa62fb1c8ced855d71
Jordan Yates <jordan@embeint.com>
2024-11-29 13:33:53 +1000

    fs: kv_store: Bluetooth peer device
    Bluetooth peer device KV definition.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- include/infuse/fs/kv_types.h
+++ include/infuse/fs/kv_types.h
@@ -245,6 +245,12 @@ struct kv_lte_pdp_config {
 	} __packed
 /* clang-format on */
 
+/* Bluetooth peer device */
+struct kv_bluetoooth_peer {
+	/* Peer device Bluetooth address */
+	struct bt_addr_le address;
+} __packed;
+
 /* Array of points defining a closed polygon */
 struct kv_geofence {
 	/* Points in geofence */
@@ -311,6 +317,8 @@ enum kv_builtin_id {
 	KV_KEY_LTE_SIM_UICC = 44,
 	/* Packet Data Protocol (PDP) default context configuration */
 	KV_KEY_LTE_PDP_CONFIG = 45,
+	/* Bluetooth peer device */
+	KV_KEY_BLUETOOOTH_PEER = 50,
 	/* Array of points defining a closed polygon */
 	KV_KEY_GEOFENCE = 100,
 #ifdef CONFIG_KV_STORE_KEY_GEOFENCE_RANGE
@@ -341,6 +349,7 @@ enum kv_builtin_size {
 	_KV_KEY_FIXED_LOCATION_SIZE = sizeof(struct kv_fixed_location),
 	_KV_KEY_EPACKET_UDP_PORT_SIZE = sizeof(struct kv_epacket_udp_port),
 	_KV_KEY_LTE_MODEM_IMEI_SIZE = sizeof(struct kv_lte_modem_imei),
+	_KV_KEY_BLUETOOOTH_PEER_SIZE = sizeof(struct kv_bluetoooth_peer),
 };
 
 /* clang-format off */
@@ -361,6 +370,7 @@ enum kv_builtin_size {
 #define _KV_KEY_LTE_MODEM_IMEI_TYPE struct kv_lte_modem_imei
 #define _KV_KEY_LTE_SIM_UICC_TYPE struct kv_lte_sim_uicc
 #define _KV_KEY_LTE_PDP_CONFIG_TYPE struct kv_lte_pdp_config
+#define _KV_KEY_BLUETOOOTH_PEER_TYPE struct kv_bluetoooth_peer
 #define _KV_KEY_GEOFENCE_TYPE struct kv_geofence
 #define _KV_KEY_SECURE_STORAGE_RESERVED_TYPE struct kv_secure_storage_reserved
 /* clang-format on */
@@ -383,6 +393,7 @@ enum kv_builtin_size {
 	IF_ENABLED(CONFIG_KV_STORE_KEY_LTE_MODEM_IMEI, (1 +)) \
 	IF_ENABLED(CONFIG_KV_STORE_KEY_LTE_SIM_UICC, (1 +)) \
 	IF_ENABLED(CONFIG_KV_STORE_KEY_LTE_PDP_CONFIG, (1 +)) \
+	IF_ENABLED(CONFIG_KV_STORE_KEY_BLUETOOOTH_PEER, (1 +)) \
 	IF_ENABLED(CONFIG_KV_STORE_KEY_GEOFENCE, (CONFIG_KV_STORE_KEY_GEOFENCE_RANGE +)) \
 	0)
 /* clang-format on */
