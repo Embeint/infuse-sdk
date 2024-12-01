bf356481c243b26eee3b0023fa15d884c570d957
Jordan Yates <jordan@embeint.com>
2024-12-01 19:36:26 +1000

    task_runner: runner: remove task runner workqueue
    Remove the task runner workqueue in favor of the general Infuse-IoT
    workqueue.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- include/infuse/task_runner/runner.h
+++ include/infuse/task_runner/runner.h
@@ -29,13 +29,6 @@ extern "C" {
  */
 
 /**
- * @brief Retrieve the work queue thread used by the task runner
- *
- * @return Workqueue pointer
- */
-struct k_work_q *task_runner_work_q(void);
-
-/**
  * @brief Initialise the task runner with a schedule list
  *
  * @param schedules Task schedules to evaluate
