9de3e3b2b7fed21083945d2abd1556f8e1414ca0
Jordan Yates <jordan@embeint.com>
2024-12-03 10:45:22 +1000

    tests: task_runner: schedules: test custom task arguments
    Ensure that `CONFIG_TASK_RUNNER_CUSTOM_TASK_DEFINITIONS` results in
    custom task arguments being available in `struct task_schedule`.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ tests/subsys/task_runner/schedules/include/custom_tasks.h
@@ -0,0 +1,40 @@
+/**
+ * @file
+ * @brief Demonstration of `TASK_RUNNER_CUSTOM_TASK_DEFINITIONS_PATH`
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#ifndef INFUSE_SDK_TESTS_SUBSYS_TASK_RUNNER_SCHEDULES_INCLUDE_CUSTOM_TASKS_H_
+#define INFUSE_SDK_TESTS_SUBSYS_TASK_RUNNER_SCHEDULES_INCLUDE_CUSTOM_TASKS_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct custom_args_1 {
+	uint32_t arg1;
+	int16_t arg2;
+	uint8_t arg3[5];
+} __packed;
+
+struct custom_args_2 {
+	uint8_t arg1;
+	int32_t arg2;
+	uint16_t arg3[5];
+} __packed;
+
+union custom_task_arguments {
+	struct custom_args_1 custom1;
+	struct custom_args_2 custom2;
+} __packed;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INFUSE_SDK_TESTS_SUBSYS_TASK_RUNNER_SCHEDULES_INCLUDE_CUSTOM_TASKS_H_ */
