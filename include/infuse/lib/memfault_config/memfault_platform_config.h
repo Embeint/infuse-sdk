616b7a4a9327d95faa57a87d7282a3e39add1e55
Jordan Yates <jordan@embeint.com>
2025-01-07 21:03:29 +1000

    memfault: optional platform configuration file
    Add optional configuration files that allow for updating Memfault
    platform defaults.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ include/infuse/lib/memfault_config/memfault_platform_config.h
@@ -0,0 +1,10 @@
+/**
+ * @file
+ * @brief Infuse-IoT Memfault configuration
+ * @copyright 2025 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#define MEMFAULT_METRICS_HEARTBEAT_INTERVAL_SECS CONFIG_MEMFAULT_METRICS_HEARTBEAT_INTERVAL_SECS
