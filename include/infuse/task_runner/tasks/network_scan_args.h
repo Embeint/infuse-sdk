761c05569369165b86b366d76802480a0355d876
Jordan Yates <jordan@embeint.com>
2025-01-22 20:38:49 +1000

    task_runner: tasks: network_scan: support Wi-Fi
    Add support for Wi-Fi AP scanning to the `network_scan` task.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- include/infuse/task_runner/tasks/network_scan_args.h
+++ include/infuse/task_runner/tasks/network_scan_args.h
@@ -21,16 +21,54 @@ extern "C" {
 enum {
 	/** @ref TDF_LTE_TAC_CELLS */
 	TASK_NETWORK_SCAN_LOG_LTE_CELLS = BIT(0),
+	/** @ref TDF_WIFI_AP_INFO */
+	TASK_NETWORK_SCAN_LOG_WIFI_AP = BIT(1),
 };
 
 enum {
+	/** Scan nearby LTE cells */
 	TASK_NETWORK_SCAN_FLAGS_LTE_CELLS = BIT(0),
+	/** Scan nearby Wi-Fi access points */
+	TASK_NETWORK_SCAN_FLAGS_WIFI_CELLS = BIT(1),
+	/** Skip LTE scan if @a desired_aps Wi-Fi access points found */
+	TASK_NETWORK_SCAN_FLAGS_SKIP_LTE_IF_WIFI_GOOD = BIT(7),
+};
+
+enum {
+	/**
+	 * A single access point can broadcast multiple networks simultaneously.
+	 * This can usually be detected through the BSSID, which is the same for all networks
+	 * being broadcast except the least significant nibble (4 bits).
+	 * Reporting multiple networks from the same AP is generally not useful for localisation
+	 * purposes. When set, multiple networks from the same AP will be reported anyway.
+	 */
+	TASK_NETWORK_SCAN_WIFI_FLAGS_INCLUDE_DUPLICATES = BIT(0),
+	/**
+	 * Reporting networks that use a locally administered BSSID is generally not useful for
+	 * localisation purposes. When set, locally administered BSSIDs' are reported anyway.
+	 */
+	TASK_NETWORK_SCAN_WIFI_FLAGS_INCLUDE_LOCALLY_ADMINISTERED = BIT(0),
+	/** If enabled, scan Wi-Fi channels over multiple calls in order of most to least
+	 * common. Scanning terminates as soon as @a desired_aps is reached.
+	 */
+	TASK_NETWORK_SCAN_WIFI_FLAGS_SCAN_PROGRESSIVE = BIT(1),
+	/** Active scanning, default is passive */
+	TASK_NETWORK_SCAN_WIFI_FLAGS_SCAN_ACTIVE = BIT(2),
 };
 
 /** @brief Network scan task arguments */
 struct task_network_scan_args {
 	/** Meta operation flags */
 	uint8_t flags;
+	/** Wi-Fi AP scanning arguments */
+	struct {
+		/** Wi-Fi scanning flags */
+		uint8_t flags;
+		/** Number of unique access-points we want */
+		uint8_t desired_aps;
+		/** Maximum number of access-points to report */
+		uint8_t max_aps;
+	} wifi;
 	/** LTE Cell scanning arguments */
 	struct {
 		/**
