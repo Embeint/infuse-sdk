11d435a0c6f869d0a4f2d235da9a318b97636289
Jordan Yates <jordan@embeint.com>
2025-02-03 09:26:07 +1000

    gnss: dummy driver for nRF9x GNSS
    Add a dummy driver for nRF9x devices to use for GNSS. This allows valid
    devicetree nodes to be used in other subsystem logic.
    
    Currently all users of the nRF9x GNSS are expected to use
    `nrf_modem_gnss.h` directly.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ drivers/gnss/gnss_nrf9x.c
@@ -0,0 +1,26 @@
+/**
+ * @file
+ * @copyright 2025 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ *
+ * This "driver" exists purely to provide a devicetree handle to higher-level
+ * code that expects one. Actual usage of the built-in GNSS modem should be
+ * done directly through the API in `nrf_modem_gnss.h`.
+ */
+
+#include <zephyr/device.h>
+
+#define DT_DRV_COMPAT nordic_nrf9x_gnss
+
+static int nrf9x_gnss_init(const struct device *dev)
+{
+	return 0;
+}
+
+#define NRF9X_GNSS_INST(inst)                                                                      \
+	DEVICE_DT_INST_DEFINE(inst, nrf9x_gnss_init, NULL, NULL, NULL, POST_KERNEL,                \
+			      CONFIG_SENSOR_INIT_PRIORITY, NULL);
+
+DT_INST_FOREACH_STATUS_OKAY(NRF9X_GNSS_INST);
