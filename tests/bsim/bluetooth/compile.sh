3d351a3790123114c87b1a0c1f9a863b7823ccac
Jordan Yates <jordan@embeint.com>
2025-03-07 20:14:02 +1000

    tests: bsim: bluetooth: task_runner: added
    Add tests for the `bt_scanner` task.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- tests/bsim/bluetooth/compile.sh
+++ tests/bsim/bluetooth/compile.sh
@@ -14,12 +14,15 @@ source ${ZEPHYR_BASE}/tests/bsim/compile.source
 
 APP_EPACKET=tests/bsim/bluetooth/epacket
 APP_GATT=tests/bsim/bluetooth/gatt
+APP_TASK_RUNNER=tests/bsim/bluetooth/task_runner
 
 app_root=$INFUSE_BASE app=$APP_EPACKET conf_file=prj_legacy_adv.conf snippet=infuse compile
 app_root=$INFUSE_BASE app=$APP_EPACKET conf_file=prj_conn_terminator.conf snippet=infuse compile
 app_root=$INFUSE_BASE app=$APP_EPACKET conf_file=prj_device.conf snippet=infuse compile
 app_root=$INFUSE_BASE app=$APP_EPACKET conf_file=prj_device_public.conf snippet=infuse compile
+app_root=$INFUSE_BASE app=$APP_EPACKET conf_file=prj_device_alt_network.conf snippet=infuse compile
 app_root=$INFUSE_BASE app=$APP_EPACKET conf_file=prj_gateway.conf snippet=infuse compile
 app_root=$INFUSE_BASE app=$APP_GATT snippet=infuse compile
+app_root=$INFUSE_BASE app=$APP_TASK_RUNNER snippet=infuse compile
 
 wait_for_background_jobs
---
+++ tests/bsim/bluetooth/task_runner/tests_scripts/bt_scanner_defer_filter.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# Copyright (c) 2025 Embeint Inc
+# SPDX-License-Identifier: Apache-2.0
+
+source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
+
+simulation_id="bt_scanner_defer_filter"
+verbosity_level=2
+
+cd ${BSIM_OUT_PATH}/bin
+
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_task_runner_prj_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=0 -RealEncryption=0 \
+  -testid=bt_scanner_defer_filter -rs=10
+
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_epacket_prj_device_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=1 -RealEncryption=0 \
+  -testid=epacket_bt_device -rs=20
+
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_epacket_prj_device_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=2 -RealEncryption=0 \
+  -testid=epacket_bt_device -rs=21
+
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_epacket_prj_device_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=3 -RealEncryption=0 \
+  -testid=epacket_bt_device -rs=22
+
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_epacket_prj_device_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=4 -RealEncryption=0 \
+  -testid=epacket_bt_device -rs=23
+
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_epacket_prj_device_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=5 -RealEncryption=0 \
+  -testid=epacket_bt_device -rs=24
+
+Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
+  -D=6 -sim_length=10e6 $@
+
+wait_for_background_jobs
