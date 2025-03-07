1a500f7d724a81354db55631f59c1824576f6941
Jordan Yates <jordan@embeint.com>
2025-03-07 15:26:42 +1000

    vendor: posix: random Infuse-IoT IDs for babblesim
    Allocate babblesim devices random Infuse-IoT addresses to that we don't
    get ID collisions in tests.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- lib/vendor/posix/identifiers.c
+++ lib/vendor/posix/identifiers.c
@@ -8,11 +8,17 @@
 
 #include <stdint.h>
 
+#if defined(CONFIG_SOC_SERIES_BSIM_NRFXX)
+#include <nrf.h>
+#endif
+
 uint64_t vendor_infuse_device_id(void)
 {
 	/* Hardcoded ID */
-#ifdef CONFIG_INFUSE_SECURITY_TEST_CREDENTIALS
+#if defined(CONFIG_INFUSE_SECURITY_TEST_CREDENTIALS)
 	return 0xFFFFFFFFFFFFFFFDULL;
+#elif defined(CONFIG_SOC_SERIES_BSIM_NRFXX)
+	return 0xB000000000000000ULL + NRF_FICR->DEVICEADDR[0];
 #else
 	return 0x0000000001000000ULL;
 #endif /* CONFIG_INFUSE_SECURITY_TEST_CREDENTIALS */
