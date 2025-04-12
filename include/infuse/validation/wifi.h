423c846b8252650781c9cc94f9481e68fd4efbf5
Jordan Yates <jordan@embeint.com>
2025-04-12 15:17:46 +1000

    validation: wifi: added
    Add a validation test for the Wi-Fi interface. Supports scanning,
    connecting and running a SNTP query.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ include/infuse/validation/wifi.h
@@ -0,0 +1,54 @@
+/**
+ * @file
+ * @copyright 2025 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#ifndef INFUSE_SDK_INCLUDE_INFUSE_VALIDATION_WIFI_H_
+#define INFUSE_SDK_INCLUDE_INFUSE_VALIDATION_WIFI_H_
+
+#include <zephyr/net/net_if.h>
+#include <zephyr/sys/util.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @defgroup validation_wifi Wi-Fi validation API
+ * @{
+ */
+
+enum {
+	/** Validate that Wi-Fi interface powers up */
+	VALIDATION_WIFI_POWER_UP = 0,
+	/** Validate that Wi-Fi network scan completes */
+	VALIDATION_WIFI_SSID_SCAN = BIT(0),
+	/** Validate that Wi-Fi connects to defined network */
+	VALIDATION_WIFI_CONNECT = BIT(1),
+	/** Validate that Wi-Fi can query the current time */
+	VALIDATION_WIFI_SNTP_QUERY = BIT(2),
+};
+
+/**
+ * @brief Validate the behaviour of a Wi-Fi device
+ *
+ * @param iface Networking interface
+ * @param flags Validation tests to run
+ *
+ * @retval 0 On success
+ * @retval -errno On failure
+ */
+int infuse_validation_wifi(struct net_if *iface, uint8_t flags);
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INFUSE_SDK_INCLUDE_INFUSE_VALIDATION_WIFI_H_ */
