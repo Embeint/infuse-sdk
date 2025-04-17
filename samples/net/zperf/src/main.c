a879c56d5814a28869a397d716223f010557abdb
Jordan Yates <jordan@embeint.com>
2025-04-17 16:44:04 +1000

    samples: net: zperf: added
    Add a sample that can be used with iperf to measure upload data
    throughput.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ samples/net/zperf/src/main.c
@@ -0,0 +1,40 @@
+/**
+ * @file
+ * @copyright 2025 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <stdio.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/net/conn_mgr_connectivity.h>
+
+#include <infuse/epacket/interface.h>
+#include <infuse/epacket/packet.h>
+
+LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
+
+int main(void)
+{
+	const struct device *epacket_serial = DEVICE_DT_GET(DT_NODELABEL(epacket_serial));
+
+	/* Always listening on Bluetooth and serial */
+	epacket_receive(epacket_serial, K_FOREVER);
+
+	/* Send key identifiers on boot */
+	epacket_send_key_ids(epacket_serial, K_FOREVER);
+
+	/* Always want network connectivity */
+	conn_mgr_all_if_up(false);
+	conn_mgr_all_if_connect(false);
+
+	/* Loop forever, zperf runs from the RPC context */
+	for (;;) {
+		LOG_INF("Uptime: %6d seconds", k_uptime_seconds());
+		k_sleep(K_SECONDS(1));
+	}
+	return 0;
+}
