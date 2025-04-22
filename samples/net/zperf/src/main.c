9462b3b0980ec474618228e3de195b711793e5ab
Jordan Yates <jordan@embeint.com>
2025-04-22 11:45:43 +1000

    samples: net: zperf: higher CPU frequency
    For nRF5340 based boards, the CPU frequency needs to be boosted from
    64MHz to 128MHz for optimal throughput. This approximately doubles the
    throughput.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- samples/net/zperf/src/main.c
+++ samples/net/zperf/src/main.c
@@ -8,6 +8,10 @@
 
 #include <stdio.h>
 
+#ifdef CONFIG_SOC_NRF5340_CPUAPP
+#include <nrfx_clock.h>
+#endif /* CONFIG_SOC_NRF5340_CPUAPP */
+
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/net/conn_mgr_connectivity.h>
@@ -20,6 +24,17 @@ LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
 int main(void)
 {
 	const struct device *epacket_serial = DEVICE_DT_GET(DT_NODELABEL(epacket_serial));
+
+#ifdef CONFIG_SOC_NRF5340_CPUAPP
+	int err;
+
+	/* For optimal performance, the CPU frequency needs to be set to 128 MHz */
+	err = nrfx_clock_divider_set(NRF_CLOCK_DOMAIN_HFCLK, NRF_CLOCK_HFCLK_DIV_1);
+	err -= NRFX_ERROR_BASE_NUM;
+	if (err != 0) {
+		LOG_WRN("Failed to set 128 MHz: %d", err);
+	}
+#endif /* CONFIG_SOC_NRF5340_CPUAPP */
 
 	/* Always listening on Bluetooth and serial */
 	epacket_receive(epacket_serial, K_FOREVER);
