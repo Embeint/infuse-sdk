9638996a60bfb6c45405fe7c4bfd7af1c08d35b7
Jordan Yates <jordan@embeint.com>
2025-05-17 11:10:25 +1000

    data_logger: shim: add backend
    Add a shim backend to allow testing higher level
    API usage of backends.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ include/infuse/data_logger/backend/shim.h
@@ -0,0 +1,87 @@
+/**
+ * @file
+ * @copyright 2025 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#ifndef INFUSE_SDK_INCLUDE_INFUSE_DATA_LOGGER_BACKEND_SHIM_H_
+#define INFUSE_SDK_INCLUDE_INFUSE_DATA_LOGGER_BACKEND_SHIM_H_
+
+#include <stdint.h>
+
+#include <zephyr/kernel.h>
+
+#include <infuse/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Data Logger shim backend API
+ * @defgroup data_logger_backend_shim Data Logger shim backend
+ * @{
+ */
+
+/** Data structure used for shim */
+struct data_logger_shim_function_data {
+	struct {
+		uint32_t num_calls;
+		uint32_t block;
+		uint16_t data_len;
+		enum infuse_type data_type;
+		int rc;
+	} write;
+	struct {
+		uint32_t num_calls;
+		uint32_t block;
+		uint16_t block_offset;
+		uint16_t data_len;
+		int rc;
+	} read;
+	struct {
+		uint32_t num_calls;
+		uint32_t phy_block;
+		uint32_t num;
+		struct k_sem *block_until;
+		int rc;
+	} erase;
+	struct {
+		uint32_t num_calls;
+		uint32_t block_hint;
+		struct k_sem *block_until;
+		int rc;
+	} reset;
+};
+
+/**
+ * @brief Re-initialise the logger
+ *
+ * @param dev Device to re-initialise
+ *
+ * @retval 0 on success
+ * @retval -errno on error
+ */
+int logger_shim_init(const struct device *dev);
+
+/**
+ * @brief Get the pointer to the function data struct
+ *
+ * @param dev Device to get pointer from
+ *
+ * @return Pointer to the function data struct
+ */
+struct data_logger_shim_function_data *
+data_logger_backend_shim_data_pointer(const struct device *dev);
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INFUSE_SDK_INCLUDE_INFUSE_DATA_LOGGER_BACKEND_SHIM_H_ */
