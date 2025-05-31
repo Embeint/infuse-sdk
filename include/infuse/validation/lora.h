8510783c229857176f3b4027d2b142133fa0b3bc
Jordan Yates <jordan@embeint.com>
2025-05-31 17:21:55 +1000

    validation: lora: added
    Add validation for LoRa modems, transmitting and receiving a packet.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ include/infuse/validation/lora.h
@@ -0,0 +1,53 @@
+/**
+ * @file
+ * @brief LoRa modem validation
+ * @copyright 2025 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#ifndef INFUSE_SDK_INCLUDE_INFUSE_VALIDATION_LORA_H_
+#define INFUSE_SDK_INCLUDE_INFUSE_VALIDATION_LORA_H_
+
+#include <zephyr/device.h>
+#include <zephyr/sys/util.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @defgroup validation_lora LoRa validation API
+ * @{
+ */
+
+enum {
+	/** Validate that sensor powers up */
+	VALIDATION_LORA_POWER_UP = 0,
+	/** Transmit a packet */
+	VALIDATION_LORA_TX = BIT(0),
+	/** Receive a packet */
+	VALIDATION_LORA_RX = BIT(1),
+};
+
+/**
+ * @brief Validate the behaviour of a LoRa modem
+ *
+ * @param dev LoRa device
+ * @param flags Validation tests to run
+ *
+ * @retval 0 On success
+ * @retval -errno On failure
+ */
+int infuse_validation_lora(const struct device *dev, uint8_t flags);
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INFUSE_SDK_INCLUDE_INFUSE_VALIDATION_LORA_H_ */
---
+++ subsys/validation/validation_lora.c
@@ -0,0 +1,89 @@
+/**
+ * @file
+ * @copyright 2025 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <zephyr/pm/device_runtime.h>
+#include <zephyr/sys/util.h>
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/lora.h>
+#include <infuse/validation/core.h>
+#include <infuse/validation/lora.h>
+
+#define TEST "LORA"
+
+int infuse_validation_lora(const struct device *dev, uint8_t flags)
+{
+	/* Match default parameters from zephyr/samples/drivers/lora for RX compatibility */
+	struct lora_modem_config config = {
+		.frequency = 865100000,
+		.bandwidth = BW_125_KHZ,
+		.datarate = SF_10,
+		.preamble_len = 8,
+		.coding_rate = CR_4_5,
+		.iq_inverted = false,
+		.public_network = false,
+		.tx_power = 30,
+	};
+	char tx_data[] = "validation";
+	uint8_t rx_data[32];
+	int16_t rssi;
+	int8_t snr;
+	int rc = 0;
+
+	VALIDATION_REPORT_INFO(TEST, "DEV=%s", dev->name);
+
+	/* Check init succeeded */
+	if (!device_is_ready(dev)) {
+		VALIDATION_REPORT_ERROR(TEST, "Device not ready");
+		return -ENODEV;
+	}
+
+	if (flags & VALIDATION_LORA_TX) {
+		/* Configure the modem for transmission */
+		config.tx = true;
+		rc = lora_config(dev, &config);
+		if (rc < 0) {
+			VALIDATION_REPORT_ERROR(TEST, "TX Config failed (%d)", rc);
+			goto test_end;
+		}
+		/* Send a packet */
+		VALIDATION_REPORT_VALUE(TEST, "TX_PAYLOAD", "%s", tx_data);
+		rc = lora_send(dev, tx_data, sizeof(tx_data));
+		if (rc < 0) {
+			VALIDATION_REPORT_ERROR(TEST, "Send failed (%d)", rc);
+			goto test_end;
+		}
+	}
+
+	if (flags & VALIDATION_LORA_RX) {
+		config.tx = false;
+		rc = lora_config(dev, &config);
+		if (rc < 0) {
+			VALIDATION_REPORT_ERROR(TEST, "RX Config failed (%d)", rc);
+			goto test_end;
+		}
+		/* Receive a packet */
+		VALIDATION_REPORT_INFO(TEST, "Waiting for packet");
+		rc = lora_recv(dev, rx_data, sizeof(rx_data), K_SECONDS(5), &rssi, &snr);
+		if (rc < 0) {
+			VALIDATION_REPORT_ERROR(TEST, "Failed to receive packet (%d)", rc);
+			goto test_end;
+		}
+		/* Report parameters */
+		VALIDATION_REPORT_VALUE(TEST, "RX_LEN", "%d", rc);
+		VALIDATION_REPORT_VALUE(TEST, "RX_RSSI", "%d", rssi);
+		VALIDATION_REPORT_VALUE(TEST, "RX_SNR", "%d", snr);
+		rc = 0;
+	}
+test_end:
+	if (rc == 0) {
+		VALIDATION_REPORT_PASS(TEST, "DEV=%s", dev->name);
+	}
+
+	return rc;
+}
