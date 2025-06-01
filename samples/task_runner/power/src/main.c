59fdf30a923e9dbb1954c7de36d87a910bcdf71d
Jordan Yates <jordan@embeint.com>
2025-06-01 11:40:12 +1000

    samples: task_runner: power: added
    Add a sample for the power task which frequently monitors the battery
    state and sends it over Bluetooth and serial.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ samples/task_runner/power/src/main.c
@@ -0,0 +1,69 @@
+/**
+ * @file
+ * @copyright 2025 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/net/conn_mgr_connectivity.h>
+
+#include <infuse/drivers/watchdog.h>
+#include <infuse/time/epoch.h>
+#include <infuse/epacket/interface.h>
+#include <infuse/epacket/packet.h>
+#include <infuse/data_logger/high_level/tdf.h>
+#include <infuse/tdf/definitions.h>
+
+#include <infuse/task_runner/runner.h>
+#include <infuse/task_runner/tasks/infuse_tasks.h>
+
+LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
+
+static const struct task_schedule schedules[] = {
+	{
+		.task_id = TASK_ID_BATTERY,
+		.validity = TASK_VALID_ALWAYS,
+		.periodicity_type = TASK_PERIODICITY_FIXED,
+		.periodicity.fixed.period_s = 2,
+	},
+	{
+		.task_id = TASK_ID_TDF_LOGGER,
+		.validity = TASK_VALID_ALWAYS,
+		.periodicity_type = TASK_PERIODICITY_LOCKOUT,
+		.periodicity.lockout.lockout_s = 1,
+		.task_args.infuse.tdf_logger =
+			{
+				.loggers = TDF_DATA_LOGGER_BT_ADV | TDF_DATA_LOGGER_SERIAL,
+				.random_delay_ms = 250,
+				.tdfs = TASK_TDF_LOGGER_LOG_ANNOUNCE | TASK_TDF_LOGGER_LOG_BATTERY,
+			},
+	},
+};
+
+TASK_SCHEDULE_STATES_DEFINE(states, schedules);
+TASK_RUNNER_TASKS_DEFINE(app_tasks, app_tasks_data, (TDF_LOGGER_TASK, NULL),
+			 (BATTERY_TASK, DEVICE_DT_GET(DT_ALIAS(fuel_gauge0))));
+
+int main(void)
+{
+	/* Start the watchdog */
+	(void)infuse_watchdog_start();
+
+#ifdef CONFIG_NETWORKING
+	conn_mgr_all_if_up(false);
+	conn_mgr_all_if_connect(false);
+#endif /* CONFIG_NETWORKING */
+
+	/* Initialise task runner */
+	task_runner_init(schedules, states, ARRAY_SIZE(schedules), app_tasks, app_tasks_data,
+			 ARRAY_SIZE(app_tasks));
+
+	/* Start auto iteration */
+	task_runner_start_auto_iterate();
+
+	/* No more work to do in this context */
+	k_sleep(K_FOREVER);
+}
