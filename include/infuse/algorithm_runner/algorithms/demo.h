692b4ee66d8984c052d48bd3f5e9ab2c257fa773
Jordan Yates <jordan@embeint.com>
2025-06-02 11:48:55 +1000

    algorithm_runner: algorithms: demo algorithms
    Add demonstration algorithms that don't perform useful computations, but
    demonstrate the different types of algorithm outputs that can be
    created. They also serve as a useful source of synthetic data for
    testing.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ include/infuse/algorithm_runner/algorithms/demo.h
@@ -0,0 +1,162 @@
+/**
+ * @file
+ * @brief Demonstration algorithms
+ * @copyright 2025 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#ifndef INFUSE_SDK_INCLUDE_INFUSE_ALGORITHM_RUNNER_ALGORITHMS_DEMO_H_
+#define INFUSE_SDK_INCLUDE_INFUSE_ALGORITHM_RUNNER_ALGORITHMS_DEMO_H_
+
+#include <stdint.h>
+
+#include <zephyr/toolchain.h>
+
+#include <infuse/algorithm_runner/runner.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief demo API
+ * @defgroup demo_apis demo APIs
+ * @{
+ */
+
+enum {
+	ALGORITHM_DEMO_EVENT_LOG = BIT(0),
+	ALGORITHM_DEMO_STATE_LOG = BIT(0),
+	ALGORITHM_DEMO_METRIC_LOG = BIT(0),
+};
+
+struct algorithm_demo_common_config {
+	/** Common algorithm configuration */
+	struct algorithm_runner_common_config common;
+	/* Demo algorithm config */
+	union {
+		uint8_t event_gen_chance;
+		uint16_t compute_metric_len;
+	};
+} __packed;
+
+union algorithm_demo_common_data {
+	uint32_t processed;
+	uint8_t current_state;
+};
+
+/** Algorithm implementation, see @ref algorithm_run_fn */
+void algorithm_demo_event_fn(const struct zbus_channel *chan, const void *config, void *data);
+
+/** Algorithm implementation, see @ref algorithm_run_fn */
+void algorithm_demo_state_fn(const struct zbus_channel *chan, const void *config, void *data);
+
+/** Algorithm implementation, see @ref algorithm_run_fn */
+void algorithm_demo_metric_fn(const struct zbus_channel *chan, const void *config, void *data);
+
+/**
+ * @brief Statically define an instance of the demo event algorithm
+ *
+ * This algorithm randomly generates events, with a percentage chance equal to the argument on
+ * each IMU sample buffer that arrives.
+ *
+ * @param name Variable name base
+ * @param loggers_ TDF loggers to log outputs to
+ * @param tdfs TDFs to log
+ * @param event_chance_percent Chance to emit an event on each buffer
+ */
+#define ALGORITHM_DEMO_EVENT_DEFINE(name, loggers_, tdfs, event_chance_percent)                    \
+	static const struct algorithm_demo_common_config name##_config = {                         \
+		.common =                                                                          \
+			{                                                                          \
+				.algorithm_id = 0xFFFFFFF0,                                        \
+				.zbus_channel = INFUSE_ZBUS_CHAN_IMU,                              \
+				.state_size = sizeof(union algorithm_demo_common_data),            \
+				.logging =                                                         \
+					{                                                          \
+						.loggers = loggers_,                               \
+						.tdf_mask = tdfs,                                  \
+					},                                                         \
+			},                                                                         \
+		.event_gen_chance = event_chance_percent,                                          \
+	};                                                                                         \
+	static union algorithm_demo_common_data name##_data;                                       \
+	static struct algorithm_runner_algorithm name = {                                          \
+		.impl = algorithm_demo_event_fn,                                                   \
+		.config = &name##_config.common,                                                   \
+		.runtime_state = &name##_data,                                                     \
+	}
+
+/**
+ * @brief Statically define an instance of the demo state algorithm
+ *
+ * This algorithm randomly transitions between states on IMU buffers
+ *
+ * @param name Variable name base
+ * @param loggers_ TDF loggers to log outputs to
+ * @param tdfs TDFs to log
+ */
+#define ALGORITHM_DEMO_STATE_DEFINE(name, loggers_, tdfs)                                          \
+	static const struct algorithm_demo_common_config name##_config = {                         \
+		.common =                                                                          \
+			{                                                                          \
+				.algorithm_id = 0xFFFFFFF1,                                        \
+				.zbus_channel = INFUSE_ZBUS_CHAN_IMU,                              \
+				.state_size = sizeof(union algorithm_demo_common_data),            \
+				.logging =                                                         \
+					{                                                          \
+						.loggers = loggers_,                               \
+						.tdf_mask = tdfs,                                  \
+					},                                                         \
+			},                                                                         \
+	};                                                                                         \
+	static union algorithm_demo_common_data name##_data;                                       \
+	static struct algorithm_runner_algorithm name = {                                          \
+		.impl = algorithm_demo_state_fn,                                                   \
+		.config = &name##_config.common,                                                   \
+		.runtime_state = &name##_data,                                                     \
+	}
+
+/**
+ * @brief Statically define an instance of the demo compute algorithm
+ *
+ * This algorithm generates a compute metric, with one metric computed every N samples
+ *
+ * @param name Variable name base
+ * @param loggers_ TDF loggers to log outputs to
+ * @param tdfs TDFs to log
+ * @param metric_compute_len Number of samples to compute metric over
+ */
+#define ALGORITHM_DEMO_METRIC_DEFINE(name, loggers_, tdfs, metric_compute_len)                     \
+	static const struct algorithm_demo_common_config name##_config = {                         \
+		.common =                                                                          \
+			{                                                                          \
+				.algorithm_id = 0xFFFFFFF2,                                        \
+				.zbus_channel = INFUSE_ZBUS_CHAN_IMU,                              \
+				.state_size = sizeof(union algorithm_demo_common_data),            \
+				.logging =                                                         \
+					{                                                          \
+						.loggers = loggers_,                               \
+						.tdf_mask = tdfs,                                  \
+					},                                                         \
+			},                                                                         \
+		.compute_metric_len = metric_compute_len,                                          \
+	};                                                                                         \
+	static union algorithm_demo_common_data name##_data;                                       \
+	static struct algorithm_runner_algorithm name = {                                          \
+		.impl = algorithm_demo_metric_fn,                                                  \
+		.config = &name##_config.common,                                                   \
+		.runtime_state = &name##_data,                                                     \
+	}
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INFUSE_SDK_INCLUDE_INFUSE_ALGORITHM_RUNNER_ALGORITHMS_DEMO_H_ */
---
+++ subsys/algorithm_runner/algorithms/alg_demo.c
@@ -0,0 +1,154 @@
+/**
+ * @file
+ * @copyright 2025 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <zephyr/logging/log.h>
+#include <zephyr/random/random.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/zbus/zbus.h>
+
+#include <infuse/algorithm_runner/algorithms/demo.h>
+#include <infuse/task_runner/tasks/imu.h>
+#include <infuse/time/epoch.h>
+#include <infuse/zbus/channels.h>
+
+LOG_MODULE_REGISTER(alg_demo, LOG_LEVEL_INF);
+
+TDF_ALGORITHM_OUTPUT_VAR(tdf_demo_event_output, 1);
+TDF_ALGORITHM_OUTPUT_VAR(tdf_demo_state_output, 1);
+TDF_ALGORITHM_OUTPUT_VAR(tdf_demo_metric_output, 4);
+
+void algorithm_demo_event_fn(const struct zbus_channel *chan, const void *config, void *data)
+{
+	const struct algorithm_demo_common_config *c = config;
+	struct tdf_demo_event_output tdf;
+	uint8_t rand_100;
+
+	if (chan == NULL) {
+		/* Nothing to setup */
+		return;
+	}
+
+	/* Demo algorithm doesn't use the incoming data at all */
+	zbus_chan_finish(chan);
+
+	rand_100 = sys_rand32_get() % 100;
+	LOG_DBG("Event roll: %d for %d%%", rand_100, c->event_gen_chance);
+	if (rand_100 >= c->event_gen_chance) {
+		return;
+	}
+	LOG_INF("Event generated from %d%% chance", c->event_gen_chance);
+
+	/* Populate the event TDF */
+	tdf.algorithm_id = c->common.algorithm_id;
+	tdf.algorithm_version = 0;
+	tdf.output[0] = rand_100;
+
+	/* Log output TDF */
+	algorithm_runner_tdf_log(&c->common, ALGORITHM_DEMO_EVENT_LOG, TDF_ALGORITHM_OUTPUT,
+				 sizeof(tdf), epoch_time_now(), &tdf);
+}
+
+static const uint8_t demo_state_transitions[4][4] = {
+	/* State 0 transitions (Rarely to state 2 or 3) */
+	{90, 0, 5, 5},
+	/* State 1 transitions (75/25 to stay or return to 0) */
+	{25, 75, 0, 0},
+	/* State 2 transitions (Return to 0 or 1) */
+	{10, 15, 75, 0},
+	/* State 3 transitions */
+	{20, 30, 10, 50},
+};
+
+void algorithm_demo_state_fn(const struct zbus_channel *chan, const void *config, void *data)
+{
+	const struct algorithm_demo_common_config *c = config;
+	union algorithm_demo_common_data *d = data;
+	struct tdf_demo_state_output tdf;
+	const uint8_t *transitions;
+	uint8_t cumulative;
+	uint8_t rand_100;
+	int i;
+
+	if (chan == NULL) {
+		d->current_state = 0;
+		return;
+	}
+
+	/* Demo algorithm doesn't use the incoming data at all */
+	zbus_chan_finish(chan);
+
+	transitions = demo_state_transitions[d->current_state];
+	rand_100 = sys_rand32_get() % 100;
+
+	/* Determine which state we are transitioning to or staying in */
+	cumulative = 0;
+	for (i = 0; i < 4; i++) {
+		cumulative += transitions[i];
+		if (rand_100 < cumulative) {
+			break;
+		}
+	}
+	if (i != d->current_state) {
+		LOG_INF("Transition from %d to %d", d->current_state, i);
+		d->current_state = i;
+
+		/* Populate the event TDF */
+		tdf.algorithm_id = c->common.algorithm_id;
+		tdf.algorithm_version = 0;
+		tdf.output[0] = d->current_state;
+
+		/* Log output TDF */
+		algorithm_runner_tdf_log(&c->common, ALGORITHM_DEMO_STATE_LOG, TDF_ALGORITHM_OUTPUT,
+					 sizeof(tdf), epoch_time_now(), &tdf);
+
+	} else {
+		LOG_DBG("Remain in %d", i);
+	}
+}
+
+void algorithm_demo_metric_fn(const struct zbus_channel *chan, const void *config, void *data)
+{
+	const struct algorithm_demo_common_config *c = config;
+	union algorithm_demo_common_data *d = data;
+	const struct imu_sample_array *samples;
+	struct tdf_demo_metric_output tdf;
+	k_ticks_t t_event;
+	uint32_t metric;
+
+	if (chan == NULL) {
+		d->processed = 0;
+		return;
+	}
+
+	samples = chan->message;
+
+	for (int i = 0; i < samples->accelerometer.num; i++) {
+		if (++d->processed < c->compute_metric_len) {
+			continue;
+		}
+		d->processed = 0;
+
+		/* Compute the metric (uptime) */
+		metric = k_uptime_seconds();
+		LOG_INF("Metric: %d", metric);
+
+		/* Populate the metric TDF */
+		tdf.algorithm_id = c->common.algorithm_id;
+		tdf.algorithm_version = 0;
+		sys_put_le32(metric, tdf.output);
+
+		t_event = imu_sample_timestamp(&samples->accelerometer, i);
+
+		/* Log output TDF */
+		algorithm_runner_tdf_log(&c->common, ALGORITHM_DEMO_METRIC_LOG,
+					 TDF_ALGORITHM_OUTPUT, sizeof(tdf),
+					 epoch_time_from_ticks(t_event), &tdf);
+	}
+
+	zbus_chan_finish(chan);
+}
