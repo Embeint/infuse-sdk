3a22dc6bc41fc19cc255983ce5d799ccef1bc307
Jordan Yates <jordan@embeint.com>
2025-06-02 13:12:18 +1000

    tests: bsim: bluetooth: test name updates
    Validate that broadcast names are updated when KV slots are updated.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- tests/bsim/bluetooth/epacket/src/legacy_adv.c
+++ tests/bsim/bluetooth/epacket/src/legacy_adv.c
@@ -6,6 +6,7 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/bluetooth/bluetooth.h>
 
 #include "common.h"
 
@@ -17,6 +18,8 @@
 #include <infuse/bluetooth/legacy_adv.h>
 #include <infuse/epacket/interface.h>
 #include <infuse/data_logger/high_level/tdf.h>
+#include <infuse/fs/kv_store.h>
+#include <infuse/fs/kv_types.h>
 #include <infuse/tdf/definitions.h>
 
 extern enum bst_result_t bst_result;
@@ -69,13 +72,138 @@ static void main_epacket_bt_legacy_broadcast(void)
 	PASS("Legacy advertising device complete\n");
 }
 
+#define NAME_1 "BOB"
+#define NAME_2 "SALLY"
+
+static void main_legacy_adv_name_update(void)
+{
+	KV_STRING_CONST(default_name, NAME_1);
+	KV_STRING_CONST(updated_name, NAME_2);
+
+	if (kv_store_write(KV_KEY_DEVICE_NAME, &default_name, sizeof(default_name)) < 0) {
+		FAIL("Failed to write device name\n");
+		return;
+	}
+
+	if (bluetooth_legacy_advertising_run() < 0) {
+		FAIL("Failed to start legacy advertiser\n");
+		return;
+	}
+
+	k_sleep(K_SECONDS(2));
+
+	/* Write a new name to the store */
+	if (kv_store_write(KV_KEY_DEVICE_NAME, &updated_name, sizeof(updated_name)) < 0) {
+		FAIL("Failed to write updated device name\n");
+		return;
+	}
+
+	k_sleep(K_SECONDS(2));
+
+	/* Delete the name */
+	if (kv_store_delete(KV_KEY_DEVICE_NAME) < 0) {
+		FAIL("Failed to write delete device name\n");
+		return;
+	}
+
+	k_sleep(K_SECONDS(2));
+
+	PASS("Legacy advertising device complete\n");
+}
+
+static bool seen_name_1;
+static bool seen_name_2;
+static bool seen_name_default;
+
+static void scan_cb(const bt_addr_le_t *addr, int8_t rssi, uint8_t adv_type,
+		    struct net_buf_simple *buf)
+{
+	const char *name;
+
+	/* Only look at legacy advertising packets with a name */
+	if (adv_type != BT_GAP_ADV_TYPE_ADV_IND) {
+		return;
+	}
+	if (buf->data[1] != BT_DATA_NAME_COMPLETE) {
+		return;
+	}
+	name = buf->data + 2;
+
+	/* Compare against the names we expect to see */
+	if (buf->data[0] == (1 + strlen(NAME_1)) && strncmp(NAME_1, name, strlen(NAME_1)) == 0) {
+		seen_name_1 = true;
+	}
+	if (buf->data[0] == (1 + strlen(NAME_2)) && strncmp(NAME_2, name, strlen(NAME_2)) == 0) {
+		seen_name_2 = true;
+	}
+	if (buf->data[0] == (1 + strlen(CONFIG_BT_DEVICE_NAME)) &&
+	    strncmp(CONFIG_BT_DEVICE_NAME, name, strlen(CONFIG_BT_DEVICE_NAME)) == 0) {
+		seen_name_default = true;
+	}
+}
+
+static void main_legacy_adv_name_watcher(void)
+{
+	static const struct bt_le_scan_param scan_param = {
+		.type = BT_LE_SCAN_TYPE_PASSIVE,
+		.options = BT_LE_SCAN_OPT_NONE,
+		/* 32 * 0.625 = 20ms */
+		.interval = 0x0020,
+		.window = 0x0020,
+	};
+	int rc;
+
+	/* Run scanning for 8 seconds */
+	rc = bt_le_scan_start(&scan_param, scan_cb);
+	if (rc < 0) {
+		FAIL("Failed to start scanning\n");
+	}
+
+	k_sleep(K_SECONDS(8));
+
+	(void)bt_le_scan_stop();
+
+	/* Expect to have seen all 3 names */
+	if (!seen_name_1) {
+		FAIL("Failed to observe '%s'\n", NAME_1);
+		return;
+	}
+	if (!seen_name_2) {
+		FAIL("Failed to observe '%s'\n", NAME_2);
+		return;
+	}
+	if (!seen_name_default) {
+		FAIL("Failed to observe '%s'\n", CONFIG_BT_DEVICE_NAME);
+		return;
+	}
+
+	PASS("Legacy advertising name watcher complete\n");
+}
+
 static const struct bst_test_instance legacy_adv_advertiser[] = {
-	{.test_id = "epacket_bt_legacy_adv",
-	 .test_descr = "Basic Infuse-IoT Bluetooth device advertising on legacy channels",
-	 .test_pre_init_f = test_init,
-	 .test_tick_f = test_tick,
-	 .test_main_f = main_epacket_bt_legacy_broadcast},
-	BSTEST_END_MARKER};
+	{
+		.test_id = "epacket_bt_legacy_adv",
+		.test_descr = "Basic Infuse-IoT Bluetooth device advertising on legacy channels",
+		.test_pre_init_f = test_init,
+		.test_tick_f = test_tick,
+		.test_main_f = main_epacket_bt_legacy_broadcast,
+	},
+	{
+		.test_id = "legacy_adv_name_update",
+		.test_descr = "Test updating the device name through KV store",
+		.test_pre_init_f = test_init,
+		.test_tick_f = test_tick,
+		.test_main_f = main_legacy_adv_name_update,
+	},
+	{
+		.test_id = "legacy_adv_name_scanner",
+		.test_descr = "Watch names being advertised",
+		.test_pre_init_f = test_init,
+		.test_tick_f = test_tick,
+		.test_main_f = main_legacy_adv_name_watcher,
+	},
+	BSTEST_END_MARKER,
+};
 
 struct bst_test_list *test_legacy_adv(struct bst_test_list *tests)
 {
