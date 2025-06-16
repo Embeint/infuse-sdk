e1b697508a7015605ffe101ca3c76028bd96d7db
Jordan Yates <jordan@embeint.com>
2025-06-16 15:56:10 +1000

    rpc: commands: heap_stats: added
    Add a command for querying heap statistics on remote devices.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- generated/include/infuse/rpc/commands_impl.h
+++ generated/include/infuse/rpc/commands_impl.h
@@ -179,6 +179,15 @@ struct net_buf *rpc_command_infuse_states_update(struct net_buf *request);
 struct net_buf *rpc_command_data_logger_erase(struct net_buf *request);
 
 /**
+ * @brief Run heap_stats RPC
+ *
+ * @param request @ref INFUSE_RPC_CMD packet to respond to
+ *
+ * @return struct net_buf* @ref INFUSE_RPC_RSP packet buffer
+ */
+struct net_buf *rpc_command_heap_stats(struct net_buf *request);
+
+/**
  * @brief Run lte_at_cmd RPC
  *
  * @param request @ref INFUSE_RPC_CMD packet to respond to
---
+++ subsys/rpc/commands/heap_stats.c
@@ -0,0 +1,51 @@
+/**
+ * @file
+ * @copyright 2025 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <string.h>
+
+#include <zephyr/net_buf.h>
+
+#include <infuse/rpc/commands.h>
+#include <infuse/rpc/types.h>
+#include <infuse/states.h>
+
+struct net_buf *rpc_command_heap_stats(struct net_buf *request)
+{
+	struct rpc_heap_stats_response rsp = {0};
+	struct sys_memory_stats heap_stats;
+	int max_rsp, max_entries, num_heaps;
+	struct net_buf *rsp_buf;
+	struct k_heap *heaps;
+	int rc;
+
+	/* Allocate response object */
+	rsp_buf = rpc_response_simple_req(request, 0, &rsp, sizeof(rsp));
+	max_rsp = net_buf_tailroom(rsp_buf);
+	max_entries = max_rsp / sizeof(struct rpc_struct_heap_info);
+
+	/* Get the static heaps */
+	num_heaps = k_heap_array_get(&heaps);
+	for (int i = 0; i < MIN(max_entries, num_heaps); i++) {
+		/* Query info */
+		rc = sys_heap_runtime_stats_get(&heaps[i].heap, &heap_stats);
+		if ((rc != 0) || (heaps[i].heap.init_bytes == 0)) {
+			continue;
+		}
+
+		/* Push into the response */
+		struct rpc_struct_heap_info *info = net_buf_add(rsp_buf, sizeof(*info));
+
+		info->addr = (uintptr_t)&heaps[i];
+		info->free_bytes = heap_stats.free_bytes;
+		info->allocated_bytes = heap_stats.allocated_bytes;
+		info->max_allocated_bytes = heap_stats.max_allocated_bytes;
+	}
+
+	/* Return the response */
+	return rsp_buf;
+}
