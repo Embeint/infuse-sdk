8248ed66a19384ac1039c87c869c40be27517feb
Jordan Yates <jordan@embeint.com>
2025-07-02 18:16:26 +1000

    lib: infuse_work_q: static stack area
    Make the stack variable for the Infuse workqueue static to prevent name
    collisions.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- lib/infuse_work_q.c
+++ lib/infuse_work_q.c
@@ -10,14 +10,14 @@
 
 struct k_work_q infuse_iot_work_q;
 
-K_THREAD_STACK_DEFINE(workq_stack_area, CONFIG_INFUSE_WORKQ_STACK_SIZE);
+static K_THREAD_STACK_DEFINE(infuse_workq_stack_area, CONFIG_INFUSE_WORKQ_STACK_SIZE);
 
 static int infuse_iot_work_queue_init(void)
 {
 	/* Boot the task runner workqueue */
 	k_work_queue_init(&infuse_iot_work_q);
-	k_work_queue_start(&infuse_iot_work_q, workq_stack_area,
-			   K_THREAD_STACK_SIZEOF(workq_stack_area),
+	k_work_queue_start(&infuse_iot_work_q, infuse_workq_stack_area,
+			   K_THREAD_STACK_SIZEOF(infuse_workq_stack_area),
 			   CONFIG_SYSTEM_WORKQUEUE_PRIORITY, NULL);
 	k_thread_name_set(k_work_queue_thread_get(&infuse_iot_work_q), "infuse_workq");
 	return 0;
