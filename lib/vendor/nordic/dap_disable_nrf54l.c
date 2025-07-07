c22ce912de736d920e0f9f99e6219bc6b8011355
Jordan Yates <jordan@embeint.com>
2025-07-07 16:19:16 +1000

    lib: vendor: nordic: nRF54L DAP disable
    Add support for disabling the debug access port on nRF54L devices.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ lib/vendor/nordic/dap_disable_nrf54l.c
@@ -0,0 +1,44 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ *
+ * https://docs.nordicsemi.com/bundle/ps_nrf54L15/page/debug.html
+ */
+
+#include <zephyr/sys/reboot.h>
+
+#include <nrfx_rramc.h>
+#include <nrf.h>
+
+void infuse_security_disable_dap(void)
+{
+	if ((NRF_UICR_S->APPROTECT[0].PROTECT0 & UICR_APPROTECT_PROTECT0_PALL_Msk) !=
+	    UICR_APPROTECT_PROTECT0_PALL_Unprotected) {
+		/* APPROTECT already written to enabled state */
+		return;
+	}
+
+	/* Unbuffered writes */
+	nrf_rramc_config_t config = {.mode_write = true, .write_buff_size = 0};
+
+	/* Enable write mode */
+	nrf_rramc_config_set(NRF_RRAMC, &config);
+
+	/* Write PROTECT registers to a non-default value */
+	NRF_UICR_S->SECUREAPPROTECT[0].PROTECT0 = 0xAA55AA55;
+	while (!nrf_rramc_ready_check(NRF_RRAMC)) {
+	}
+	NRF_UICR_S->APPROTECT[0].PROTECT0 = 0xAA55AA55;
+	while (!nrf_rramc_ready_check(NRF_RRAMC)) {
+	}
+
+	/* Disable write mode */
+	config.mode_write = false;
+	nrf_rramc_config_set(NRF_RRAMC, &config);
+
+	/* Reboot so that configuration is applied */
+	sys_reboot(SYS_REBOOT_WARM);
+}
