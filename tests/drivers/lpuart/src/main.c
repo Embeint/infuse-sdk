f81cfb6029ac74d96c22fd17aab1893597253603
Jordan Yates <jordan@embeint.com>
2025-07-17 21:32:43 +1000

    tests: drivers: lpuart: build test
    Validate the LPUART driver compiles.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ tests/drivers/lpuart/src/main.c
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/kernel.h>
+
+int main(void)
+{
+	const struct device *lpuart = DEVICE_DT_GET_ONE(nordic_nrf_sw_lpuart);
+
+	printk("LPUART: %p %s\n", lpuart, lpuart->name);
+
+	return 0;
+}
