bf1753cc7f96573bab2da856b4439f0ceda545e3
Jordan Yates <jordan@embeint.com>
2025-07-21 00:13:39 +1000

    epacket: interfaces: udp: dynamic MTU
    Update the UDP interface to dynamically set the maximum packet size
    instead of hardcoding from the IPV4 MTU. This allows the interface to
    take advantage of larger values of `CONFIG_EPACKET_PACKET_SIZE_MAX`.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- subsys/epacket/interfaces/epacket_udp.c
+++ subsys/epacket/interfaces/epacket_udp.c
@@ -42,9 +42,7 @@
 
 #define DT_DRV_COMPAT embeint_epacket_udp
 
-#define STATIC_MAX_PAYLOAD                                                                         \
-	EPACKET_INTERFACE_PAYLOAD_FROM_PACKET(DT_DRV_INST(0), (NET_IPV4_MTU - NET_IPV4UDPH_LEN))
-#define STATIC_MAX_PACKET MIN(CONFIG_EPACKET_PACKET_SIZE_MAX, (NET_IPV4_MTU - NET_IPV4UDPH_LEN))
+#define UDP_PAYLOAD(max_pkt) EPACKET_INTERFACE_PAYLOAD_FROM_PACKET(DT_DRV_INST(0), max_pkt)
 
 enum {
 	UDP_STATE_L4_CONNECTED = BIT(0),
@@ -71,6 +69,7 @@ struct udp_state {
 	uint16_t ack_countdown;
 	socklen_t remote_len;
 	uint16_t remote_port;
+	uint16_t iface_max_pkt;
 	int sock;
 };
 static struct udp_state udp_state;
@@ -145,8 +144,14 @@ static void cleanup_interface(const struct device *epacket_udp)
 static void l4_event_handler(struct net_mgmt_event_callback *cb, uint32_t event,
 			     struct net_if *iface)
 {
+	uint16_t iface_max_pkt;
+	uint16_t iface_mtu;
+
 	if (event == NET_EVENT_L4_CONNECTED) {
-		LOG_INF("Network connected");
+		iface_mtu = net_if_get_mtu(iface);
+		iface_max_pkt = iface_mtu - NET_IPV4UDPH_LEN;
+		udp_state.iface_max_pkt = MIN(iface_max_pkt, CONFIG_EPACKET_PACKET_SIZE_MAX);
+		LOG_INF("Network connected (MTU %d, PKT %d)", iface_mtu, udp_state.iface_max_pkt);
 		k_event_post(&udp_state.state, UDP_STATE_L4_CONNECTED);
 	} else if (event == NET_EVENT_L4_DISCONNECTED) {
 		k_event_clear(&udp_state.state, UDP_STATE_L4_CONNECTED);
@@ -233,7 +238,8 @@ static int epacket_udp_loop(void *a, void *b, void *c)
 		/* Interface is now connected */
 		SYS_SLIST_FOR_EACH_CONTAINER(&data->callback_list, cb, node) {
 			if (cb->interface_state) {
-				cb->interface_state(STATIC_MAX_PAYLOAD, cb->user_ctx);
+				cb->interface_state(UDP_PAYLOAD(udp_state.iface_max_pkt),
+						    cb->user_ctx);
 			}
 		}
 		k_event_post(&udp_state.state, UDP_STATE_CLIENTS_NOTIFIED_UP);
@@ -479,7 +485,7 @@ static void epacket_udp_decrypt_res(const struct device *dev, struct net_buf *bu
 
 static uint16_t epacket_udp_max_packet(const struct device *dev)
 {
-	return k_event_test(&udp_state.state, UDP_STATE_SOCKET_OPEN) ? STATIC_MAX_PACKET : 0;
+	return k_event_test(&udp_state.state, UDP_STATE_SOCKET_OPEN) ? udp_state.iface_max_pkt : 0;
 }
 
 #ifdef CONFIG_ZTEST
@@ -526,7 +532,7 @@ static const struct epacket_interface_api udp_api = {
 BUILD_ASSERT(sizeof(struct epacket_udp_frame) == DT_INST_PROP(0, header_size));
 static struct epacket_interface_common_data epacket_udp_data;
 static const struct epacket_interface_common_config epacket_udp_config = {
-	.max_packet_size = STATIC_MAX_PACKET,
+	.max_packet_size = CONFIG_EPACKET_PACKET_SIZE_MAX,
 	.header_size = DT_INST_PROP(0, header_size),
 	.footer_size = DT_INST_PROP(0, footer_size),
 };
