8c355f0d7068ccd22953e34e5cb04757b475025f
Jordan Yates <jordan@embeint.com>
2025-08-08 12:39:21 +1000

    data_logger: backends: shim: test PM states
    Valdiate PM states while running API functions in the shim driver.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- subsys/data_logger/backends/shim.c
+++ subsys/data_logger/backends/shim.c
@@ -11,6 +11,9 @@
 #include <string.h>
 
 #include <zephyr/device.h>
+#include <zephyr/sys/__assert.h>
+#include <zephyr/pm/device.h>
+#include <zephyr/pm/device_runtime.h>
 
 #include <infuse/data_logger/logger.h>
 #include <infuse/data_logger/backend/shim.h>
@@ -31,6 +34,12 @@ static int logger_shim_write(const struct device *dev, uint32_t phy_block,
 			     enum infuse_type data_type, const void *mem, uint16_t mem_len)
 {
 	struct dl_shim_data *data = dev->data;
+	enum pm_device_state state;
+
+	if (pm_device_runtime_is_enabled(dev)) {
+		__ASSERT_NO_MSG(pm_device_state_get(dev, &state) == 0);
+		__ASSERT_NO_MSG(state == PM_DEVICE_STATE_ACTIVE);
+	}
 
 	data->func.write.num_calls += 1;
 	data->func.write.data_type = data_type;
@@ -43,6 +52,12 @@ static int logger_shim_read(const struct device *dev, uint32_t phy_block, uint16
 			    void *mem, uint16_t mem_len)
 {
 	struct dl_shim_data *data = dev->data;
+	enum pm_device_state state;
+
+	if (pm_device_runtime_is_enabled(dev)) {
+		__ASSERT_NO_MSG(pm_device_state_get(dev, &state) == 0);
+		__ASSERT_NO_MSG(state == PM_DEVICE_STATE_ACTIVE);
+	}
 
 	memset(mem, 0x00, mem_len);
 
@@ -55,6 +70,12 @@ static int logger_shim_read(const struct device *dev, uint32_t phy_block, uint16
 static int logger_shim_erase(const struct device *dev, uint32_t phy_block, uint32_t num)
 {
 	struct dl_shim_data *data = dev->data;
+	enum pm_device_state state;
+
+	if (pm_device_runtime_is_enabled(dev)) {
+		__ASSERT_NO_MSG(pm_device_state_get(dev, &state) == 0);
+		__ASSERT_NO_MSG(state == PM_DEVICE_STATE_ACTIVE);
+	}
 
 	data->func.erase.num_calls += 1;
 	data->func.erase.phy_block = phy_block;
@@ -69,6 +90,12 @@ static int logger_shim_reset(const struct device *dev, uint32_t block_hint,
 			     void (*erase_progress)(uint32_t blocks_erased))
 {
 	struct dl_shim_data *data = dev->data;
+	enum pm_device_state state;
+
+	if (pm_device_runtime_is_enabled(dev)) {
+		__ASSERT_NO_MSG(pm_device_state_get(dev, &state) == 0);
+		__ASSERT_NO_MSG(state == PM_DEVICE_STATE_ACTIVE);
+	}
 
 	data->func.reset.num_calls += 1;
 	data->func.reset.block_hint = block_hint;
@@ -104,11 +131,20 @@ int logger_shim_init(const struct device *dev)
 	data->func.erase.block_until = NULL;
 	data->func.reset.block_until = NULL;
 
+	/* Disable PM (in case this is a re-initialisation in testing) */
+	(void)pm_device_runtime_disable(dev);
+
 	/* Common init function */
 	rc = data_logger_common_init(dev);
 
 	/* Common init does a number of read calls */
 	data->func.read.num_calls = 0;
+
+	if (rc == 0) {
+		/* Force PM to be enabled to validate states */
+		rc = pm_device_runtime_enable(dev);
+		__ASSERT_NO_MSG(rc == 0);
+	}
 	return rc;
 }
 
@@ -135,6 +171,14 @@ data_logger_backend_shim_data_pointer(const struct device *dev)
 	return &data->func;
 }
 
+static int shim_pm_control(const struct device *dev, enum pm_device_action action)
+{
+	ARG_UNUSED(dev);
+	ARG_UNUSED(action);
+
+	return 0;
+}
+
 const struct data_logger_api data_logger_shim_api = {
 	.write = logger_shim_write,
 	.read = logger_shim_read,
@@ -149,8 +193,9 @@ const struct data_logger_api data_logger_shim_api = {
 		.physical_blocks = DT_INST_PROP(inst, physical_blocks),                            \
 	};                                                                                         \
 	static struct dl_shim_data data##inst;                                                     \
-	DEVICE_DT_INST_DEFINE(inst, logger_shim_init, NULL, &data##inst, &config##inst,            \
-			      POST_KERNEL, 80, &data_logger_shim_api);
+	PM_DEVICE_DT_INST_DEFINE(inst, shim_pm_control);                                           \
+	DEVICE_DT_INST_DEFINE(inst, logger_shim_init, PM_DEVICE_DT_INST_GET(inst), &data##inst,    \
+			      &config##inst, POST_KERNEL, 80, &data_logger_shim_api);
 
 #define DATA_LOGGER_DEFINE_WRAPPER(inst)                                                           \
 	IF_ENABLED(DATA_LOGGER_DEPENDENCIES_MET(DT_DRV_INST(inst)), (DATA_LOGGER_DEFINE(inst)))
