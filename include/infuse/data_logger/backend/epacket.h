09bdd2d66191f93c3c35c46410a56060258f6136
Jordan Yates <jordan@embeint.com>
2025-08-14 20:44:09 +1000

    data_logger: epacket: option to set flags
    Add the option to set flags on an ePacket data logger backend.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ include/infuse/data_logger/backend/epacket.h
@@ -0,0 +1,33 @@
+/**
+ * @file
+ * @brief ePacket specific data logging interface
+ * @copyright 2025 Embeint Inc
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: LicenseRef-Embeint
+ */
+
+#ifndef INFUSE_SDK_INCLUDE_INFUSE_DATA_LOGGER_BACKEND_EPACKET_H_
+#define INFUSE_SDK_INCLUDE_INFUSE_DATA_LOGGER_BACKEND_EPACKET_H_
+
+#include <stdint.h>
+
+#include <zephyr/device.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Set ePacket flags for this backend
+ *
+ * @param dev ePacket data logger to configure
+ * @param flags Bitmask of @ref epacket_flags or interface specific flags
+ */
+void logger_epacket_flags_set(const struct device *dev, uint16_t flags);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INFUSE_SDK_INCLUDE_INFUSE_DATA_LOGGER_BACKEND_EPACKET_H_ */
