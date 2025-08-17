6ad6c6aeba72be7270a624b740ef23938abb4166
Jordan Yates <jordan@embeint.com>
2025-08-17 13:01:45 +1000

    tests: tdf: util: test `tdf_reboot_info_from_state`
    Add tests for various `tdf_reboot_info_from_state` inputs.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- tests/subsys/tdf/util/src/main.c
+++ tests/subsys/tdf/util/src/main.c
@@ -15,6 +15,7 @@
 #include <infuse/tdf/tdf.h>
 #include <infuse/tdf/util.h>
 #include <infuse/time/epoch.h>
+#include <infuse/reboot.h>
 
 ZTEST(tdf_util, test_acc_range_to_tdf)
 {
@@ -46,6 +47,80 @@ ZTEST(tdf_util, test_bt_addr_conv)
 	tdf_bt_addr_le_from_stack(&addr_rnd, &tdf_addr);
 	zassert_equal(BT_ADDR_LE_RANDOM, tdf_addr.type);
 	zassert_mem_equal(tdf_addr.val, addr_rnd.a.val, 6);
+}
+
+ZTEST(tdf_util, test_reboot_info)
+{
+	struct infuse_reboot_state state;
+	struct tdf_reboot_info tdf;
+
+	/* Generic storage */
+	state.reason = INFUSE_REBOOT_MCUMGR;
+	state.uptime = 12345678;
+	state.hardware_reason = 0x9876;
+	state.info_type = INFUSE_REBOOT_INFO_GENERIC;
+	state.info.generic.info1 = 0x1234;
+	state.info.generic.info2 = 0x2345;
+	tdf_reboot_info_from_state(&state, &tdf);
+
+	zassert_equal(state.reason, tdf.reason);
+	zassert_equal(state.uptime, tdf.uptime);
+	zassert_equal(state.hardware_reason, tdf.hardware_flags);
+	zassert_equal(state.info.generic.info1, tdf.param_1);
+	zassert_equal(state.info.generic.info2, tdf.param_2);
+
+	/* Watchdog storage */
+	state.reason = INFUSE_REBOOT_HW_WATCHDOG;
+	state.info_type = INFUSE_REBOOT_INFO_WATCHDOG;
+	state.info.watchdog.info1 = 0x4321;
+	state.info.watchdog.info2 = 0x5432;
+	tdf_reboot_info_from_state(&state, &tdf);
+
+	zassert_equal(state.reason, tdf.reason);
+	zassert_equal(state.uptime, tdf.uptime);
+	zassert_equal(state.hardware_reason, tdf.hardware_flags);
+	zassert_equal(state.info.watchdog.info1, tdf.param_1);
+	zassert_equal(state.info.watchdog.info2, tdf.param_2);
+
+	/* Exception basic storage */
+	state.reason = K_ERR_CPU_EXCEPTION;
+	state.info_type = INFUSE_REBOOT_INFO_EXCEPTION_BASIC;
+	state.info.exception_basic.program_counter = 0x4567;
+	state.info.exception_basic.link_register = 0x5678;
+	tdf_reboot_info_from_state(&state, &tdf);
+
+	zassert_equal(state.reason, tdf.reason);
+	zassert_equal(state.uptime, tdf.uptime);
+	zassert_equal(state.hardware_reason, tdf.hardware_flags);
+	zassert_equal(state.info.exception_basic.program_counter, tdf.param_1);
+	zassert_equal(state.info.exception_basic.link_register, tdf.param_2);
+
+	/* Exception ESF storage */
+#ifdef CONFIG_ARM
+	state.reason = K_ERR_ARCH_START;
+	state.info_type = INFUSE_REBOOT_INFO_EXCEPTION_ESF;
+	state.info.exception_full.basic.pc = 0xABCD;
+	state.info.exception_full.basic.lr = 0xBCDE;
+	tdf_reboot_info_from_state(&state, &tdf);
+
+	zassert_equal(state.reason, tdf.reason);
+	zassert_equal(state.uptime, tdf.uptime);
+	zassert_equal(state.hardware_reason, tdf.hardware_flags);
+	zassert_equal(state.info.exception_full.basic.pc, tdf.param_1);
+	zassert_equal(state.info.exception_full.basic.lr, tdf.param_2);
+#endif /* CONFIG_ARM */
+#ifdef CONFIG_ARCH_POSIX
+	state.reason = K_ERR_ARCH_START;
+	state.info_type = INFUSE_REBOOT_INFO_EXCEPTION_ESF;
+	state.info.exception_full.dummy = 0x12345678;
+	tdf_reboot_info_from_state(&state, &tdf);
+
+	zassert_equal(state.reason, tdf.reason);
+	zassert_equal(state.uptime, tdf.uptime);
+	zassert_equal(state.hardware_reason, tdf.hardware_flags);
+	zassert_equal(0x00, tdf.param_1);
+	zassert_equal(0x00, tdf.param_2);
+#endif /* CONFIG_ARCH_POSIX*/
 }
 
 ZTEST_SUITE(tdf_util, NULL, NULL, NULL, NULL, NULL);
