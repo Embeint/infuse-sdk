2850f81b9e0d3947c5052bd96ebdccb74ac3269d
Jordan Yates <jordan@embeint.com>
2025-08-17 13:10:21 +1000

    tdf: generic exception stack frame definition
    Define a generic 32bit exception stack frame TDF.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- tests/subsys/definitions_extend/generated/include/infuse/fs/kv_types.h
+++ tests/subsys/definitions_extend/generated/include/infuse/fs/kv_types.h
@@ -326,6 +326,12 @@ struct kv_lora_config {
 	uint8_t sync_word;
 } __packed;
 
+/** Request connected Bluetooth peers to limit throughtput */
+struct kv_bluetooth_throughput_limit {
+	/** Requested throughput limit (kbps) */
+	uint16_t limit_kbps;
+} __packed;
+
 /** Reference gravity vector for tilt calculations */
 struct kv_gravity_reference {
 	/** X axis component of gravity vector */
@@ -470,6 +476,8 @@ enum kv_builtin_id {
 	KV_KEY_BLUETOOTH_PEER = 50,
 	/** LoRa modem configuration */
 	KV_KEY_LORA_CONFIG = 51,
+	/** Request connected Bluetooth peers to limit throughtput */
+	KV_KEY_BLUETOOTH_THROUGHPUT_LIMIT = 52,
 	/** Reference gravity vector for tilt calculations */
 	KV_KEY_GRAVITY_REFERENCE = 60,
 	/** Array of points defining a closed polygon */
@@ -528,6 +536,7 @@ enum kv_builtin_size {
 	_KV_KEY_LTE_NETWORKING_MODES_SIZE = sizeof(struct kv_lte_networking_modes),
 	_KV_KEY_BLUETOOTH_PEER_SIZE = sizeof(struct kv_bluetooth_peer),
 	_KV_KEY_LORA_CONFIG_SIZE = sizeof(struct kv_lora_config),
+	_KV_KEY_BLUETOOTH_THROUGHPUT_LIMIT_SIZE = sizeof(struct kv_bluetooth_throughput_limit),
 	_KV_KEY_GRAVITY_REFERENCE_SIZE = sizeof(struct kv_gravity_reference),
 	_KV_KEY_TASK_SCHEDULES_DEFAULT_ID_SIZE = sizeof(struct kv_task_schedules_default_id),
 	_KV_KEY_EXT1_SIZE = sizeof(struct kv_ext1),
@@ -559,6 +568,7 @@ enum kv_builtin_size {
 #define _KV_KEY_LTE_NETWORKING_MODES_TYPE struct kv_lte_networking_modes
 #define _KV_KEY_BLUETOOTH_PEER_TYPE struct kv_bluetooth_peer
 #define _KV_KEY_LORA_CONFIG_TYPE struct kv_lora_config
+#define _KV_KEY_BLUETOOTH_THROUGHPUT_LIMIT_TYPE struct kv_bluetooth_throughput_limit
 #define _KV_KEY_GRAVITY_REFERENCE_TYPE struct kv_gravity_reference
 #define _KV_KEY_GEOFENCE_TYPE struct kv_geofence
 #define _KV_KEY_TASK_SCHEDULES_DEFAULT_ID_TYPE struct kv_task_schedules_default_id
@@ -611,6 +621,8 @@ enum kv_builtin_size {
 	IF_ENABLED(CONFIG_KV_STORE_KEY_BLUETOOTH_PEER, \
 		   (1 +)) \
 	IF_ENABLED(CONFIG_KV_STORE_KEY_LORA_CONFIG, \
+		   (1 +)) \
+	IF_ENABLED(CONFIG_KV_STORE_KEY_BLUETOOTH_THROUGHPUT_LIMIT, \
 		   (1 +)) \
 	IF_ENABLED(CONFIG_KV_STORE_KEY_GRAVITY_REFERENCE, \
 		   (1 +)) \
@@ -812,6 +824,13 @@ static struct key_value_slot_definition _KV_SLOTS_ARRAY_DEFINE[] = {
 		.flags = KV_FLAGS_REFLECT,
 	},
 #endif /* CONFIG_KV_STORE_KEY_LORA_CONFIG */
+#ifdef CONFIG_KV_STORE_KEY_BLUETOOTH_THROUGHPUT_LIMIT
+	{
+		.key = KV_KEY_BLUETOOTH_THROUGHPUT_LIMIT,
+		.range = 1,
+		.flags = KV_FLAGS_REFLECT,
+	},
+#endif /* CONFIG_KV_STORE_KEY_BLUETOOTH_THROUGHPUT_LIMIT */
 #ifdef CONFIG_KV_STORE_KEY_GRAVITY_REFERENCE
 	{
 		.key = KV_KEY_GRAVITY_REFERENCE,
