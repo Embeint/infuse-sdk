bfb3bed88134556c8e42a1d960d8042bb39bb513
Jordan Yates <jordan@embeint.com>
2025-09-13 21:54:37 +1000

    tests: bsim: test with Nordic Softdevice
    Add babblesim tests for a subset of tests using the Nordic Softdevice.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ tests/bsim/bluetooth/epacket/tests_scripts/connect_sdc.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Copyright (c) 2024 Embeint Holdings Pty Ltd
+# SPDX-License-Identifier: Apache-2.0
+
+source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
+
+simulation_id="epacket_connect_sdc"
+verbosity_level=2
+
+cd ${BSIM_OUT_PATH}/bin
+
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_epacket_device_sdc_prj_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=0 -RealEncryption=0 \
+  -testid=epacket_bt_device -rs=23
+
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_epacket_gateway_sdc_prj_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=1 -RealEncryption=0 \
+  -testid=epacket_bt_gateway_connect -rs=6
+
+Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
+  -D=2 -sim_length=10e6 $@
+
+wait_for_background_jobs
