b713d4a412f65a94d75fd3b498d776ba2236a2d0
Jordan Yates <jordan@embeint.com>
2025-09-17 17:28:03 +1000

    manifest: update zephyr fork
    Update zephyr fork with support for writing coverage data using
    semihosting.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- scripts/twister_coverage.sh
+++ scripts/twister_coverage.sh
@@ -3,5 +3,4 @@
 echo "Twister Coverage - mps2/an385"
 ./zephyr/scripts/twister -i --force-color -N -v --filter runnable \
     -p mps2/an385 --coverage -T infuse-sdk/tests --coverage-tool gcovr \
-    -x=CONFIG_TEST_EXTRA_STACK_SIZE=4096 -e nano --coverage-basedir infuse-sdk \
-    -x=CONFIG_COVERAGE_DUMP_PATH_EXCLUSIVE=\"*twister-out/*infuse-sdk*\"
+    -x=CONFIG_TEST_EXTRA_STACK_SIZE=4096 -e nano --coverage-basedir infuse-sdk
