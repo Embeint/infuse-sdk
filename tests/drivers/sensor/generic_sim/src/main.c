2a6b96c1e02ad248e360e12ed9bd24bfeafa473f
Jordan Yates <jordan@embeint.com>
2025-10-06 11:33:35 +1000

    tests: drivers: test generic simulated sensor
    Add tests for the generic simulated sensor.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ tests/drivers/sensor/generic_sim/src/main.c
@@ -0,0 +1,71 @@
+/**
+ * @file
+ * @copyright 2024 Embeint Holdings Pty Ltd
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: FSL-1.1-ALv2
+ */
+
+#include <zephyr/ztest.h>
+#include <zephyr/kernel.h>
+#include <zephyr/devicetree.h>
+
+#include <infuse/drivers/sensor/generic_sim.h>
+
+ZTEST(drivers_sensor_generic_sim, test_init_fail)
+{
+	const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(fail_sensor));
+
+	zassert_false(device_is_ready(dev));
+}
+
+ZTEST(drivers_sensor_generic_sim, test_init_pass)
+{
+	const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(test_sensor));
+
+	zassert_true(device_is_ready(dev));
+}
+
+ZTEST(drivers_sensor_generic_sim, test_set_invalid)
+{
+	const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(test_sensor));
+	struct sensor_value val = {0};
+
+	zassert_equal(-EINVAL, generic_sim_channel_set(dev, SENSOR_CHAN_ALL, val));
+}
+
+ZTEST(drivers_sensor_generic_sim, test_value_echo)
+{
+	const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(test_sensor));
+	struct sensor_value val_write, val_read;
+
+	zassert_equal(0, sensor_sample_fetch(dev));
+
+	for (int i = 0; i < SENSOR_CHAN_ALL; i++) {
+		/* Not supported before configuring */
+		zassert_equal(-ENOTSUP, sensor_channel_get(dev, i, &val_read));
+		/* Can configure */
+		val_write.val1 = i + 1;
+		val_write.val2 = i - 10;
+		zassert_equal(0, generic_sim_channel_set(dev, i, val_write));
+		/* Returns the expected value */
+		zassert_equal(0, sensor_channel_get(dev, i, &val_read));
+		zassert_equal(val_write.val1, val_read.val1);
+		zassert_equal(val_write.val2, val_read.val2);
+	}
+
+	/* Reset clears all channels */
+	generic_sim_reset(dev);
+	for (int i = 0; i < SENSOR_CHAN_ALL; i++) {
+		zassert_equal(-ENOTSUP, sensor_channel_get(dev, i, &val_read));
+	}
+}
+
+static void before_fn(void *unused)
+{
+	const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(test_sensor));
+
+	generic_sim_reset(dev);
+}
+
+ZTEST_SUITE(drivers_sensor_generic_sim, NULL, NULL, before_fn, NULL, NULL);
