32b229def39aa271e2c936adb25b524caffa28ab
Jordan Yates <jordan@embeint.com>
2025-10-07 15:10:26 +1000

    drivers: led: lp581x: added
    Add a driver for the Texas Instruments LP581x family of LED controllers.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ drivers/led/lp581x.h
@@ -0,0 +1,30 @@
+/**
+ * @file
+ * @copyright 2025 Embeint Holdings Pty Ltd
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef INFUSE_SDK_DRIVERS_LED_LP581X_H_
+#define INFUSE_SDK_DRIVERS_LED_LP581X_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief lp581x API
+ * @defgroup lp581x_apis lp581x APIs
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INFUSE_SDK_DRIVERS_LED_LP581X_H_ */
