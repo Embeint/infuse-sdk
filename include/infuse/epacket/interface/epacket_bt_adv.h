78c63034906ee05482beaac071510f2918b27065
Jordan Yates <jordan@embeint.com>
2025-10-11 21:12:03 +1000

    epacket: interface: bt_adv: API to pause scanning
    Add an API for subsystems to request a pause in Bluetooth scanning, if
    it is currently running. This can yield significant speed improvements
    for some tasks, like erasing or writing to internal flash on nRF
    devices.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- include/infuse/epacket/interface/epacket_bt_adv.h
+++ include/infuse/epacket/interface/epacket_bt_adv.h
@@ -29,6 +29,31 @@ extern "C" {
 #define epacket_bt_adv_frame epacket_v0_versioned_frame_format
 
 /**
+ * @brief Request Bluetooth scanning to be suspended
+ *
+ * After calling, any active Bluetooth scanning is suspended and any future
+ * scanning is disabled until @ref epacket_bt_adv_scan_resume is called.
+ *
+ * A call to @ref epacket_receive received between these two function calls will
+ * be actioned once @ref epacket_bt_adv_scan_resume is called for the remaining
+ * duration.
+ *
+ * The duration of any suspension should be tightly bounded to much less than
+ * CONFIG_EPACKET_INTERFACE_BT_ADV_SCAN_WATCHDOG_SEC (default 10 minutes).
+ *
+ * This can be useful when performing actions where performance is typically degraded
+ * by Bluetooth activity, for example writing to internal flash.
+ */
+void epacket_bt_adv_scan_suspend(void);
+
+/**
+ * @brief Release a request for Bluetooth scanning to be suspended
+ *
+ * Release the constraint of Bluetooth scanning created by @ref epacket_bt_adv_scan_suspend.
+ */
+void epacket_bt_adv_scan_resume(void);
+
+/**
  * @}
  */
 
