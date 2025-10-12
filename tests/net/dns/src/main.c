cd9aa9bd7bffe029caf7b0eb5c0e7869a149c43d
Jordan Yates <jordan@embeint.com>
2025-10-12 12:45:23 +1000

    tests: net: dns: test `infuse_async_dns`
    Add tests for `infuse_async_dns`.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- tests/net/dns/src/main.c
+++ tests/net/dns/src/main.c
@@ -59,10 +59,112 @@ ZTEST(infuse_dns, test_dns_query)
 					 &address_len));
 	zassert_equal(sizeof(struct sockaddr_in6), address_len);
 
+#ifndef CONFIG_NET_NATIVE_OFFLOADED_SOCKETS
+	/* IPv6 queries through getaddrinfo to invalid addresses on POSIX can take a long time and
+	 * break the other tests for currently unknown reasons.
+	 */
 	zassert_not_equal(0, infuse_sync_dns("not.a.real.address", 80, AF_INET6, SOCK_STREAM,
 					     &address, &address_len));
+#endif /* !CONFIG_NET_NATIVE_OFFLOADED_SOCKETS */
 #endif /* defined(CONFIG_NET_IPV6) && !defined(CONFIG_WIFI) */
+
+	/* Interface is still up */
+	k_sem_give(&l4_up);
 }
+
+#ifdef CONFIG_INFUSE_DNS_ASYNC
+
+static K_SEM_DEFINE(async_success, 0, 1);
+static K_SEM_DEFINE(async_complete, 0, 1);
+static K_SEM_DEFINE(async_failure, 0, 1);
+
+static void async_dns_cb(int result, struct sockaddr *addr, socklen_t addrlen,
+			 struct infuse_async_dns_context *cb_ctx)
+{
+	socklen_t *address_len;
+
+	zassert_not_null(cb_ctx);
+
+	address_len = cb_ctx->user_data;
+	if (result == INFUSE_ASYNC_DNS_RESULT) {
+		/* Completion events have not occurred */
+		zassert_equal(-EBUSY, k_sem_take(&async_success, K_NO_WAIT));
+		zassert_equal(-EBUSY, k_sem_take(&async_complete, K_NO_WAIT));
+		/* Address is provided */
+		zassert_not_null(addr);
+		zassert_not_equal(0, addrlen);
+		/* Store/notify result */
+		*address_len = addrlen;
+		k_sem_give(&async_success);
+	} else if (result == INFUSE_ASYNC_DNS_COMPLETE) {
+		zassert_is_null(addr);
+		zassert_equal(0, addrlen);
+		k_sem_give(&async_complete);
+	} else {
+		zassert_is_null(addr);
+		zassert_equal(0, addrlen);
+		k_sem_give(&async_failure);
+	}
+}
+
+ZTEST(infuse_dns, test_dns_query_async)
+{
+	socklen_t address_len;
+	struct infuse_async_dns_context cb_ctx = {
+		.cb = async_dns_cb,
+		.user_data = &address_len,
+	};
+
+	/* Wait for the interface to come up */
+	zassert_equal(0, k_sem_take(&l4_up, IF_DELAY));
+
+	/* Invalid address families */
+	zassert_equal(-EINVAL, infuse_async_dns("google.com", AF_UNSPEC, &cb_ctx, 2000));
+	zassert_equal(-EINVAL, infuse_async_dns("google.com", AF_PACKET, &cb_ctx, 2000));
+	zassert_equal(-EINVAL, infuse_async_dns("google.com", AF_CAN, &cb_ctx, 2000));
+	zassert_equal(-EINVAL, infuse_async_dns("google.com", AF_NET_MGMT, &cb_ctx, 2000));
+	zassert_equal(-EINVAL, infuse_async_dns("google.com", AF_LOCAL, &cb_ctx, 2000));
+	zassert_equal(-EINVAL, infuse_async_dns("google.com", AF_UNIX, &cb_ctx, 2000));
+
+	/* IPv4 lookups */
+	zassert_equal(0, infuse_async_dns("google.com", AF_INET, &cb_ctx, 2000));
+	zassert_equal(0, k_sem_take(&async_success, K_SECONDS(2)));
+	zassert_equal(0, k_sem_take(&async_complete, K_MSEC(100)));
+	zassert_equal(-EBUSY, k_sem_take(&async_failure, K_NO_WAIT));
+	zassert_equal(sizeof(struct sockaddr_in), address_len);
+
+	zassert_equal(0, infuse_async_dns("not.a.real.address", AF_INET, &cb_ctx, 2000));
+	zassert_equal(0, k_sem_take(&async_failure, K_SECONDS(2)));
+	zassert_equal(-EBUSY, k_sem_take(&async_success, K_NO_WAIT));
+	zassert_equal(-EBUSY, k_sem_take(&async_complete, K_NO_WAIT));
+
+	/* Not working on WiFi network, not sure how to validate at runtime if this should work */
+#if defined(CONFIG_NET_IPV6) && !defined(CONFIG_WIFI)
+	/* IPv6 lookups */
+	zassert_equal(0, infuse_async_dns("google.com", AF_INET6, &cb_ctx, 2000));
+	zassert_equal(0, k_sem_take(&async_success, K_SECONDS(2)));
+	zassert_equal(0, k_sem_take(&async_complete, K_MSEC(100)));
+	zassert_equal(-EBUSY, k_sem_take(&async_failure, K_NO_WAIT));
+	zassert_equal(sizeof(struct sockaddr_in6), address_len);
+
+	zassert_equal(0, infuse_async_dns("not.a.real.address", AF_INET6, &cb_ctx, 2000));
+	zassert_equal(0, k_sem_take(&async_failure, K_SECONDS(2)));
+	zassert_equal(-EBUSY, k_sem_take(&async_success, K_NO_WAIT));
+	zassert_equal(-EBUSY, k_sem_take(&async_complete, K_NO_WAIT));
+#endif /* defined(CONFIG_NET_IPV6) && !defined(CONFIG_WIFI) */
+
+	/* Interface is still up */
+	k_sem_give(&l4_up);
+}
+
+#else
+
+ZTEST(infuse_dns, test_dns_query_async)
+{
+	ztest_test_skip();
+}
+
+#endif /* CONFIG_INFUSE_DNS_ASYNC */
 
 void *test_init(void)
 {
