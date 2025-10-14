df2cb728e08cdd815e373efa4e434b5a000c4d26
Jordan Yates <jordan@embeint.com>
2025-10-14 13:36:50 +1000

    auto: kv_state_observer: added
    Add a help library that monitors the KV store and updates application
    states. Currently only handles `KV_KEY_LED_DISABLE_DAILY_TIME_RANGE`.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ lib/auto/kv_state_observer.c
@@ -0,0 +1,147 @@
+/**
+ * @file
+ * @copyright 2025 Embeint Holdings Pty Ltd
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: FSL-1.1-ALv2
+ */
+
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+
+#include <infuse/fs/kv_store.h>
+#include <infuse/fs/kv_types.h>
+#include <infuse/states.h>
+#include <infuse/time/epoch.h>
+
+static void kv_state_obs_value_changed(uint16_t key, const void *data, size_t data_len,
+				       void *user_ctx);
+static void reference_time_updated(enum epoch_time_source source, struct timeutil_sync_instant old,
+				   struct timeutil_sync_instant new, void *user_ctx);
+
+static struct k_work_delayable led_delayable;
+static struct kv_store_cb kv_observer_cb = {
+	.value_changed = kv_state_obs_value_changed,
+};
+static struct epoch_time_cb epoch_observer_cb = {
+	.reference_time_updated = reference_time_updated,
+};
+static uint32_t disable_daily_seconds_start;
+static uint32_t disable_daily_seconds_end;
+static bool has_disable_daily;
+
+static uint32_t utc_seconds_from_hms(const struct kv_utc_hms *hms)
+{
+	return (hms->hour * SEC_PER_HOUR) + (hms->minute * SEC_PER_MIN) + hms->second;
+}
+
+static void led_disable_delayable(struct k_work *work)
+{
+	uint32_t utc_seconds;
+	struct kv_utc_hms hms;
+	uint32_t boundary;
+	uint64_t now;
+	struct tm c;
+
+	/* If we don't know the time or have a KV, we can't suppress */
+	if (!has_disable_daily || !epoch_time_trusted_source(epoch_time_get_source(), true)) {
+		infuse_state_clear(INFUSE_STATE_LED_SUPPRESS);
+		return;
+	}
+
+	/* Get current time */
+	now = epoch_time_now();
+	epoch_time_unix_calendar(now, &c);
+	hms.hour = c.tm_hour;
+	hms.minute = c.tm_min;
+	hms.second = c.tm_sec;
+	utc_seconds = utc_seconds_from_hms(&hms);
+
+	/* Handle current time vs windows */
+	if (disable_daily_seconds_start < disable_daily_seconds_end) {
+		if (utc_seconds < disable_daily_seconds_start) {
+			/* Before start window */
+			boundary = disable_daily_seconds_start - utc_seconds;
+			infuse_state_clear(INFUSE_STATE_LED_SUPPRESS);
+		} else if (utc_seconds > disable_daily_seconds_end) {
+			/* After end window */
+			boundary = disable_daily_seconds_start + (SEC_PER_DAY - utc_seconds);
+			infuse_state_clear(INFUSE_STATE_LED_SUPPRESS);
+		} else {
+			/* In range */
+			boundary = disable_daily_seconds_end - utc_seconds;
+			infuse_state_set(INFUSE_STATE_LED_SUPPRESS);
+		}
+	} else {
+		if (utc_seconds <= disable_daily_seconds_end) {
+			/* In range */
+			boundary = disable_daily_seconds_end - utc_seconds;
+			infuse_state_set(INFUSE_STATE_LED_SUPPRESS);
+		} else if (utc_seconds >= disable_daily_seconds_start) {
+			/* In range */
+			boundary = (SEC_PER_DAY - utc_seconds) + disable_daily_seconds_end;
+			infuse_state_set(INFUSE_STATE_LED_SUPPRESS);
+		} else {
+			/* Before start window */
+			boundary = disable_daily_seconds_start - utc_seconds;
+			infuse_state_clear(INFUSE_STATE_LED_SUPPRESS);
+		}
+	}
+
+	/* Set reschedule time */
+	boundary = MAX(1, boundary);
+	k_work_reschedule(&led_delayable, K_SECONDS(boundary));
+}
+
+static void kv_state_obs_value_changed(uint16_t key, const void *data, size_t data_len,
+				       void *user_ctx)
+{
+	const struct kv_led_disable_daily_time_range *disable_daily;
+
+	if (key == KV_KEY_LED_DISABLE_DAILY_TIME_RANGE) {
+		if (data == NULL) {
+			/* Slot has been deleted, cancel any suppression */
+			has_disable_daily = false;
+			k_work_cancel_delayable(&led_delayable);
+			infuse_state_clear(INFUSE_STATE_LED_SUPPRESS);
+		} else {
+			disable_daily = data;
+			/* Cache the current value */
+			disable_daily_seconds_start =
+				utc_seconds_from_hms(&disable_daily->disable_start);
+			disable_daily_seconds_end =
+				utc_seconds_from_hms(&disable_daily->disable_end);
+			has_disable_daily = true;
+			/* Re-evaluate immediately */
+			k_work_reschedule(&led_delayable, K_NO_WAIT);
+		}
+	}
+}
+
+static void reference_time_updated(enum epoch_time_source source, struct timeutil_sync_instant old,
+				   struct timeutil_sync_instant new, void *user_ctx)
+{
+	/* Re-evaluate immediately */
+	k_work_reschedule(&led_delayable, K_NO_WAIT);
+}
+
+static int kv_state_observer_init(void)
+{
+	struct kv_led_disable_daily_time_range disable_daily;
+
+	epoch_time_register_callback(&epoch_observer_cb);
+	kv_store_register_callback(&kv_observer_cb);
+	k_work_init_delayable(&led_delayable, led_disable_delayable);
+	/* Initialise the cached values */
+	if (KV_STORE_READ(KV_KEY_LED_DISABLE_DAILY_TIME_RANGE, &disable_daily) ==
+	    sizeof(disable_daily)) {
+		disable_daily_seconds_start = utc_seconds_from_hms(&disable_daily.disable_start);
+		disable_daily_seconds_end = utc_seconds_from_hms(&disable_daily.disable_end);
+		has_disable_daily = true;
+	}
+	/* Evaluate immediately */
+	k_work_schedule(&led_delayable, K_NO_WAIT);
+	return 0;
+}
+
+SYS_INIT(kv_state_observer_init, APPLICATION, 0);
