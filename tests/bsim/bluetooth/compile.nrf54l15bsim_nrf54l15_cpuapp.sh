5ea0749fbb2fe43bc6e836ad1fc5741f54cf5fd0
Jordan Yates <jordan@embeint.com>
2025-11-27 16:08:12 +1000

    tests: bsim: compile `nrf54l15bsim` apps
    Compile the test applications for `nrf54l15bsim`.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ tests/bsim/bluetooth/compile.nrf54l15bsim_nrf54l15_cpuapp.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Copyright 2025 Embeint Holdings Pty Ltd
+# SPDX-License-Identifier: Apache-2.0
+
+# Compile all the applications needed by the bsim tests in these subfolders
+
+# set -x #uncomment this line for debugging
+set -ue
+
+: "${ZEPHYR_BASE:?ZEPHYR_BASE must be set to point to the zephyr root directory}"
+: "${INFUSE_BASE:?INFUSE_BASE must be set to point to the Infuse-IoT root directory}"
+
+#Set a default value to BOARD if it does not have one yet
+BOARD="${BOARD:-nrf54l15bsim/nrf54l15/cpuapp}"
+OPTS="-iv --no-clean --outdir bsim/bluetooth_nrf54l15 -p ${BOARD}"
+
+west twister $OPTS -T ${INFUSE_BASE}/tests/bsim/bluetooth/
