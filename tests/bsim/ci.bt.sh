8487b7edf8d0ad31e1cb3768f5c91aed3f5859b9
Jordan Yates <jordan@embeint.com>
2025-11-27 16:08:12 +1000

    tests: bsim: compile `nrf54l15bsim` apps
    Compile and run a subset of test applications for `nrf54l15bsim`.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- tests/bsim/ci.bt.sh
+++ tests/bsim/ci.bt.sh
@@ -14,6 +14,25 @@ set -uex
 # nrf52_bsim set:
 nice ${INFUSE_BASE}/tests/bsim/bluetooth/compile.sh
 
+BOARD=nrf52_bsim/native \
 RESULTS_FILE=${WORK_DIR}/bsim_results.bt.52.xml \
 SEARCH_PATH=${INFUSE_RELATIVE}/tests/bsim/bluetooth \
+${ZEPHYR_BASE}/tests/bsim/run_parallel.sh
+
+# nrf54l15bsim/nrf54l15/cpuapp set:
+nice ${INFUSE_BASE}/tests/bsim/bluetooth/compile.nrf54l15bsim_nrf54l15_cpuapp.sh
+
+export EPACKET_TESTS="${INFUSE_BASE}/tests/bsim/bluetooth/epacket/tests_scripts"
+
+# Only run a subset of tests to validate basic functionality
+# The full testsuite takes much longer to run, and does not currently pass
+# The Zephyr controller has a few bugs with
+BOARD=nrf54l15bsim/nrf54l15/cpuapp \
+RESULTS_FILE=${WORK_DIR}/bsim_results.bt.54.xml \
+TESTS_LIST=\
+"${EPACKET_TESTS}/bt_file_copy.sh \
+${EPACKET_TESTS}/connect.sh \
+${EPACKET_TESTS}/scan.sh \
+${EPACKET_TESTS}/legacy_connect.sh \
+${EPACKET_TESTS}/mcumgr_reboot.sh" \
 ${ZEPHYR_BASE}/tests/bsim/run_parallel.sh
