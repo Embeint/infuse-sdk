5033fee955eb4fa8751848b8bb6b9e277bc53ec8
Jordan Yates <jordan@embeint.com>
2025-11-27 15:05:48 +1000

    tests: bsim: compile into workspace folder
    Compile the bsim binaries into the workspace folder, not `infuse-sdk`.
    
    The search path is provided as a relative path as the Github Actions
    workspace root directory is `/__w/`, which is filtered out by the
    `run_parallel.sh` filtering.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- tests/bsim/ci.802154.sh
+++ tests/bsim/ci.802154.sh
@@ -7,13 +7,13 @@
 
 export INFUSE_BASE="${INFUSE_BASE:-${PWD}}"
 export ZEPHYR_BASE="${ZEPHYR_BASE:-${INFUSE_BASE}/../zephyr}"
-cd ${INFUSE_BASE}
+export INFUSE_RELATIVE=$(realpath --relative-to=. ${INFUSE_BASE})
 
 set -uex
 
 # nrf52_bsim set:
-nice tests/bsim/ieee802154/compile.sh
+nice ${INFUSE_BASE}/tests/bsim/ieee802154/compile.sh
 
 RESULTS_FILE=${WORK_DIR}/bsim_results.802154.52.xml \
-SEARCH_PATH=tests/bsim/ieee802154 \
+SEARCH_PATH=${INFUSE_RELATIVE}/tests/bsim/ieee802154 \
 ${ZEPHYR_BASE}/tests/bsim/run_parallel.sh
