6cadf51b5e7ac2ed3a694fa20fb4451f8b987827
Jordan Yates <jordan@embeint.com>
2025-11-29 13:50:04 +1000

    modem: backend: common UBX modem structure
    Extract the fields common to both backends to a common header.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- include/infuse/modem/backend/u_blox_i2c.h
+++ include/infuse/modem/backend/u_blox_i2c.h
@@ -12,6 +12,8 @@
 
 #include <zephyr/modem/pipe.h>
 
+#include "common.h"
+
 #ifndef INFUSE_MODEM_BACKEND_U_BLOX_I2C_
 #define INFUSE_MODEM_BACKEND_U_BLOX_I2C_
 
@@ -20,30 +22,10 @@ extern "C" {
 #endif
 
 struct modem_backend_ublox_i2c {
+	/* Common backend */
+	struct modem_backend_common common;
 	/* Pointer to I2C configuration */
 	const struct i2c_dt_spec *i2c;
-	/* Pointer to data ready pin configuration */
-	const struct gpio_dt_spec *data_ready;
-	/* Communication pipe */
-	struct modem_pipe pipe;
-	/* Ring buffer for holding pipe data stream */
-	struct ring_buf pipe_ring_buf;
-	/* Worker that reads the FIFO */
-	struct k_work_delayable fifo_read;
-	/* Signal to notify when FIFO read completes */
-	struct k_poll_signal read_result;
-	/* Callback for data ready GPIO */
-	struct gpio_callback data_ready_cb;
-	/* Bus (RTIO SQE) contention semaphore */
-	struct k_sem bus_sem;
-	/* Duration between polls */
-	k_timeout_t poll_period;
-	/* Number of bytes pending on FIFO */
-	uint16_t bytes_pending;
-	/* Internal state flags */
-	uint8_t flags;
-	/* Backing memory for pipe data stream */
-	uint8_t pipe_memory[CONFIG_GNSS_U_BLOX_PIPE_SIZE];
 };
 
 struct modem_backend_ublox_i2c_config {
--- include/infuse/modem/backend/u_blox_spi.h
+++ include/infuse/modem/backend/u_blox_spi.h
@@ -12,6 +12,8 @@
 
 #include <zephyr/modem/pipe.h>
 
+#include "common.h"
+
 #ifndef INFUSE_MODEM_BACKEND_U_BLOX_SPI_
 #define INFUSE_MODEM_BACKEND_U_BLOX_SPI_
 
@@ -20,30 +22,10 @@ extern "C" {
 #endif
 
 struct modem_backend_ublox_spi {
+	/* Common backend */
+	struct modem_backend_common common;
 	/* Pointer to SPI configuration */
 	const struct spi_dt_spec *spi;
-	/* Pointer to data ready pin configuration */
-	const struct gpio_dt_spec *data_ready;
-	/* Communication pipe */
-	struct modem_pipe pipe;
-	/* Ring buffer for holding pipe data stream */
-	struct ring_buf pipe_ring_buf;
-	/* Worker that reads the FIFO */
-	struct k_work_delayable fifo_read;
-	/* Signal to notify when FIFO read completes */
-	struct k_poll_signal read_result;
-	/* Callback for data ready GPIO */
-	struct gpio_callback data_ready_cb;
-	/* Bus contention semaphore */
-	struct k_sem bus_sem;
-	/* Duration between polls */
-	k_timeout_t poll_period;
-	/* Number of bytes pending on FIFO */
-	uint16_t bytes_pending;
-	/* Internal state flags */
-	uint8_t flags;
-	/* Backing memory for pipe data stream */
-	uint8_t pipe_memory[CONFIG_GNSS_U_BLOX_PIPE_SIZE];
 	/* Temporary RX buffer */
 	uint8_t spi_rx[70];
 };
