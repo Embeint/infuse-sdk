12661ebf26eb99522d319b8c5f347ddf9181772f
Jordan Yates <jordan@embeint.com>
2025-12-13 12:11:56 +1000

    rpc: commands: data_logger_state_v2: added
    Add a new version of the `data_logger_state` RPC that can return larger
    erase block sizes.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- subsys/rpc/commands/data_logger_state.c
+++ subsys/rpc/commands/data_logger_state.c
@@ -12,15 +12,11 @@
 #include <infuse/rpc/types.h>
 #include <infuse/data_logger/logger.h>
 
-struct net_buf *rpc_command_data_logger_state(struct net_buf *request)
+static int data_logger_query(uint8_t logger_id, struct data_logger_state *state)
 {
-	struct rpc_data_logger_state_request *req = (void *)request->data;
-	struct rpc_data_logger_state_response rsp = {0};
-	struct data_logger_state state;
 	const struct device *logger;
-	int rc = 0;
 
-	switch (req->logger) {
+	switch (logger_id) {
 #ifdef CONFIG_DATA_LOGGER_FLASH_MAP
 	case RPC_ENUM_DATA_LOGGER_FLASH_ONBOARD:
 		logger = DEVICE_DT_GET_ONE(embeint_data_logger_flash_map);
@@ -32,29 +28,65 @@ struct net_buf *rpc_command_data_logger_state(struct net_buf *request)
 		break;
 #endif /* CONFIG_DATA_LOGGER_EXFAT */
 	default:
-		rc = -ENODEV;
-		goto end;
+		return -ENODEV;
 	}
 
-	/* Ensure device initialised properly */
+	/* Ensure device logger_id properly */
 	if (!device_is_ready(logger)) {
-		rc = -EBADF;
-		goto end;
+		return -EBADF;
 	}
 
 	/* Query logger state */
-	data_logger_get_state(logger, &state);
-	rsp.bytes_logged = state.bytes_logged;
-	rsp.logical_blocks = state.logical_blocks;
-	rsp.physical_blocks = state.physical_blocks;
-	rsp.boot_block = state.boot_block;
-	rsp.current_block = state.current_block;
-	rsp.earliest_block = state.earliest_block;
-	rsp.block_size = state.block_size;
-	rsp.block_overhead = state.block_overhead;
-	rsp.erase_unit = state.erase_unit;
-	rsp.uptime = k_uptime_seconds();
+	data_logger_get_state(logger, state);
+	return 0;
+}
 
-end:
+#ifdef CONFIG_INFUSE_RPC_COMMAND_DATA_LOGGER_STATE
+struct net_buf *rpc_command_data_logger_state(struct net_buf *request)
+{
+	struct rpc_data_logger_state_request *req = (void *)request->data;
+	struct rpc_data_logger_state_response rsp = {0};
+	struct data_logger_state state;
+	int rc;
+
+	rc = data_logger_query(req->logger, &state);
+	if (rc == 0) {
+		rsp.bytes_logged = state.bytes_logged;
+		rsp.logical_blocks = state.logical_blocks;
+		rsp.physical_blocks = state.physical_blocks;
+		rsp.boot_block = state.boot_block;
+		rsp.current_block = state.current_block;
+		rsp.earliest_block = state.earliest_block;
+		rsp.block_size = state.block_size;
+		rsp.block_overhead = state.block_overhead;
+		rsp.erase_unit = MIN(state.erase_unit, UINT16_MAX);
+		rsp.uptime = k_uptime_seconds();
+	}
 	return rpc_response_simple_req(request, rc, &rsp, sizeof(rsp));
 }
+#endif /* CONFIG_INFUSE_RPC_COMMAND_DATA_LOGGER_STATE */
+
+#ifdef CONFIG_INFUSE_RPC_COMMAND_DATA_LOGGER_STATE_V2
+struct net_buf *rpc_command_data_logger_state_v2(struct net_buf *request)
+{
+	struct rpc_data_logger_state_v2_request *req = (void *)request->data;
+	struct rpc_data_logger_state_v2_response rsp = {0};
+	struct data_logger_state state;
+	int rc;
+
+	rc = data_logger_query(req->logger, &state);
+	if (rc == 0) {
+		rsp.bytes_logged = state.bytes_logged;
+		rsp.logical_blocks = state.logical_blocks;
+		rsp.physical_blocks = state.physical_blocks;
+		rsp.boot_block = state.boot_block;
+		rsp.current_block = state.current_block;
+		rsp.earliest_block = state.earliest_block;
+		rsp.block_size = state.block_size;
+		rsp.block_overhead = state.block_overhead;
+		rsp.erase_unit = state.erase_unit;
+		rsp.uptime = k_uptime_seconds();
+	}
+	return rpc_response_simple_req(request, rc, &rsp, sizeof(rsp));
+}
+#endif /* CONFIG_INFUSE_RPC_COMMAND_DATA_LOGGER_STATE_V2 */
