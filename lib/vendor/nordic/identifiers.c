dd058f36a986845621f1673200bc6629ebbe0d57
Jordan Yates <jordan@embeint.com>
2026-01-16 09:26:15 +1000

    vendor: nordic: identifiers: locally managed address
    For Nordic SoCs with a trivially accessible random Bluetooth address,
    use it as a locally managed address if not otherwise provisioned.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- lib/vendor/nordic/identifiers.c
+++ lib/vendor/nordic/identifiers.c
@@ -14,13 +14,18 @@
 #include <nrf.h>
 
 #if defined(NRF_UICR_S)
+#define FICR_PTR NRF_FICR_NS
 #define UICR_PTR NRF_UICR_S
 #define UICR_REG OTP
 #else
+#define FICR_PTR NRF_FICR
 #define UICR_PTR NRF_UICR
 #define UICR_REG CUSTOMER
 #endif
 
+#if defined(CONFIG_SOC_SERIES_NRF52X) || defined(CONFIG_SOC_SERIES_NRF54LX)
+#define HAS_DEVICEADDR 1
+#endif
 struct nrf_uicr_structure {
 	uint64_t infuse_device_id;
 };
@@ -43,6 +48,19 @@ uint64_t vendor_infuse_device_id(void)
 
 	memcpy(&readout, (const void *)UICR_PTR->UICR_REG, sizeof(readout));
 #endif
+
+#ifdef HAS_DEVICEADDR
+	if (readout.infuse_device_id == UINT64_MAX) {
+		/* Device not provisioned, generate a locally administered address fom the Bluetooth
+		 * address
+		 */
+		const uint64_t bt_addr = BLUETOOTH_STATIC_RANDOM_PREFIX |
+					 (uint64_t)(FICR_PTR->DEVICEADDR[1] & 0xFFFF) << 32 |
+					 FICR_PTR->DEVICEADDR[0];
+
+		return local_infuse_device_id_from_bt(bt_addr);
+	}
+#endif /* HAS_DEVICEADDR */
 
 	return readout.infuse_device_id;
 }
