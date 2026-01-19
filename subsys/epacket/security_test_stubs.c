0e56e51a1f957c4900adf7f4dbda41c88bdd01f6
Jordan Yates <jordan@embeint.com>
2026-01-19 13:25:25 +1000

    epacket: security_test_stubs: added
    Add a file to stub out the required security functions in testing if
    `INFUSE_SECURITY` is not enabled.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ subsys/epacket/security_test_stubs.c
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * @copyright 2026 Embeint Holdings Pty Ltd
+ * @author Jordan Yates <jordan@embeint.com>
+ *
+ * SPDX-License-Identifier: FSL-1.1-ALv2
+ */
+
+#include <infuse/security_ids.h>
+
+uint32_t infuse_security_device_key_identifier(void)
+{
+	return 0;
+}
+
+uint32_t infuse_security_network_key_identifier(void)
+{
+	return 0;
+}
