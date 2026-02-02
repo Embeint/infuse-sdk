0cf6fa332b4c278e1d6b13e37486454be43023c5
Jordan Yates <jordan@embeint.com>
2026-02-02 14:36:04 +1000

    net: coap: extract common code
    Extra common code from the two backends into a common file.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- subsys/net/coap/infuse_coap_serial.c
+++ subsys/net/coap/infuse_coap_serial.c
@@ -15,109 +15,15 @@
 
 #include <infuse/net/coap.h>
 
-BUILD_ASSERT(COAP_TOKEN_MAX_LEN == sizeof(uint64_t));
+#include "common.h"
 
-#ifdef CONFIG_NET_IPV4_MTU
-/* If the MTU is explicitly defined, check the size before trying to use 1KB blocks */
-#define MTU_SUPPORTS_1KB (CONFIG_NET_IPV4_MTU >= (1024 + 64))
-#else
-/* Assume it is supported until a configuration proves otherwise */
-#define MTU_SUPPORTS_1KB 1
-#endif
+BUILD_ASSERT(COAP_TOKEN_MAX_LEN == sizeof(uint64_t));
 
 #ifdef CONFIG_INFUSE_COAP_TEST_PACKET_DROP_BITMASK
 static uint32_t rx_pkt_count;
 #endif /* INFUSE_COAP_TEST_PACKET_DROPS */
 
 LOG_MODULE_REGISTER(infuse_coap, CONFIG_INFUSE_COAP_LOG_LEVEL);
-
-/* Determine the locations of '/' characters and encode into array */
-static int resource_path_split(const char *resource, uint8_t *component_starts, uint8_t array_len)
-{
-	uint8_t num_paths = 1;
-	int i = 0;
-
-	/* Scan through string for the '/' character */
-	while (resource[i] != '\0') {
-		if (resource[i] == '/') {
-			if (num_paths == (array_len - 1)) {
-				/* Too many path splits */
-				return -EINVAL;
-			}
-			/* Store the start of the next component */
-			component_starts[num_paths++] = i + 1;
-		}
-		i++;
-	}
-	/* Add the end of the string with a hypothetical next component */
-	component_starts[num_paths] = i + 1;
-	return num_paths;
-}
-
-/* Append resource path using splits from `resource_path_split` */
-static int resource_path_append(struct coap_packet *request, const char *resource,
-				uint8_t *component_starts, uint8_t num_components)
-{
-	int rc;
-
-	/* Add all path components to packet */
-	for (int i = 0; i < num_components; i++) {
-		const char *comp_start = resource + component_starts[i];
-		int comp_len = component_starts[i + 1] - component_starts[i] - 1;
-
-		rc = coap_packet_append_option(request, COAP_OPTION_URI_PATH, comp_start, comp_len);
-		if (rc < 0) {
-			return rc;
-		}
-	}
-	return 0;
-}
-
-static int get_block_size(size_t working_size, uint16_t block_size, enum coap_block_size *used_size)
-{
-	switch (block_size) {
-	case 1024:
-		*used_size = COAP_BLOCK_1024;
-		break;
-	case 512:
-		*used_size = COAP_BLOCK_512;
-		break;
-	case 256:
-		*used_size = COAP_BLOCK_256;
-		break;
-	case 128:
-		*used_size = COAP_BLOCK_128;
-		break;
-	case 64:
-		*used_size = COAP_BLOCK_64;
-		break;
-	case 32:
-		*used_size = COAP_BLOCK_32;
-		break;
-	case 16:
-		*used_size = COAP_BLOCK_16;
-		break;
-	case 0:
-		/* Automatically determine block size */
-		if (MTU_SUPPORTS_1KB && (working_size >= (1024 + 64))) {
-			*used_size = COAP_BLOCK_1024;
-		} else if (working_size >= (512 + 64)) {
-			*used_size = COAP_BLOCK_512;
-		} else if (working_size >= (256 + 64)) {
-			*used_size = COAP_BLOCK_256;
-		} else if (working_size >= (128 + 64)) {
-			*used_size = COAP_BLOCK_128;
-		} else if (working_size >= 128) {
-			*used_size = COAP_BLOCK_64;
-		} else {
-			return -ENOMEM;
-		}
-		break;
-	default:
-		return -EINVAL;
-	}
-	return 0;
-}
 
 int infuse_coap_download(int socket, const char *resource, infuse_coap_data_cb data_cb,
 			 void *user_context, uint8_t *working_mem, size_t working_size,
@@ -139,7 +45,7 @@ int infuse_coap_download(int socket, const char *resource, infuse_coap_data_cb d
 		return -EINVAL;
 	}
 
-	rc = get_block_size(working_size, req_block_size, &block_size);
+	rc = ic_get_block_size(working_size, req_block_size, &block_size);
 	if (rc < 0) {
 		return rc;
 	}
@@ -148,7 +54,7 @@ int infuse_coap_download(int socket, const char *resource, infuse_coap_data_cb d
 
 	/* Pre-split the resource path into components */
 	uint8_t path_split[CONFIG_INFUSE_COAP_MAX_URI_SEGMENTS + 1] = {0};
-	int num_paths = resource_path_split(resource, path_split, ARRAY_SIZE(path_split));
+	int num_paths = ic_resource_path_split(resource, path_split, ARRAY_SIZE(path_split));
 
 	if (num_paths < 0) {
 		LOG_ERR("Failed to split resource path");
@@ -172,7 +78,7 @@ int infuse_coap_download(int socket, const char *resource, infuse_coap_data_cb d
 		__ASSERT_NO_MSG(rc == 0);
 
 		/* Resource path is arbitrary length so can fail */
-		rc = resource_path_append(&request, resource, path_split, num_paths);
+		rc = ic_resource_path_append(&request, resource, path_split, num_paths);
 		if (rc < 0) {
 			LOG_ERR("Path append failure");
 			return rc;
