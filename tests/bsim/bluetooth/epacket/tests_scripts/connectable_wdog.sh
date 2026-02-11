ec40b24c64a1ea62b002c434ef40239251b30a6c
Jordan Yates <jordan@embeint.com>
2026-02-11 10:26:43 +1000

    test: bsim: test connectable watchdog
    Validate the connectable watchdog expires if packets aren't advertised.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ tests/bsim/bluetooth/epacket/tests_scripts/connectable_wdog.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Copyright (c) 2026 Embeint Holdings Pty Ltd
+# SPDX-License-Identifier: Apache-2.0
+
+source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
+
+simulation_id="epacket_connectable_wdog"
+verbosity_level=2
+
+cd ${BSIM_OUT_PATH}/bin
+
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_epacket_device_prj_wdog_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=0 -RealEncryption=0 \
+  -testid=epacket_bt_connectable_wdog -rs=23
+
+Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
+  -D=1 -sim_length=10e6 $@
+
+wait_for_background_jobs
