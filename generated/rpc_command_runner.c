16fdd8a583d4d2d21aeec9069dc7ce54b57c0b70
Jordan Yates <jordan@embeint.com>
2026-06-16 21:43:03 +1000

    rpc: commands: coap_download_v3: added
    Add new variant of `coap_download` that supports saving arbitrary files
    to the LittleFS filesystem.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- generated/rpc_command_runner.c
+++ generated/rpc_command_runner.c
@@ -277,6 +277,13 @@ void rpc_command_runner(struct net_buf *request)
 		}
 		break;
 #endif /* CONFIG_INFUSE_RPC_COMMAND_COAP_DOWNLOAD_V2 */
+#ifdef CONFIG_INFUSE_RPC_COMMAND_COAP_DOWNLOAD_V3
+	case RPC_ID_COAP_DOWNLOAD_V3:
+		if (AUTHORISED(auth, COAP_DOWNLOAD_V3)) { /* GCOVR_EXCL_BR_LINE */
+			response = rpc_command_coap_download_v3(request);
+		}
+		break;
+#endif /* CONFIG_INFUSE_RPC_COMMAND_COAP_DOWNLOAD_V3 */
 #ifdef CONFIG_INFUSE_RPC_COMMAND_FILE_WRITE_BASIC
 	case RPC_ID_FILE_WRITE_BASIC:
 		if (AUTHORISED(auth, FILE_WRITE_BASIC)) { /* GCOVR_EXCL_BR_LINE */
