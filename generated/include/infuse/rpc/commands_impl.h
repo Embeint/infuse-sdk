1b851210747b7b1588502ac879a6498b34fba3c7
Jordan Yates <jordan@embeint.com>
2026-06-16 21:43:03 +1000

    rpc: commands: coap_download_v3: added
    Add new variant of `coap_download` that supports saving arbitrary files
    to the LittleFS filesystem.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- generated/include/infuse/rpc/commands_impl.h
+++ generated/include/infuse/rpc/commands_impl.h
@@ -280,6 +280,15 @@ struct net_buf *rpc_command_zperf_upload(struct net_buf *request);
 struct net_buf *rpc_command_coap_download_v2(struct net_buf *request);
 
 /**
+ * @brief Run coap_download_v3 RPC
+ *
+ * @param request @ref INFUSE_RPC_CMD packet to respond to
+ *
+ * @return struct net_buf* @ref INFUSE_RPC_RSP packet buffer
+ */
+struct net_buf *rpc_command_coap_download_v3(struct net_buf *request);
+
+/**
  * @brief Run file_write_basic RPC
  *
  * @param request @ref INFUSE_RPC_CMD packet to respond to
