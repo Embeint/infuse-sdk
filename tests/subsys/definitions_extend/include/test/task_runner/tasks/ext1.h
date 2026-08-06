607a041d210406483d74e2fe70906b9a71bc4405
Jordan Yates <jordan@embeint.com>
2026-08-06 16:16:21 +1000

    scripts: cloudgen: `tasks.json`: include namespace
    Make the namespacing of the task includes configurable from the JSON.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ tests/subsys/definitions_extend/include/test/task_runner/tasks/ext1.h
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * @brief Extension task definition
+ * @copyright 2024 Embeint Holdings Pty Ltd
+ *
+ * SPDX-License-Identifier: FSL-1.1-ALv2
+ */
+
+#ifndef INFUSE_SDK_TESTS_SUBSYS_DEFINITIONS_EXTEND_INCLUDE_TEST_TASK_RUNNER_TASKS_EXT1_H_
+#define INFUSE_SDK_TESTS_SUBSYS_DEFINITIONS_EXTEND_INCLUDE_TEST_TASK_RUNNER_TASKS_EXT1_H_
+
+#include <infuse/task_runner/tasks/ext1_args.h>
+#include <infuse/task_runner/tasks/infuse_task_ids.h>
+
+#endif /* INFUSE_SDK_TESTS_SUBSYS_DEFINITIONS_EXTEND_INCLUDE_TEST_TASK_RUNNER_TASKS_EXT1_H_ */
