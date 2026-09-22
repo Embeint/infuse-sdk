378a27e12e60210283229ff58d726c7a44fdd27c
Jordan Yates <jordan@embeint.com>
2026-09-22 16:57:42 +1000

    rpc: commands: kv_write: split implementation
    Break the implementation of `kv_write` into several functions.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- subsys/rpc/commands/kv_write.c
+++ subsys/rpc/commands/kv_write.c
@@ -18,15 +18,12 @@
 
 LOG_MODULE_DECLARE(rpc_server, CONFIG_INFUSE_RPC_LOG_LEVEL);
 
-struct net_buf *rpc_command_kv_write(struct net_buf *request)
+static struct net_buf *request_validate(struct net_buf *request)
 {
-	__maybe_unused struct epacket_rx_metadata *meta = net_buf_user_data(request);
 	struct rpc_kv_write_request *req = (void *)request->data;
-	struct rpc_kv_write_response rsp;
-	struct net_buf *response;
 	size_t offset = sizeof(struct rpc_kv_write_request);
+	struct rpc_kv_write_response rsp = {0};
 	size_t consumed;
-	int16_t rc;
 
 	/* Validate write requests don't run off the end of the buffer */
 	for (int i = 0; i < req->num; i++) {
@@ -60,6 +57,80 @@ struct net_buf *rpc_command_kv_write(struct net_buf *request)
 					       sizeof(rsp));
 	}
 
+	return NULL;
+}
+
+static int handle_single_write(struct epacket_rx_metadata *meta,
+			       struct rpc_struct_kv_store_value *v, struct net_buf *response)
+{
+	int rc;
+
+	/* Check for read only protection */
+	rc = kv_store_external_read_only(v->id);
+	if (rc == -EACCES) {
+		rc = INFUSE_RPC_ERROR_KV_KEY_NOT_ENABLED;
+	} else if (rc < 0) {
+		rc = INFUSE_RPC_ERROR_WRITE_PROTECTED;
+	}
+
+#ifdef CONFIG_INFUSE_RPC_OPTION_KV_WRITE_APP_VALIDATE
+	if (rc == 0) {
+		const void *ptr = v->len == 0 ? NULL : v->data;
+
+		/* Run application validation if read only check passed */
+		rc = infuse_rpc_command_kv_write_validate(meta, v->id, ptr, v->len)
+			     ? 0
+			     : INFUSE_RPC_ERROR_KV_WRITE_REJECTED;
+	}
+#endif /* CONFIG_INFUSE_RPC_OPTION_KV_WRITE_APP_VALIDATE */
+
+	if (rc != 0) {
+		goto push_response;
+	}
+
+	if (v->len == 0) {
+		/* Write the value */
+		LOG_DBG("Deleting key %d", v->id);
+		rc = kv_store_delete(v->id);
+		if (rc < 0) {
+			rc = rc == -EACCES ? INFUSE_RPC_ERROR_KV_KEY_NOT_ENABLED
+					   : INFUSE_RPC_ERROR_KV_DELETE_FAILED;
+		}
+	} else {
+		/* Write the value */
+		LOG_DBG("Writing key %d len %d", v->id, v->len);
+		rc = kv_store_write(v->id, v->data, v->len);
+		if (rc < 0) {
+			rc = rc == -EACCES ? INFUSE_RPC_ERROR_KV_KEY_NOT_ENABLED
+					   : INFUSE_RPC_ERROR_KV_WRITE_FAILED;
+		}
+	}
+
+push_response:
+	/* Push response onto the buffer.
+	 * If the backend has gone down, we still want to action writes.
+	 */
+	if (net_buf_tailroom(response) >= sizeof(int16_t)) {
+		net_buf_add_le16(response, rc);
+	}
+
+	return sizeof(struct rpc_struct_kv_store_value) + v->len;
+}
+
+struct net_buf *rpc_command_kv_write(struct net_buf *request)
+{
+	__maybe_unused struct epacket_rx_metadata *meta = net_buf_user_data(request);
+	struct rpc_kv_write_request *req = (void *)request->data;
+	struct rpc_kv_write_response rsp;
+	struct net_buf *response;
+	size_t offset;
+
+	/* Validate the request before actioning */
+	response = request_validate(request);
+	if (response != NULL) {
+		return response;
+	}
+
 	/* Allocate response object */
 	response = rpc_response_simple_req(request, 0, &rsp, sizeof(rsp));
 	offset = sizeof(struct rpc_kv_write_request);
@@ -67,53 +138,7 @@ struct net_buf *rpc_command_kv_write(struct net_buf *request)
 	for (int i = 0; i < req->num; i++) {
 		struct rpc_struct_kv_store_value *v = (void *)(request->data + offset);
 
-		/* Check for read only protection */
-		rc = kv_store_external_read_only(v->id);
-		if (rc == -EACCES) {
-			rc = INFUSE_RPC_ERROR_KV_KEY_NOT_ENABLED;
-		} else if (rc < 0) {
-			rc = INFUSE_RPC_ERROR_WRITE_PROTECTED;
-		}
-
-#ifdef CONFIG_INFUSE_RPC_OPTION_KV_WRITE_APP_VALIDATE
-		if (rc == 0) {
-			const void *ptr = v->len == 0 ? NULL : v->data;
-
-			/* Run application validation if read only check passed */
-			rc = infuse_rpc_command_kv_write_validate(meta, v->id, ptr, v->len)
-				     ? 0
-				     : INFUSE_RPC_ERROR_KV_WRITE_REJECTED;
-		}
-#endif /* CONFIG_INFUSE_RPC_OPTION_KV_WRITE_APP_VALIDATE */
-
-		if (rc == 0) {
-			if (v->len == 0) {
-				/* Write the value */
-				LOG_DBG("Deleting key %d", v->id);
-				rc = kv_store_delete(v->id);
-				if (rc < 0) {
-					rc = rc == -EACCES ? INFUSE_RPC_ERROR_KV_KEY_NOT_ENABLED
-							   : INFUSE_RPC_ERROR_KV_DELETE_FAILED;
-				}
-			} else {
-				/* Write the value */
-				LOG_DBG("Writing key %d len %d", v->id, v->len);
-				rc = kv_store_write(v->id, v->data, v->len);
-				if (rc < 0) {
-					rc = rc == -EACCES ? INFUSE_RPC_ERROR_KV_KEY_NOT_ENABLED
-							   : INFUSE_RPC_ERROR_KV_WRITE_FAILED;
-				}
-			}
-		}
-		/* Push response onto the buffer.
-		 * If the backend has gone down, we still want to action writes.
-		 */
-		if (net_buf_tailroom(response) >= sizeof(int16_t)) {
-			net_buf_add_le16(response, rc);
-		}
-
-		consumed = sizeof(struct rpc_struct_kv_store_value) + v->len;
-		offset += consumed;
+		offset += handle_single_write(meta, v, response);
 	}
 	return response;
 }
